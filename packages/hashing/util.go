4750668daa8124f8e9ca053f1cf72e09233228af
lunfardo314 <evaldas.drasutis@iota.org>
2020-06-10 13:53:06 +0300

    moved Bytes to util

--- packages/hashing/util.go
+++ packages/hashing/util.go
@@ -2,29 +2,8 @@ package hashing
 
 import (
 	"bytes"
-	"io"
 	"sort"
 )
-
-func Bytes(obj interface{ Write(io.Writer) error }) ([]byte, error) {
-	var buf bytes.Buffer
-	if err := obj.Write(&buf); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-func MustBytes(obj interface{ Write(io.Writer) error }) []byte {
-	ret, err := Bytes(obj)
-	if err != nil {
-		panic(err)
-	}
-	return ret
-}
-
-func GetHashValue(obj interface{ Write(io.Writer) error }) HashValue {
-	return *HashData(MustBytes(obj))
-}
 
 type sortedHashes []*HashValue
 
---
+++ packages/util/bytes.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"bytes"
+	"github.com/iotaledger/wasp/packages/hashing"
+	"io"
+)
+
+func Bytes(obj interface{ Write(io.Writer) error }) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := obj.Write(&buf); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func MustBytes(obj interface{ Write(io.Writer) error }) []byte {
+	ret, err := Bytes(obj)
+	if err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+func GetHashValue(obj interface{ Write(io.Writer) error }) hashing.HashValue {
+	return *hashing.HashData(MustBytes(obj))
+}
