63d71ff0899b0a5414e55fddec5216ffb4b3ac46
Diego Essaya <diego.essaya@iota.org>
2020-06-25 18:23:49 -0300

    Fix set-then-get behavior in sandbox state

--- packages/vm/processor/sandboximpl.go
+++ packages/vm/processor/sandboximpl.go
@@ -1,6 +1,8 @@
 package processor
 
 import (
+	"fmt"
+
 	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/address"
 	"github.com/iotaledger/hive.go/logger"
 	"github.com/iotaledger/wasp/packages/hashing"
@@ -94,9 +96,41 @@ func (s *stateWrapper) Index() uint32 {
 	return s.virtualState.StateIndex()
 }
 
+func (s *stateWrapper) Get(name string) ([]byte, bool) {
+	// FIXME: this is O(N) with N = amount of accumulated mutations
+	// it could be improved by caching the latest mutation for evey key
+	muts := s.stateUpdate.Mutations()
+	for i := muts.Len() - 1; i >= 0; i-- {
+		m := muts.At(i)
+		if (*m).Key() == name {
+			// The key-value pair has been modified during the current request
+			// return the latest assigned value
+			return (*m).Value()
+		}
+	}
+
+	// The key-value pair has not been modified
+	// Fetch its value from the virtual state
+	return s.virtualState.Variables().Get(name)
+}
+
 func (s *stateWrapper) GetInt64(name string) (int64, bool, error) {
-	// TODO: look in state update mutations?
-	return s.virtualState.Variables().GetInt64(name)
+	v, ok := s.Get(name)
+	if !ok {
+		return 0, false, nil
+	}
+	if len(v) != 8 {
+		return 0, false, fmt.Errorf("variable %s: %v is not an int64", name, v)
+	}
+	return int64(util.Uint64From8Bytes(v)), true, nil
+}
+
+func (s *stateWrapper) Del(name string) {
+	s.stateUpdate.Mutations().Add(variables.NewMutationDel(name))
+}
+
+func (s *stateWrapper) Set(name string, value []byte) {
+	s.stateUpdate.Mutations().Add(variables.NewMutationSet(name, value))
 }
 
 func (s *stateWrapper) SetInt64(name string, value int64) {
