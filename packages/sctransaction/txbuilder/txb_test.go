35c61b3c5846451d5c7f8638500961f0f54504a6
lunfardo314 <evaldas.drasutis@iota.org>
2020-06-26 17:46:02 +0300

    txbuilder unit tests

--- packages/sctransaction/txbuilder/txb_test.go
+++ packages/sctransaction/txbuilder/txb_test.go
@@ -207,3 +207,48 @@ func TestClone(t *testing.T) {
 
 	assert.EqualValues(t, tx.ID(), txClone.ID())
 }
+
+func TestDeterminism(t *testing.T) {
+	initUtxodb()
+
+	outs := utxodb.GetAddressOutputs(utxodb.GetAddress(1))
+	txb, err := NewFromOutputBalances(outs)
+	assert.NoError(t, err)
+
+	sh := hashing.RandomHash(nil)
+	err = txb.AddOriginStateBlock(sh, &scAddress)
+	assert.NoError(t, err)
+
+	err = txb.MoveToAddress(scAddress, balance.ColorIOTA, 50)
+
+	err = txb.AddRequestBlock(sctransaction.NewRequestBlock(scAddress, vmconst.RequestCodeInit))
+	assert.NoError(t, err)
+
+	txbClone := txb.Clone()
+
+	err = txb.MoveToAddress(scAddress, balance.ColorIOTA, 50)
+	assert.NoError(t, err)
+
+	err = txb.AddRequestBlock(sctransaction.NewRequestBlock(scAddress, vmconst.RequestCodeNOP))
+	assert.NoError(t, err)
+
+	err = txbClone.AddRequestBlock(sctransaction.NewRequestBlock(scAddress, vmconst.RequestCodeNOP))
+	assert.NoError(t, err)
+
+	err = txbClone.MoveToAddress(scAddress, balance.ColorIOTA, 50)
+	assert.NoError(t, err)
+
+	tx, err := txb.Build(false)
+	assert.NoError(t, err)
+
+	tx.Sign(utxodb.GetSigScheme(utxodb.GetAddress(1)))
+	assert.True(t, tx.SignaturesValid())
+
+	txClone, err := txbClone.Build(false)
+	assert.NoError(t, err)
+
+	txClone.Sign(utxodb.GetSigScheme(utxodb.GetAddress(1)))
+	assert.True(t, tx.SignaturesValid())
+
+	assert.EqualValues(t, tx.ID(), txClone.ID())
+}
