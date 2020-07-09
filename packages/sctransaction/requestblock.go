17f677d87491a85854bc719046237a9c2a32cb50
lunfardo314 <evaldas.drasutis@iota.org>
2020-07-09 10:37:15 +0300

    ref year 2038_problem

--- packages/sctransaction/requestblock.go
+++ packages/sctransaction/requestblock.go
@@ -22,6 +22,8 @@ type RequestId [RequestIdSize]byte
 // FIXME timelock uint32 ref Year 2038 problem https://en.wikipedia.org/wiki/Year_2038_problem
 // signed int32 can store values uo to 03:14:07 UTC on 19 January 2038
 // But if we use uint32 we should extend the range twice, something like until 2092. Not a problem ???
+// other wise we can use values in 'timelock' field of the request block
+// counting from 2020.01.01 Then it would extend until 2140 or so
 
 type RequestBlock struct {
 	// address of the target smart contract
