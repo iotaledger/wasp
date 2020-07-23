01ab0c0be42c39b1e27417681d4b9089de0b08f4
Diego Essaya <diego.essaya@iota.org>
2020-07-23 13:54:33 -0300

    dashboard: load fonts from jsdelivr

--- tools/fairroulette/dashboard/template.go
+++ tools/fairroulette/dashboard/template.go
@@ -39,7 +39,7 @@ var renderer = &Template{
 
 	<title>FairRoulette dashboard</title>
 
-	<link rel="stylesheet" href="https://fonts.xz.style/serve/inter.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/inter.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">
   </head>
 
