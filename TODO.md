50aa09ca82fbef31eac966acfd41b1f5e7c6bed6
lunfardo314 <evaldas.drasutis@iota.org>
2020-07-23 15:11:28 +0300

    todo list edit

--- TODO.md
+++ TODO.md
@@ -10,15 +10,24 @@
 - [ ] BufferedKVStore: Cache DB reads (which should not change in the DB during
       the BufferedKVStore lifetime)
 - [ ] implement mocked empty Sandbox for VM unit testing 
-- 
+- [ ] `Oracle Data Bulletin Board` description
+- [ ] `FairRoulette` with fees/rewards
+- [ ] test several contracts
+- [ ] test random confirmation delays
+- [ ] test big committees (~100 nodes)
+- [ ] test overlapping committees
+- [ ] enable and test 1 node committees
+- [ ] test quorum == 1  
+- [ ] optimize logging
+- [ ] Prometheus metrics
+- [ ] MQTT publisher
 
 # Roadmap
 - `FairRoulette` on Goshimmer testnet
 - Wasm VM and Rust environment for Wasm smart contracts 
 - `FairRoulette` on Rust->Wasm
 - `Oracle Data Bulletin Board` PoC
-- Node dashboard
-- Smart contract dashboard
-- Admin tools 
+- Node dashboard, smart contract dashboard, visualisation
+- Admin tools and APIs 
 - CLI wallet (universal and template)
 - architecture WP 
---
+++ packages/vm/sandbox/mockedsb.go
@@ -0,0 +1,82 @@
+package sandbox
+
+import (
+	"fmt"
+	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/address"
+	"github.com/iotaledger/hive.go/logger"
+	"github.com/iotaledger/wasp/packages/hashing"
+	"github.com/iotaledger/wasp/packages/kv"
+	"github.com/iotaledger/wasp/packages/sctransaction"
+	"github.com/iotaledger/wasp/packages/vm/vmtypes"
+)
+
+type MockedSandbox struct {
+}
+
+func NewMockedSandbox() vmtypes.Sandbox {
+	return &MockedSandbox{}
+}
+
+func (m *MockedSandbox) IsOriginState() bool {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) GetOwnAddress() *address.Address {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) GetTimestamp() int64 {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) GetEntropy() hashing.HashValue {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) Panic(v interface{}) {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) Rollback() {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) AccessRequest() vmtypes.RequestAccess {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) AccessState() kv.MustCodec {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) AccessOwnAccount() vmtypes.AccountAccess {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) SendRequest(par vmtypes.NewRequestParams) bool {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) SendRequestToSelf(reqCode sctransaction.RequestCode, args kv.Map) bool {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) SendRequestToSelfWithDelay(reqCode sctransaction.RequestCode, args kv.Map, deferForSec uint32) bool {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) Publish(msg string) {
+	fmt.Printf("MockedSandbox.Publish: %s", msg)
+}
+
+func (m *MockedSandbox) Publishf(format string, args ...interface{}) {
+	fmt.Printf("MockedSandbox.Publish: "+format, args)
+}
+
+func (m *MockedSandbox) GetWaspLog() *logger.Logger {
+	panic("implement me")
+}
+
+func (m *MockedSandbox) DumpAccount() string {
+	panic("implement me")
+}
