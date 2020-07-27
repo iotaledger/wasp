d48f8c599fb0b5349fa41c0bd1144adc35e023fd
lunfardo314 <evaldas.drasutis@iota.org>
2020-07-27 11:12:59 +0300

    FairAuction test. CreateRequest API for colored coins

--- tools/cluster/tests/wasptest/util.go
+++ tools/cluster/tests/wasptest/util.go
@@ -2,12 +2,77 @@ package wasptest
 
 import (
 	"fmt"
+	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/balance"
 	"github.com/iotaledger/goshimmer/dapps/waspconn/packages/utxodb"
 	waspapi "github.com/iotaledger/wasp/packages/apilib"
 	"github.com/iotaledger/wasp/packages/sctransaction"
 	"github.com/iotaledger/wasp/tools/cluster"
 	"time"
 )
+
+const goshimmerDirectly = true
+
+func CreateOrigin1SC(clu *cluster.Cluster, sc *cluster.SmartContractFinalConfig) error {
+	//fmt.Printf("------------------------------   Test 3: create origin of 1 SC\n")
+
+	var bindAddress string
+	if goshimmerDirectly {
+		bindAddress = clu.Config.GoshimmerApiHost()
+	} else {
+		bindAddress = clu.ApiHosts()[0]
+	}
+
+	//fmt.Printf("++++++++++ create origin bind address: %s\n", bindAddress)
+
+	tx, err := cluster.CreateOrigin(bindAddress, sc)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("++++++++++ created origin tx:\n%s\n", tx.String())
+
+	ownerAddr := utxodb.GetAddress(sc.OwnerIndexUtxodb)
+	sh := tx.MustState().StateHash()
+	fmt.Printf("[cluster] new origin tx: id: %s, state hash: %v, addr: %s owner: %s\n",
+		tx.ID().String(), sh.String(), sc.Address, ownerAddr.String())
+
+	if tx.ID().String() != sc.Color {
+		return fmt.Errorf("mismatch: origin tx id %s should be equal to SC color %s", tx.ID().String(), sc.Color)
+	}
+
+	// in real situation we have to wait for confirmation
+	err = clu.PostAndWaitForConfirmation(tx.Transaction)
+	if err != nil {
+		return err
+	}
+	//fmt.Printf("[cluster] posted node origin tx to Goshimmer: addr: %s, txid: %s\n", sc.Address, tx.ID().String())
+
+	//outs, err := nodeapi.GetAccountOutputs(clu.Config.Goshimmer.BindAddress, &ownerAddr)
+	//if err != nil{
+	//	fmt.Printf("nodeapi.GetAccountOutputs after post origin %s: %v\n", ownerAddr.String(), err)
+	//} else {
+	//	fmt.Printf("nodeapi.GetAccountOutputs after post origin %s: \n%+v\n", ownerAddr.String(), outs)
+	//}
+	return nil
+}
+
+func mintNewColoredTokens(wasps *cluster.Cluster, ownerIdx int, amount int64) (*balance.Color, error) {
+	ownerAddr := utxodb.GetAddress(ownerIdx)
+	ownerSigScheme := utxodb.GetSigScheme(ownerAddr)
+	tx, err := waspapi.NewColoredTokensTransaction(wasps.Config.GoshimmerApiHost(), ownerSigScheme, amount)
+	if err != nil {
+		return nil, err
+	}
+	err = wasps.PostAndWaitForConfirmation(tx)
+	if err != nil {
+		return nil, err
+	}
+	ret := (balance.Color)(tx.ID())
+
+	fmt.Printf("[cluster] minted %d new tokens of color %s\n", amount, ret.String())
+	//fmt.Printf("Value ts: %s\n", tx.String())
+	return &ret, nil
+}
 
 func MakeRequests(n int, constr func(int) *waspapi.RequestBlockJson) []*waspapi.RequestBlockJson {
 	ret := make([]*waspapi.RequestBlockJson, n)
--- tools/cluster/tests/wasptest/wasp_fa_test.go
+++ tools/cluster/tests/wasptest/wasp_fa_test.go
@@ -1,13 +1,11 @@
 package wasptest
 
 import (
-	"fmt"
 	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/address"
 	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/balance"
 	"github.com/iotaledger/goshimmer/dapps/waspconn/packages/utxodb"
 	waspapi "github.com/iotaledger/wasp/packages/apilib"
 	"github.com/iotaledger/wasp/packages/vm/examples/fairauction"
-	"github.com/iotaledger/wasp/tools/cluster"
 	"testing"
 	"time"
 )
@@ -247,20 +245,84 @@ func TestFA2Color0Bids(t *testing.T) {
 	}
 }
 
-func mintNewColoredTokens(wasps *cluster.Cluster, ownerIdx int, amount int64) (*balance.Color, error) {
-	ownerAddr := utxodb.GetAddress(ownerIdx)
-	ownerSigScheme := utxodb.GetSigScheme(ownerAddr)
-	tx, err := waspapi.NewColoredTokensTransaction(wasps.Config.GoshimmerApiHost(), ownerSigScheme, amount)
-	if err != nil {
-		return nil, err
-	}
-	err = wasps.PostAndWaitForConfirmation(tx)
-	if err != nil {
-		return nil, err
-	}
-	ret := (balance.Color)(tx.ID())
+func TestFA1Color5Bids(t *testing.T) {
+	// setup
+	wasps := setup(t, "test_cluster", "TestFairAuction5Requests5Sec1")
 
-	fmt.Printf("[cluster] minted %d new tokens of color %s\n", amount, ret.String())
-	//fmt.Printf("Value ts: %s\n", tx.String())
-	return &ret, nil
+	err := wasps.ListenToMessages(map[string]int{
+		"bootuprec":           wasps.NumSmartContracts(),
+		"active_committee":    1,
+		"dismissed_committee": 0,
+		"request_in":          3,
+		"request_out":         4,
+		"state":               -1,
+		"vmmsg":               -1,
+	})
+	check(err, t)
+
+	err = PutBootupRecords(wasps)
+	check(err, t)
+
+	// number 5 is "Wasm VM PoC program" in cluster.json
+	sc := &wasps.SmartContractConfig[scNumFairAuction]
+
+	err = Activate1SC(wasps, sc)
+	check(err, t)
+
+	err = CreateOrigin1SC(wasps, sc)
+	check(err, t)
+
+	time.Sleep(1 * time.Second)
+
+	// create 1 colored token
+	color1, err := mintNewColoredTokens(wasps, sc.OwnerIndexUtxodb, 1)
+	check(err, t)
+
+	ownerAddr := utxodb.GetAddress(sc.OwnerIndexUtxodb)
+	if !wasps.VerifyAddressBalances(ownerAddr, map[balance.Color]int64{
+		balance.ColorIOTA: 1000000000 - 3,
+		*color1:           1,
+	}) {
+		t.Fail()
+		return
+	}
+
+	scAddr, err := address.FromBase58(sc.Address)
+	check(err, t)
+
+	// send request StartAuction
+	err = SendSimpleRequest(wasps, sc.OwnerIndexUtxodb, waspapi.CreateSimpleRequestParams{
+		SCAddress:   &scAddr,
+		RequestCode: fairauction.RequestStartAuction,
+		Vars: map[string]interface{}{
+			fairauction.VarReqAuctionColor:                color1,
+			fairauction.VarReqStartAuctionMinimumBid:      100,
+			fairauction.VarReqStartAuctionDurationMinutes: 1,
+		},
+		Transfer: map[balance.Color]int64{
+			balance.ColorIOTA: 5, // 5% from 100
+			*color1:           1, // token for sale
+		},
+	})
+	check(err, t)
+
+	wasps.CollectMessages(70 * time.Second)
+
+	if !wasps.Report() {
+		t.Fail()
+	}
+
+	if !wasps.VerifyAddressBalances(scAddr, map[balance.Color]int64{
+		balance.ColorIOTA: 7,
+		// +1 SC token
+	}) {
+		t.Fail()
+	}
+
+	if !wasps.VerifyAddressBalances(ownerAddr, map[balance.Color]int64{
+		balance.ColorIOTA: 1000000000 - 8 - 1,
+		*color1:           1,
+	}) {
+		t.Fail()
+	}
 }
