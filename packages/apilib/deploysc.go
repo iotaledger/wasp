4a1c1ca1d57cea2cd69f3a95a6184c7349e0273a
lunfardo314 <evaldas.drasutis@iota.org>
2020-08-16 20:43:44 +0300

    consensus: notifications and leader rotation deadline

--- packages/apilib/deploysc.go
+++ packages/apilib/deploysc.go
@@ -46,21 +46,19 @@ func CreateAndDeploySC(par CreateAndDeploySCParams) (*address.Address, *balance.
 
 	fmt.Fprint(textout, par.Prefix)
 	if ok := vm.IsBuiltinProgramHash(par.ProgramHash.String()); !ok {
-		fmt.Fprint(textout, "user-defined program hash.. \n")
+		fmt.Fprint(textout, "program hash is user-defined.. \n")
 		// it is not a builtin smart contract. Check for metadata
 		// must exist and be consistent
 		if err := CheckProgramMetadata(par.CommitteeApiHosts, &par.ProgramHash); err != nil {
-			fmt.Fprintf(textout, "checking program hash: FAILED: %v\n", err)
+			fmt.Fprintf(textout, "checking program metadata: FAILED: %v\n", err)
 			return nil, nil, err
 		} else {
-			fmt.Fprint(textout, "checking program hash: DONE\n ")
+			fmt.Fprint(textout, "checking program metadata: OK\n ")
 		}
 	} else {
-		fmt.Fprint(textout, "builtin program hash.. DONE\n")
+		fmt.Fprintf(textout, "builtin program %s. OK\n", par.ProgramHash.String())
 	}
 	// generate distributed key set on committee nodes
-	fmt.Fprint(textout, par.Prefix)
-	fmt.Fprint(textout, "generating distributed key set..\n")
 	scAddr, err := GenerateNewDistributedKeySet(par.CommitteeApiHosts, par.N, par.T)
 
 	fmt.Fprint(textout, par.Prefix)
@@ -68,11 +66,10 @@ func CreateAndDeploySC(par CreateAndDeploySCParams) (*address.Address, *balance.
 		fmt.Fprintf(textout, "generating distributed key set.. FAILED: %v\n", err)
 		return nil, nil, err
 	} else {
-		fmt.Fprint(textout, "generating distributed key set.. DONE\n ")
+		fmt.Fprintf(textout, "generating distributed key set.. OK. Generated address = %s\n", scAddr.String())
 	}
 
 	fmt.Fprint(textout, par.Prefix)
-	fmt.Fprint(textout, "requesting owner address' UTXOs from node..\n")
 	allOuts, err := par.Node.GetAccountOutputs(&ownerAddr)
 
 	fmt.Fprint(textout, par.Prefix)
@@ -80,12 +77,10 @@ func CreateAndDeploySC(par CreateAndDeploySCParams) (*address.Address, *balance.
 		fmt.Fprintf(textout, "requesting owner address' UTXOs from node.. FAILED: %v\n", err)
 		return nil, nil, err
 	} else {
-		fmt.Fprint(textout, "requesting owner address' UTXOs from node.. DONE\n ")
+		fmt.Fprint(textout, "requesting owner address' UTXOs from node.. OK\n")
 	}
 
 	// create origin transaction
-	fmt.Fprint(textout, par.Prefix)
-	fmt.Fprint(textout, "creating origin transaction..\n")
 	originTx, err := origin.NewOriginTransaction(origin.NewOriginTransactionParams{
 		Address:              *scAddr,
 		OwnerSignatureScheme: par.OwnerSigScheme,
@@ -99,15 +94,18 @@ func CreateAndDeploySC(par CreateAndDeploySCParams) (*address.Address, *balance.
 		fmt.Fprintf(textout, "creating origin transaction.. FAILED: %v\n", err)
 		return nil, nil, err
 	} else {
-		fmt.Fprintf(textout, "creating origin transaction.. DONE. Origin txid = %s\n", originTx.ID().String())
+		fmt.Fprintf(textout, "creating origin transaction.. OK. Origin txid = %s\n", originTx.ID().String())
 	}
 
-	fmt.Fprint(textout, par.Prefix)
-	fmt.Fprint(textout, "posting and confirming origin transaction to the Tangle..")
 	err = par.Node.PostAndWaitForConfirmation(originTx.Transaction)
+	fmt.Fprint(textout, par.Prefix)
 	if err != nil {
+		fmt.Fprintf(textout, "posting origin transaction.. FAILED: %v\n", err)
 		return nil, nil, err
+	} else {
+		fmt.Fprintf(textout, "posting origin transaction.. OK. Origin txid = %s\n", originTx.ID().String())
 	}
+
 	succ, errs := PutSCDataMulti(par.CommitteeApiHosts, registry.BootupData{
 		Address:        *scAddr,
 		OwnerAddress:   ownerAddr,
@@ -119,15 +117,13 @@ func CreateAndDeploySC(par CreateAndDeploySCParams) (*address.Address, *balance.
 	fmt.Fprint(textout, par.Prefix)
 	if !succ {
 		err = multicall.WrapErrors(errs)
-		fmt.Fprintf(textout, "posting and confirming origin transaction to the Tangle.. FAILED: %v\n", err)
+		fmt.Fprintf(textout, "sending smart contract metadata to Wasp nodes.. FAILED: %v\n", err)
 		return nil, nil, err
 	} else {
-		fmt.Fprint(textout, "posting and confirming origin transaction to the Tangle.. DONE.\n")
+		fmt.Fprint(textout, "sending smart contract metadata to Wasp nodes.. OK.\n")
 	}
 	// TODO not finished with access nodes
 
-	fmt.Fprint(textout, par.Prefix)
-	fmt.Fprint(textout, "activating smart contract on Wasp nodes..")
 	err = ActivateSCMulti(par.CommitteeApiHosts, scAddr.String())
 
 	fmt.Fprint(textout, par.Prefix)
@@ -136,14 +132,13 @@ func CreateAndDeploySC(par CreateAndDeploySCParams) (*address.Address, *balance.
 		fmt.Fprintf(textout, "activating smart contract on Wasp nodes.. FAILED: %v\n", err)
 		return nil, nil, err
 	} else {
-		fmt.Fprint(textout, "activating smart contract on Wasp nodes.. DONE.\n")
+		fmt.Fprint(textout, "activating smart contract on Wasp nodes.. OK.\n")
 	}
 
 	color := (balance.Color)(originTx.ID())
 	fmt.Fprint(textout, par.Prefix)
-	fmt.Fprintf(textout, "smart contract has been created and deployed succesfully\nAddress: %s\n Color: %s\n",
-		scAddr.String(), color.String())
-	fmt.Fprintf(textout, "Number of committee nodes: %d\n Quorum factor: %d\n", par.N, par.T)
+	fmt.Fprintf(textout, "smart contract has been created and deployed succesfully. Address: %s, Color: %s, N = %d, T = %d\n",
+		scAddr.String(), color.String(), par.N, par.T)
 
 	return scAddr, &color, nil
 }
