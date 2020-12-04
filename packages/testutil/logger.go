4e5e7ef37327ff4936df20ca014bd85e6b6a09a9
lunfardo314 <evaldas.drasutis@iota.org>
2020-12-04 10:20:20 +0200

    copied testutil.NewLogger from 'dkg' branch

---
+++ packages/testutil/logger.go
@@ -0,0 +1,25 @@
+package testutil
+
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+
+	"github.com/iotaledger/hive.go/logger"
+	"go.uber.org/zap"
+)
+
+// NewLogger produces a logger adjusted for test cases.
+func NewLogger(t *testing.T) *logger.Logger {
+	log, err := zap.NewDevelopment()
+	require.NoError(t, err)
+	return log.Named(t.Name()).Sugar()
+}
+
+// WithLevel returns a logger with a level increased.
+// Can be useful in tests to disable logging in some parts of the system.
+func WithLevel(log *logger.Logger, level logger.Level) *logger.Logger {
+	return log.Desugar().WithOptions(zap.IncreaseLevel(level)).Sugar()
+}
