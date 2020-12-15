925937b615dbd7b4839f3d00725d8285a9b9dbea
Diego Essaya <diego.essaya@iota.org>
2020-12-15 14:07:45 -0300

    dashboard code cleanup

---
+++ packages/dashboard/callview.go
@@ -0,0 +1,23 @@
+package dashboard
+
+import (
+	"fmt"
+
+	"github.com/iotaledger/wasp/packages/chain"
+	"github.com/iotaledger/wasp/packages/coretypes"
+	"github.com/iotaledger/wasp/packages/kv/dict"
+	"github.com/iotaledger/wasp/packages/vm/viewcontext"
+)
+
+func callView(chain chain.Chain, hname coretypes.Hname, fname string, params dict.Dict) (dict.Dict, error) {
+	vctx, err := viewcontext.NewFromDB(*chain.ID(), chain.Processors())
+	if err != nil {
+		return nil, fmt.Errorf(fmt.Sprintf("Failed to create context: %v", err))
+	}
+
+	ret, err := vctx.CallView(hname, coretypes.Hn(fname), nil)
+	if err != nil {
+		return nil, fmt.Errorf("root view call failed: %v", err)
+	}
+	return ret, nil
+}
