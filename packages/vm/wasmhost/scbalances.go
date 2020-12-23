064685a58b57660641705dd09cae79a27bf47eb7
Eric Hop <git@bugfree.be>
2020-12-23 09:22:08 -0800

    Added color list to balances

--- packages/vm/wasmhost/scbalances.go
+++ packages/vm/wasmhost/scbalances.go
@@ -45,9 +45,59 @@ func (o *ScBalances) GetInt(keyId int32) int64 {
 	return balances.Balance(color)
 }
 
+func (o *ScBalances) GetObjectId(keyId int32, typeId int32) int32 {
+	return GetMapObjectId(o, keyId, typeId, ObjFactories{
+		KeyColor: func() WaspObject { return &ScBalanceColors{incoming: o.incoming} },
+	})
+}
+
 func (o *ScBalances) GetTypeId(keyId int32) int32 {
 	if o.Exists(keyId) {
 		return OBJTYPE_INT
 	}
 	return 0
+}
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\
+
+type ScBalanceColors struct {
+	ScDict
+	incoming bool
+	colors   []balance.Color
+}
+
+func (o *ScBalanceColors) Exists(keyId int32) bool {
+	o.loadColors()
+	return keyId >= 0 && keyId < int32(len(o.colors))
+}
+
+func (o *ScBalanceColors) GetBytes(keyId int32) []byte {
+	if !o.Exists(keyId) {
+		o.Panic("GetBytes: invalid color index")
+	}
+	return o.colors[keyId].Bytes()
+}
+
+func (o *ScBalanceColors) GetTypeId(keyId int32) int32 {
+	if o.Exists(keyId) {
+		return OBJTYPE_COLOR
+	}
+	return 0
+}
+
+func (o *ScBalanceColors) loadColors() {
+	if len(o.colors) > 0 {
+		return
+	}
+	balances := o.vm.Balances()
+	if o.incoming {
+		if o.vm.ctx == nil {
+			return
+		}
+		balances = o.vm.ctx.IncomingTransfer()
+	}
+	balances.IterateDeterministic(func(color balance.Color, balance int64) bool {
+		o.colors = append(o.colors, color)
+		return true
+	})
 }
