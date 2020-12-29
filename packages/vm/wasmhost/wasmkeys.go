cec2c6ede40c04ab974074fff9ed64b2236d9c36
Eric Hop <git@bugfree.be>
2020-12-28 19:22:43 -0800

    Removed unused keys, added latest Wasm

--- packages/vm/wasmhost/wasmkeys.go
+++ packages/vm/wasmhost/wasmkeys.go
@@ -3,8 +3,7 @@ package wasmhost
 // @formatter:off
 const (
 	KeyAgent       = int32(-1)
-	KeyAmount      = KeyAgent       - 1
-	KeyBalances    = KeyAmount      - 1
+	KeyBalances    = KeyAgent       - 1
 	KeyBase58      = KeyBalances    - 1
 	KeyCaller      = KeyBase58      - 1
 	KeyCalls       = KeyCaller      - 1
@@ -16,15 +15,13 @@ const (
 	KeyData        = KeyCreator     - 1
 	KeyDelay       = KeyData        - 1
 	KeyDescription = KeyDelay       - 1
-	KeyError       = KeyDescription - 1
-	KeyEvent       = KeyError       - 1
+	KeyEvent       = KeyDescription - 1
 	KeyExports     = KeyEvent       - 1
 	KeyFunction    = KeyExports     - 1
 	KeyHash        = KeyFunction    - 1
 	KeyId          = KeyHash        - 1
 	KeyIncoming    = KeyId          - 1
-	KeyIota        = KeyIncoming    - 1
-	KeyLength      = KeyIota        - 1
+	KeyLength      = KeyIncoming    - 1
 	KeyLog         = KeyLength      - 1
 	KeyLogs        = KeyLog         - 1
 	KeyName        = KeyLogs        - 1
@@ -39,14 +36,12 @@ const (
 	KeyTransfers   = KeyTrace       - 1
 	KeyUtility     = KeyTransfers   - 1
 	KeyViews       = KeyUtility     - 1
-	KeyWarning     = KeyViews       - 1
-	KeyZzzzzzz     = KeyWarning     - 1
+	KeyZzzzzzz     = KeyViews       - 1
 )
 // @formatter:on
 
 var keyMap = map[string]int32{
 	"agent":       KeyAgent,
-	"amount":      KeyAmount,
 	"balances":    KeyBalances,
 	"base58":      KeyBase58,
 	"caller":      KeyCaller,
@@ -59,14 +54,12 @@ var keyMap = map[string]int32{
 	"data":        KeyData,
 	"delay":       KeyDelay,
 	"description": KeyDescription,
-	"error":       KeyError,
 	"event":       KeyEvent,
 	"exports":     KeyExports,
 	"function":    KeyFunction,
 	"hash":        KeyHash,
 	"id":          KeyId,
 	"incoming":    KeyIncoming,
-	"iota":        KeyIota,
 	"length":      KeyLength,
 	"log":         KeyLog,
 	"logs":        KeyLogs,
@@ -82,5 +75,4 @@ var keyMap = map[string]int32{
 	"transfers":   KeyTransfers,
 	"utility":     KeyUtility,
 	"views":       KeyViews,
-	"warning":     KeyWarning,
 }
