19939c65f805bcfa6e09b82bac4acc296997c20e
lunfardo314 <evaldas.drasutis@iota.org>
2021-01-04 11:04:02 +0200

    minor edit

--- packages/vm/sandbox/sandbox.go
+++ packages/vm/sandbox/sandbox.go
@@ -103,7 +103,7 @@ func (s *sandbox) Log() vmtypes.LogInterface {
 }
 
 func (s *sandbox) Event(msg string) {
-	s.Log().Infof("chainlog -> '%s'", msg)
+	s.Log().Infof("eventlog -> '%s'", msg)
 	s.vmctx.StoreToEventLog(s.vmctx.CurrentContractHname(), []byte(msg))
 	s.vmctx.EventPublisher().Publish(msg)
 }
