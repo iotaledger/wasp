b8a6fce5135abb72fbed728dc2e9c2c9d9404a35
Diego Essaya <diego.essaya@iota.org>
2021-03-10 11:26:05 -0300

    add nodeconn test for post tx

--- packages/nodeconn/nodeconn_test.go
+++ packages/nodeconn/nodeconn_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/iotaledger/goshimmer/packages/waspconn/utxodb"
 	"github.com/iotaledger/hive.go/crypto/ed25519"
 	"github.com/iotaledger/hive.go/events"
+	"github.com/iotaledger/hive.go/identity"
 	"github.com/iotaledger/hive.go/logger"
 	"github.com/stretchr/testify/require"
 )
@@ -49,7 +50,7 @@ func doAndWaitForResponse(t *testing.T, n *NodeConn, val interface{}, send func(
 			reflect.ValueOf(val).Elem().Set(reflect.ValueOf(msg))
 			close(done)
 		} else {
-			t.Logf("No handler for message: %+v", msg)
+			t.Logf("Received unexpected message: %T (expected %T)", msg, val)
 		}
 	})
 
@@ -89,4 +90,64 @@ func TestRequestOutputs(t *testing.T) {
 		require.EqualValues(t, 1, len(cb))
 		require.EqualValues(t, 1337, cb[ledgerstate.ColorIOTA])
 	}
+}
+
+func TestPostTransaction(t *testing.T) {
+	tangle, n := start(t)
+
+	// transfer 1337 iotas to addr
+	seed := ed25519.NewSeed()
+	addr := ledgerstate.NewED25519Address(seed.KeyPair(0).PublicKey)
+	err := tangle.RequestFunds(addr)
+	require.NoError(t, err)
+
+	// request utxos for addr
+	var addrOutsMsg *waspconn.WaspFromNodeAddressOutputsMsg
+	doAndWaitForResponse(t, n, &addrOutsMsg, func() error {
+		return n.RequestOutputsFromNode(addr)
+	})
+	var outID ledgerstate.OutputID
+	for txID := range addrOutsMsg.Balances {
+		var txMsg *waspconn.WaspFromNodeConfirmedTransactionMsg
+		doAndWaitForResponse(t, n, &txMsg, func() error {
+			return n.RequestConfirmedTransactionFromNode(txID)
+		})
+		for _, out := range txMsg.Tx.Essence().Outputs() {
+			if out.Address().Array() != addr.Array() {
+				continue
+			}
+			require.EqualValues(t, 1337, out.Balances().Map()[ledgerstate.ColorIOTA])
+			outID = out.ID()
+		}
+	}
+	require.NotEqualValues(t, ledgerstate.EmptyOutputID, outID)
+
+	// transfer 1 iota from addr to addr2
+	addr2 := ledgerstate.NewED25519Address(seed.KeyPair(1).PublicKey)
+	pledge, _ := identity.RandomID()
+	txEssence := ledgerstate.NewTransactionEssence(
+		0,
+		time.Now(),
+		pledge,
+		pledge,
+		ledgerstate.NewInputs(ledgerstate.NewUTXOInput(outID)),
+		ledgerstate.NewOutputs(
+			ledgerstate.NewSigLockedSingleOutput(uint64(1), addr2),
+			ledgerstate.NewSigLockedSingleOutput(uint64(1336), addr),
+		),
+	)
+	sig := ledgerstate.NewED25519Signature(seed.KeyPair(0).PublicKey, ed25519.Signature(seed.KeyPair(0).PrivateKey.Sign(txEssence.Bytes())))
+	unlockBlock := ledgerstate.NewSignatureUnlockBlock(sig)
+	tx := ledgerstate.NewTransaction(txEssence, ledgerstate.UnlockBlocks{unlockBlock})
+
+	// post tx
+	err = n.PostTransactionToNode(tx, addr, 0)
+	require.NoError(t, err)
+
+	// request tx
+	var txMsg *waspconn.WaspFromNodeConfirmedTransactionMsg
+	doAndWaitForResponse(t, n, &txMsg, func() error {
+		return n.RequestConfirmedTransactionFromNode(tx.ID())
+	})
+	require.EqualValues(t, txMsg.Tx.ID(), tx.ID())
 }
