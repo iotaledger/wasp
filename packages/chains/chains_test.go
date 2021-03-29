b3dbbabbaa85ceb7f89015340a824a40b7d5e9e3
lunfardo314 <evaldas.drasutis@iota.org>
2021-03-29 14:13:35 +0300

    changed plugin dependencies. Bug fixes. Doc writing

--- packages/chains/chains_test.go
+++ packages/chains/chains_test.go
@@ -1,19 +1,19 @@
 package chains
 
 import (
+	txstream "github.com/iotaledger/goshimmer/packages/txstream/client"
 	"github.com/iotaledger/wasp/packages/testutil/testlogger"
-	"github.com/iotaledger/wasp/plugins/nodeconn"
 	"golang.org/x/xerrors"
 	"net"
 	"testing"
-	"time"
 )
 
 func TestBasic(t *testing.T) {
 	logger := testlogger.NewLogger(t)
-	nconn := nodeconn.New("dummyNetId", logger, func() (addr string, conn net.Conn, err error) {
-		time.Sleep(1 * time.Second)
-		return "127.0.0.1", nil, xerrors.New("dummy error")
+	ch := New(logger)
+
+	nconn := txstream.New("dummyID", logger, func() (addr string, conn net.Conn, err error) {
+		return "", nil, xerrors.New("dummy dial error")
 	})
-	_ = New(logger, nconn)
+	ch.Attach(nconn)
 }
