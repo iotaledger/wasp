de7f98a9a33c971ce267460f19bc873ce62aecf7
Julius Andrikonis <julius.andrikonis@ext.iota.org>
2021-04-09 16:22:47 +0300

    Code changes after merge

--- packages/chain/statemgr/node_conn.go
+++ packages/chain/statemgr/node_conn.go
@@ -10,4 +10,5 @@ import (
 //NodeConn is implemented by packages/txstream/client.Client in goshimmer
 type NodeConn interface {
 	RequestBacklog(addr ledgerstate.Address)
+	RequestConfirmedOutput(addr ledgerstate.Address, outputID ledgerstate.OutputID)
 }
