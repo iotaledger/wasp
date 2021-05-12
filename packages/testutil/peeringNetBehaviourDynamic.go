65cd1748fc6cb2dfea3b04e9b81d17ebac925b54
Julius Andrikonis <julius.andrikonis@ext.iota.org>
2021-05-12 18:13:14 +0300

    Dynamic peering net behaviour

---
+++ packages/testutil/peeringNetBehaviourDynamic.go
@@ -0,0 +1,200 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+package testutil
+
+// An interface providing some network behavior.
+// It is used for testing network protocols in more realistic settings.
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/iotaledger/hive.go/logger"
+)
+
+// peeringNetDynamic provides a behavior of a network with dynamically
+// changeable working conditions. It works as peeringNetReliable without any
+// handlers set.
+type PeeringNetDynamic struct {
+	closeChs []chan bool
+	handlers []peeringNetDynamicHandlerEntry
+	log      *logger.Logger
+}
+
+type peeringNetDynamicHandlerEntry struct {
+	id *string
+	peeringNetDynamicHandler
+}
+
+type peeringNetDynamicHandler interface {
+	handleSendMessage(
+		msg *peeringMsg,
+		dstNetID string,
+		nextHandlers []peeringNetDynamicHandlerEntry,
+		callHandlersAndSendFun func(nextHandlers []peeringNetDynamicHandlerEntry),
+		log *logger.Logger,
+	)
+}
+
+// NewPeeringNetDynamic constructs the PeeringNetBehavior.
+func NewPeeringNetDynamic(log *logger.Logger) *PeeringNetDynamic {
+	return &PeeringNetDynamic{
+		closeChs: make([]chan bool, 0),
+		handlers: make([]peeringNetDynamicHandlerEntry, 0),
+		log:      log,
+	}
+}
+
+func (pndT *PeeringNetDynamic) WithLosingChannel(id *string, deliveryProbability int) *PeeringNetDynamic {
+	pndT.addHandlerEntry(peeringNetDynamicHandlerEntry{
+		id,
+		&peeringNetDynamicHandlerLosingChannel{
+			probability: deliveryProbability,
+		},
+	})
+	return pndT
+}
+
+func (pndT *PeeringNetDynamic) WithRepeatingChannel(id *string, repeatProbability int) *PeeringNetDynamic {
+	pndT.addHandlerEntry(peeringNetDynamicHandlerEntry{
+		id,
+		&peeringNetDynamicHandlerRepeatingChannel{
+			probability: repeatProbability,
+		},
+	})
+	return pndT
+}
+
+func (pndT *PeeringNetDynamic) WithDelayingChannel(id *string, delayFrom time.Duration, delayTill time.Duration) *PeeringNetDynamic {
+	pndT.addHandlerEntry(peeringNetDynamicHandlerEntry{
+		id,
+		&peeringNetDynamicHandlerDelayingChannel{
+			from: delayFrom,
+			till: delayTill,
+		},
+	})
+	return pndT
+}
+
+func (pndT *PeeringNetDynamic) addHandlerEntry(handler peeringNetDynamicHandlerEntry) {
+	pndT.handlers = append(pndT.handlers, handler)
+}
+
+func (pndT *PeeringNetDynamic) RemoveHandler(id string) bool {
+	var i int
+	for i = 0; i < len(pndT.handlers); i++ {
+		if *(pndT.handlers[i].getID()) == id {
+			pndT.handlers = append(pndT.handlers[:i], pndT.handlers[i+1:]...)
+			return true
+		}
+	}
+	return false
+}
+
+// Run implements PeeringNetBehavior.
+func (pndT *PeeringNetDynamic) AddLink(inCh, outCh chan *peeringMsg, dstNetID string) {
+	closeCh := make(chan bool)
+	pndT.closeChs = append(pndT.closeChs, closeCh)
+	go pndT.recvLoop(inCh, outCh, closeCh, dstNetID)
+}
+
+// Close implements PeeringNetBehavior.
+func (pndT *PeeringNetDynamic) Close() {
+	for i := range pndT.closeChs {
+		close(pndT.closeChs[i])
+	}
+}
+
+func (pndT *PeeringNetDynamic) recvLoop(inCh, outCh chan *peeringMsg, closeCh chan bool, dstNetID string) {
+	for {
+		select {
+		case <-closeCh:
+			return
+		case recv, ok := <-inCh:
+			if !ok {
+				return
+			}
+			var callHandlersAndSendFun func(nextHandlers []peeringNetDynamicHandlerEntry)
+			callHandlersAndSendFun = func(nextHandlers []peeringNetDynamicHandlerEntry) {
+				if len(nextHandlers) > 0 {
+					nextHandlers[0].handleSendMessage(recv, dstNetID, nextHandlers[1:], callHandlersAndSendFun, pndT.log)
+				} else {
+					pndT.log.Debugf("Network delivers message %v -%v-> %v", recv.from.netID, recv.msg.MsgType, dstNetID)
+					outCh <- recv
+				}
+			}
+			callHandlersAndSendFun(pndT.handlers)
+		}
+	}
+}
+
+func (pndheT *peeringNetDynamicHandlerEntry) getID() *string {
+	return pndheT.id
+}
+
+type peeringNetDynamicHandlerLosingChannel struct {
+	probability int // probability to deliver a message (in percents)
+}
+
+func (lcT *peeringNetDynamicHandlerLosingChannel) handleSendMessage(
+	msg *peeringMsg,
+	dstNetID string,
+	nextHandlers []peeringNetDynamicHandlerEntry,
+	callHandlersAndSendFun func(nextHandlers []peeringNetDynamicHandlerEntry),
+	log *logger.Logger,
+) {
+	if rand.Intn(100) > lcT.probability {
+		log.Debugf("Network dropped message %v -%v-> %v", msg.from.netID, msg.msg.MsgType, dstNetID)
+		return
+	}
+	callHandlersAndSendFun(nextHandlers)
+}
+
+type peeringNetDynamicHandlerRepeatingChannel struct {
+	probability int // Probability to repeat a message (in percents), 0 meaning no repeat, 100 a certain repeat, 250 - 50% that message will be sent out three times and 50% - that four times
+}
+
+func (rcT *peeringNetDynamicHandlerRepeatingChannel) handleSendMessage(
+	msg *peeringMsg,
+	dstNetID string,
+	nextHandlers []peeringNetDynamicHandlerEntry,
+	callHandlersAndSendFun func(nextHandlers []peeringNetDynamicHandlerEntry),
+	log *logger.Logger,
+) {
+	numRepeat := 1 + rcT.probability/100
+	if rand.Intn(100) < rcT.probability%100 {
+		numRepeat++
+	}
+	for i := 0; i < numRepeat; i++ {
+		callHandlersAndSendFun(nextHandlers)
+	}
+}
+
+type peeringNetDynamicHandlerDelayingChannel struct {
+	from time.Duration
+	till time.Duration
+}
+
+func (dcT *peeringNetDynamicHandlerDelayingChannel) handleSendMessage(
+	msg *peeringMsg,
+	dstNetID string,
+	nextHandlers []peeringNetDynamicHandlerEntry,
+	callHandlersAndSendFun func(nextHandlers []peeringNetDynamicHandlerEntry),
+	log *logger.Logger,
+) {
+	go func() {
+		fromMS := int(dcT.from.Milliseconds())
+		tillMS := int(dcT.till.Milliseconds())
+		var delay time.Duration
+		if tillMS > 0 {
+			if fromMS < tillMS {
+				delay = time.Duration(rand.Intn(tillMS-fromMS)+fromMS) * time.Millisecond
+			} else {
+				delay = time.Duration(fromMS) * time.Millisecond
+			}
+			<-time.After(delay)
+		}
+		callHandlersAndSendFun(nextHandlers)
+	}()
+}
---
+++ packages/testutil/peeringNetBehaviourDynamic_test.go
@@ -0,0 +1,138 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+package testutil // not `..._test` because it uses peeringMsg.
+
+import (
+	"github.com/iotaledger/wasp/packages/testutil/testlogger"
+	"testing"
+	"time"
+
+	"github.com/iotaledger/hive.go/logger"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPeeringNetDynamicReliable(t *testing.T) {
+	inCh := make(chan *peeringMsg)
+	outCh := make(chan *peeringMsg)
+	doneCh := make(chan bool)
+	go func() {
+		for i := 0; i < 10; i++ {
+			<-outCh
+		}
+		doneCh <- true
+	}()
+	var someNode = peeringNode{netID: "src"}
+	var behavior PeeringNetBehavior
+	behavior = NewPeeringNetDynamic(testlogger.WithLevel(testlogger.NewLogger(t), logger.LevelError, false))
+	behavior.AddLink(inCh, outCh, "dst")
+	for i := 0; i < 10; i++ {
+		inCh <- &peeringMsg{from: &someNode}
+	}
+	<-doneCh
+	behavior.Close()
+}
+
+func TestPeeringNetDynamicUnreliable(t *testing.T) {
+	t.SkipNow()
+
+	inCh := make(chan *peeringMsg)
+	outCh := make(chan *peeringMsg)
+	//
+	// Receiver process.
+	stopCh := make(chan bool)
+	startTime := time.Now()
+	durations := make([]time.Duration, 0)
+	go func() {
+		for {
+			select {
+			case <-stopCh:
+				return
+			case <-outCh:
+				durations = append(durations, time.Since(startTime))
+			}
+		}
+	}()
+	//
+	// Run the test.
+	var someNode = peeringNode{netID: "src"}
+	var behavior PeeringNetBehavior
+	behavior = NewPeeringNetDynamic(testlogger.WithLevel(testlogger.NewLogger(t), logger.LevelError, false)).
+		WithLosingChannel(nil, 50).
+		WithRepeatingChannel(nil, 50).
+		WithDelayingChannel(nil, 50*time.Millisecond, 100*time.Millisecond)
+	behavior.AddLink(inCh, outCh, "dst")
+	for i := 0; i < 1000; i++ {
+		inCh <- &peeringMsg{from: &someNode}
+	}
+	time.Sleep(500 * time.Millisecond)
+	//
+	// Verify the results (with some tolerance for randomness).
+	{ // 50% of messages dropped + 50% duplicated -> delivered ~75%
+		require.Greater(t, len(durations), 500)
+		require.Less(t, len(durations), 900)
+	}
+	{ // Average should be between the specified boundaries.
+		var avgDuration int64 = 0
+		for _, d := range durations {
+			avgDuration += d.Milliseconds()
+		}
+		avgDuration = avgDuration / int64(len(durations))
+		require.Greater(t, avgDuration, int64(50))
+		require.Less(t, avgDuration, int64(100))
+	}
+	//
+	// Stop the test.
+	stopCh <- true
+	behavior.Close()
+}
+
+func TestPeeringNetDynamicGoodQuality(t *testing.T) {
+	inCh := make(chan *peeringMsg)
+	outCh := make(chan *peeringMsg)
+	//
+	// Receiver process.
+	stopCh := make(chan bool)
+	startTime := time.Now()
+	durations := make([]time.Duration, 0)
+	go func() {
+		for {
+			select {
+			case <-stopCh:
+				return
+			case <-outCh:
+				durations = append(durations, time.Since(startTime))
+			}
+		}
+	}()
+	//
+	// Run the test.
+	var someNode = peeringNode{netID: "src"}
+	var behavior PeeringNetBehavior
+	behavior = NewPeeringNetDynamic(testlogger.WithLevel(testlogger.NewLogger(t), logger.LevelError, false)).
+		WithLosingChannel(nil, 100).
+		WithRepeatingChannel(nil, 0).
+		WithDelayingChannel(nil, 0*time.Millisecond, 0*time.Millisecond)
+	behavior.AddLink(inCh, outCh, "dst")
+	for i := 0; i < 1000; i++ {
+		inCh <- &peeringMsg{from: &someNode}
+	}
+	time.Sleep(500 * time.Millisecond)
+	//
+	// Verify the results (with some tolerance for randomness).
+	{ // All messages should be delivered.
+		require.Equal(t, 1000, len(durations))
+	}
+	{ // Average should be small enough.
+		var avgDuration int64 = 0
+		for _, d := range durations {
+			avgDuration += d.Milliseconds()
+		}
+		avgDuration = avgDuration / int64(len(durations))
+		require.Less(t, avgDuration, int64(100))
+	}
+	//
+	// Stop the test.
+	stopCh <- true
+	behavior.Close()
+}
