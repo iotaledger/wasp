97e4f028cb01b800a9614457d00d4436092dcbd4
Jorge Silva <jorgesilva.me@gmail.com>
2021-07-01 15:19:48 +0100

    minor improvement

--- tools/cluster/tests/advanced_inccounter_test.go
+++ tools/cluster/tests/advanced_inccounter_test.go
@@ -147,7 +147,7 @@ func TestRotation(t *testing.T) {
 		require.NoError(t, err)
 	}
 
-	waitUntil(t, createCheckCounterFn(chain1, int64(2*numRequests)), []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 5*time.Second)
+	waitUntil(t, createCheckCounterFn(chain1, int64(2*numRequests)), clu1.Config.AllNodes(), 5*time.Second)
 }
 
 func TestRotationMany(t *testing.T) {
