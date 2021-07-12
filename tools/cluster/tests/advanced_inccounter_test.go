933d8e09d0ed82635ea69a1aaab6cc982953af5e
lunfardo314 <evaldas.drasutis@iota.org>
2021-07-12 12:15:26 +0300

    test access nodes with offledger

--- tools/cluster/tests/advanced_inccounter_test.go
+++ tools/cluster/tests/advanced_inccounter_test.go
@@ -126,7 +126,7 @@ func TestAccessNodesOffLedger(t *testing.T) {
 	t.Run("cluster=4, N=4, req=8", func(t *testing.T) {
 		const numRequests = 8
 		const numValidatorNodes = 4
-		const clusterSize = 5
+		const clusterSize = 4
 		testAccessNodesOffLedger(t, numRequests, numValidatorNodes, clusterSize)
 	})
 	//t.Run("cluster=10, N=4, req=100", func(t *testing.T) {
@@ -175,7 +175,7 @@ func testAccessNodesOffLedger(t *testing.T, numRequests, numValidatorNodes, clus
 		require.NoError(t, err)
 	}
 
-	waitUntil(t, counterEquals(chain1, int64(numRequests)), sliceN(clusterSize), 10*time.Second)
+	waitUntil(t, counterEquals(chain1, int64(numRequests)), sliceN(clusterSize), 60*time.Second)
 
 	printBlocks(t, chain1, numRequests+4)
 }
