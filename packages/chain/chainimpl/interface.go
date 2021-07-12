938779cc5c3fff20df36944a90afa5a4123b1b12
lunfardo314 <evaldas.drasutis@iota.org>
2021-07-12 20:42:58 +0300

    tracing

--- packages/chain/chainimpl/interface.go
+++ packages/chain/chainimpl/interface.go
@@ -152,8 +152,10 @@ func (c *chainObj) ReceiveOffLedgerRequest(req *request.RequestOffLedger, sender
 	c.log.Debugf("ReceiveOffLedgerRequest: reqID: %s, peerID: %s", req.ID(), senderNetID)
 	c.sendRequestAckowledgementMsg(req.ID(), senderNetID)
 	if !c.mempool.ReceiveRequest(req) {
+		c.log.Infof("SEEN ReceiveOffLedgerRequest: reqID: %s, peerID: %s", req.ID(), senderNetID)
 		return
 	}
+	c.log.Infof("NEW ReceiveOffLedgerRequest: reqID: %s, peerID: %s", req.ID(), senderNetID)
 	c.broadcastOffLedgerRequest(req)
 }
 
