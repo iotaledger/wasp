52bcf7129102d1f1514da6715d6e18cfa6f3d0a3
lucastortora <lucas.tortora@iota.org>
2021-07-22 11:01:22 +0200

    * REVERT - Altered config to force local deploy_docs_to_gh-pages.yml

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'IOTA Smart Contract Protocol',
   tagline: 'IOTA smart contract protocol.',
   url: 'https://iscp.docs.iota.org', 
-  baseUrl: '/wasp/',
+  baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'throw',
   favicon: '/img/logo/favicon.ico',
