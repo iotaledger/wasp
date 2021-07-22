845e424e3b676862bacd2e1ba3889f8edf9fad27
Jorge Silva <jorgesilva.me@gmail.com>
2021-07-22 11:50:14 +0100

    cleanup

--- docOps/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/jorge/dev/iota/wasp/docOps/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/jorge/dev/iota/wasp/docOps/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/jorge/dev/iota/wasp/docOps/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/jorge/dev/iota/wasp/docOps/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jorge/dev/iota/wasp/docOps/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jorge/dev/iota/wasp/docOps/src/css/iota.css"),
-];
