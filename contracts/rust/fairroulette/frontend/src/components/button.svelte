1d4934fcfefe3fe27e4915a23e0237f9c2d52de5
evavirseda <evirseda@boxfish.studio>
2021-09-17 17:11:45 +0200

    Refine css button

--- contracts/rust/fairroulette/frontend/src/components/button.svelte
+++ contracts/rust/fairroulette/frontend/src/components/button.svelte
@@ -7,6 +7,21 @@
 
 <style lang="scss">
   button {
-    background: turquoise;
+    background: #14cabf;
+    border: 1px solid #14cabf;
+    border-radius: 6px;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 120%;
+    letter-spacing: 0.16em;
+    text-transform: uppercase;
+    color: #ffffff;
+    padding: 12px;
+    @media (min-width: 1024px) {
+      padding-top: 12px;
+      padding-bottom: 12px;
+      padding-left: 31px;
+      padding-right: 31px;
+    }
   }
 </style>
