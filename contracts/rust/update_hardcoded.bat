99813a2b29e5cf884d651443c08f69abc71a46f2
Eric Hop <eric@iota.org>
2021-09-20 14:28:26 -0700

    Added missing wasm file

--- contracts/rust/update_hardcoded.bat
+++ contracts/rust/update_hardcoded.bat
@@ -1,5 +1,6 @@
 @echo off
 for /d %%f in (*.) do if exist %%f\pkg\%%f*.wasm copy /y %%f\pkg\%%f*.wasm %%f\test\*.*
+if exist erc20\pkg\erc20_bg.wasm copy /y erc20\pkg\erc20_bg.wasm ..\..\packages\vm\core\testcore\sbtests\sbtestsc\*.*
 if exist testcore\pkg\testcore_bg.wasm copy /y testcore\pkg\testcore_bg.wasm ..\..\packages\vm\core\testcore\sbtests\sbtestsc\*.*
 if exist inccounter\pkg\inccounter_bg.wasm copy /y inccounter\pkg\inccounter_bg.wasm ..\..\tools\cluster\tests\wasm\*.*
 
