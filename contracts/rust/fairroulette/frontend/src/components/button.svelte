327e4c88cf7c8d78d4291942f9cadb28f0426254
evavirseda <evirseda@boxfish.studio>
2021-09-20 16:25:54 +0200

    Add css button

--- contracts/rust/fairroulette/frontend/src/components/button.svelte
+++ contracts/rust/fairroulette/frontend/src/components/button.svelte
@@ -8,21 +8,25 @@
 
 <style lang="scss">
   button {
-    background: #14cabf;
-    border: 1px solid #14cabf;
+    background: var(--mint-green-light);
+    border: 1px solid var(--mint-green-light);
     border-radius: 6px;
     font-weight: bold;
     font-size: 14px;
     line-height: 120%;
     letter-spacing: 0.16em;
     text-transform: uppercase;
-    color: #ffffff;
+    color: var(--white);
     padding: 12px;
     @media (min-width: 1024px) {
       padding-top: 12px;
       padding-bottom: 12px;
       padding-left: 31px;
       padding-right: 31px;
+    }
+    .disabled {
+      background: transparent;
+      color: var(--gray-5);
     }
   }
 </style>
