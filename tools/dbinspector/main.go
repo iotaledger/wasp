9ed0f387f5bcc4ac5b2357df1de476bf5a04c91e
Jorge Silva <jorgesilva.me@gmail.com>
2021-09-20 11:17:39 +0100

    remove big array log

--- tools/dbinspector/main.go
+++ tools/dbinspector/main.go
@@ -10,8 +10,23 @@ import (
 
 	"github.com/iotaledger/goshimmer/packages/database"
 	"github.com/iotaledger/hive.go/kvstore"
+	"github.com/iotaledger/wasp/packages/database/dbkeys"
 	"github.com/iotaledger/wasp/packages/iscp"
 )
+
+var dbKeysNames = map[byte]string{
+	dbkeys.ObjectTypeDBSchemaVersion:    "Schema Version",
+	dbkeys.ObjectTypeChainRecord:        "Chain Record",
+	dbkeys.ObjectTypeCommitteeRecord:    "Committee Record",
+	dbkeys.ObjectTypeDistributedKeyData: "Distributed Key Data",
+	dbkeys.ObjectTypeStateHash:          "State Hash",
+	dbkeys.ObjectTypeBlock:              "Block",
+	dbkeys.ObjectTypeStateVariable:      "State Variable",
+	dbkeys.ObjectTypeNodeIdentity:       "Node Identity",
+	dbkeys.ObjectTypeBlobCache:          "BlobCache",
+	dbkeys.ObjectTypeBlobCacheTTL:       "BlobCacheTTL",
+	dbkeys.ObjectTypeTrustedPeer:        "TrustedPeer",
+}
 
 // TODO should be a cli param
 const dbpath = "/tmp/wasp-cluster/wasp0/waspdb"
@@ -29,11 +44,16 @@ func printDbEntries(dbDir fs.DirEntry) {
 
 	fmt.Printf("\n\n------------------ %s ------------------\n", dbDir.Name())
 	accLen := 0
+
 	hnameUsedSpace := make(map[iscp.Hname]int)
 	hnameCount := make(map[iscp.Hname]int)
+
+	dbKeysUsedSpace := make(map[byte]int)
+
 	err = store.Iterate(kvstore.EmptyPrefix, func(k kvstore.Key, v []byte) bool {
 		usedSpace := len(k) + len(v)
 		accLen += usedSpace
+		dbKeysUsedSpace[k[0]] += usedSpace
 		if len(k) >= 5 {
 			hn, err := iscp.HnameFromBytes(k[1:5])
 			if err == nil {
@@ -47,8 +67,14 @@ func printDbEntries(dbDir fs.DirEntry) {
 	})
 
 	fmt.Printf("\n\n Total DB size: %d\n\n", accLen)
+
 	for hn, space := range hnameUsedSpace {
 		fmt.Printf("Hname: %s, %d entries, size: %d\n", hn, hnameCount[hn], space)
+	}
+
+	fmt.Printf("\n\n DB Usage per dbKeys: \n\n")
+	for dbkey, space := range dbKeysUsedSpace {
+		fmt.Printf("KEY: %s, space: %d\n", dbKeysNames[dbkey], space)
 	}
 
 	if err != nil {
