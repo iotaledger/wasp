09b785fca4d78e7f69bdd8d576218823c7e5c84b
evavirseda <evirseda@boxfish.studio>
2021-09-20 09:56:40 +0200

    Add css to message

--- contracts/rust/fairroulette/frontend/src/components/message.svelte
+++ contracts/rust/fairroulette/frontend/src/components/message.svelte
@@ -5,19 +5,44 @@
 </script>
 
 <div class="message">
-  <div>{title}</div>
+  <h1 class="title">{title}</h1>
   {#if subtitle}
-    <div>{subtitle}</div>
+    <span class="subtitle">{subtitle}</span>
   {/if}
   {#if description}
-    <div>{description}</div>
+    <span class="description">{description}</span>
   {/if}
   <div />
 </div>
 
 <style lang="scss">
   .message {
-    background-color: white;
+    .title {
+      font-weight: 600;
+      font-size: 32px;
+      line-height: 36px;
+      text-align: center;
+      letter-spacing: 0.75px;
+      color: #ffffff;
+      text-align: center;
+      @media (min-width: 1024px) {
+        font-size: 48px;
+      }
+    }
+    .subtitle {
+      font-weight: 600;
+      font-size: 24px;
+      line-height: 120%;
+      text-align: center;
+      letter-spacing: 0.02em;
+      color: #00e0ca;
+    }
+    .description {
+    }
+    font-size: 16px;
+    line-height: 150%;
     text-align: center;
+    letter-spacing: 0.75px;
+    color: #909fbe;
   }
 </style>
