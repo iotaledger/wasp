88c9e527f7022bbb6a1c275fe5b75efb10cbbba9
evavirseda <evirseda@boxfish.studio>
2021-09-22 15:01:47 +0200

    css:Refine players panel

--- contracts/rust/fairroulette/frontend/src/components/panels/players.svelte
+++ contracts/rust/fairroulette/frontend/src/components/panels/players.svelte
@@ -14,13 +14,16 @@
 
 <div class="panel">
   <h3>Players</h3>
-  <div class="players-wraper">
+  <div class="players-wrapper">
     {#each $players as { address, bet }, index}
       <div class="player">
         <div class="player-index">{index + 1}</div>
         <div class="player-content">
           <div class="player-address">{address}</div>
-          <div class="player-bet">Bet: {bet}</div>
+          <div class="player-bet">
+            <span>Bet:</span>
+            <span class="bet-value">{bet}</span>
+          </div>
         </div>
       </div>
     {/each}
@@ -41,6 +44,12 @@
       padding-bottom: 14px;
       margin: 0;
     }
+    .players-wrapper {
+      margin-top: 10px;
+    }
+    .player {
+      display: inline-flex;
+    }
     .player-index {
       font-size: 14px;
       line-height: 150%;
@@ -49,27 +58,6 @@
       margin-right: 16px;
       display: inline-block;
     }
-    .tag {
-      display: flex;
-      justify-content: space-between;
-      width: 100%;
-      margin-bottom: 6px;
-    }
-    .details-tag {
-      display: inline-flex;
-      margin-top: 12px;
-    }
-    .item-tag {
-      font-weight: bold;
-      font-size: 12px;
-      line-height: 150%;
-      background: rgba(0, 224, 202, 0.2);
-      border-radius: 6px;
-      letter-spacing: 0.5px;
-      color: var(--mint-green-dark);
-      padding: 2px 6px;
-      height: min-content;
-    }
     .player-address {
       font-weight: bold;
       font-size: 14px;
@@ -77,39 +65,15 @@
       letter-spacing: 0.5px;
       color: var(--gray-3);
     }
-    .tag-description {
-      display: inline-flex;
-    }
-    .item-description {
-      display: block;
-      margin: 0 0 16px 0;
-      font-size: 14px;
-      line-height: 150%;
-      letter-spacing: 0.5px;
-      color: var(--gray-5);
-      @media (min-width: 1024px) {
-        display: inline-flex;
-      }
-      .description-value {
-        margin-left: 0;
-      }
-    }
-    .description-label {
+    .player-bet {
       font-weight: bold;
       font-size: 14px;
       line-height: 150%;
       letter-spacing: 0.5px;
       color: var(--gray-5);
-      margin-left: 26px;
-    }
-    .description-value {
-      margin-left: 26px;
-      font-size: 14px;
-      line-height: 150%;
-      letter-spacing: 0.5px;
-      color: var(--gray-5);
-      @media (min-width: 1024px) {
-        display: inline-flex;
+      .bet-value {
+        font-weight: normal;
+        margin-right: 16px;
       }
     }
   }
