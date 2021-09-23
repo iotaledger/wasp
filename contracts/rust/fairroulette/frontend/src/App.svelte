a30326e6b8310ef0c30b1145df76cd5b70e49ff9
evavirseda <evirseda@boxfish.studio>
2021-09-23 17:18:34 +0200

    Add footer

--- contracts/rust/fairroulette/frontend/src/App.svelte
+++ contracts/rust/fairroulette/frontend/src/App.svelte
@@ -2,9 +2,22 @@
   import { Router } from 'svelte-router-spa';
   import Header from './components/header.svelte';
   import { routes } from './routes';
+  import Footer from './components/footer.svelte';
+
+  const FOUNDATION_DATA_URL = 'https://webassets.iota.org/data/foundation.json';
+
+  async function getFoundationData() {
+    let response = await fetch(FOUNDATION_DATA_URL);
+    let users = await response.json();
+    return users;
+  }
+  const promise = getFoundationData();
 </script>
 
 <main>
   <Header />
   <Router {routes} />
+  {#await promise then foundationData}
+    <Footer {foundationData} />
+  {/await}
 </main>
