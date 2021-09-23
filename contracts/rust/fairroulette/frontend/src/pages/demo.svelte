313ff1f06423151f513d872068a5abd2574cdce5
evavirseda <evirseda@boxfish.studio>
2021-09-23 17:21:33 +0200

    refine css demo

--- contracts/rust/fairroulette/frontend/src/pages/demo.svelte
+++ contracts/rust/fairroulette/frontend/src/pages/demo.svelte
@@ -46,86 +46,77 @@
 </div>
 
 <style lang="scss">
-  .container {
-    max-width: 1200px;
-    margin: 0 auto;
+  .layout_state {
+    display: flex;
+    flex-direction: column;
+    position: relative;
     @media (min-width: 1024px) {
-      padding: 0 24px;
+      flex-direction: row-reverse;
+      justify-content: space-between;
+      margin-top: 48px;
     }
-    @media (min-width: 2000px) {
-      max-width: 1600px;
-    }
-    .layout_state {
-      display: flex;
-      flex-direction: column;
-      position: relative;
+    .wallet {
       @media (min-width: 1024px) {
-        flex-direction: row-reverse;
-        justify-content: space-between;
-        margin-top: 48px;
-      }
-      .wallet {
-        @media (min-width: 1024px) {
-          width: 25%;
-        }
-      }
-      .roulette_state {
-        margin-top: 40px;
-        @media (min-width: 1024px) {
-          margin-top: 0;
-          position: absolute;
-          left: 50%;
-          transform: translateX(-50%);
-        }
-      }
-      .balance {
-        @media (min-width: 1024px) {
-          width: 25%;
-        }
+        width: 25%;
       }
     }
-    .layout_roulette {
-      display: flex;
-      flex-direction: column;
-      position: relative;
+    .roulette_state {
+      margin-top: 40px;
       @media (min-width: 1024px) {
-        flex-direction: row;
-        justify-content: space-between;
-        margin-top: 32px;
+        margin-top: 0;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
       }
-      .players {
-        height: calc(100vh - 650px);
-        position: relative;
-        min-height: 400px;
+    }
+    .balance {
+      @media (min-width: 1024px) {
+        width: 25%;
+      }
+    }
+  }
+  .layout_roulette {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    @media (min-width: 1024px) {
+      flex-direction: row;
+      justify-content: space-between;
+      margin-top: 32px;
+      margin-bottom: 300px;
+    }
+    .players {
+      height: calc(100vh - 650px);
+      position: relative;
+      min-height: 400px;
 
-        @media (min-width: 1024px) {
-          width: 25%;
-          height: calc(100vh - 450px);
-        }
+      @media (min-width: 1024px) {
+        width: 25%;
+        height: calc(100vh - 450px);
       }
-      .roulette_game {
-        max-width: max-content;
-        margin: 0 auto;
-        @media (min-width: 1024px) {
-          position: absolute;
-          left: 50%;
-          transform: translateX(-50%);
-        }
-        .bet_system {
-          margin-top: 40px;
-          margin-bottom: 100px;
-        }
+    }
+    .roulette_game {
+      max-width: max-content;
+      margin: 0 auto;
+      @media (min-width: 1024px) {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
       }
-      .logs {
-        height: calc(100vh - 650px);
-        position: relative;
-        min-height: 400px;
-        margin-bottom: 132px;
-        @media (min-width: 1024px) {
-          margin-bottom: 0;
-          width: 25%;
-          height: calc(100vh - 450px);
-        }
+      .bet_system {
+        margin-top: 40px;
+        margin-bottom: 100px;
+      }
+    }
+    .logs {
+      height: calc(100vh - 650px);
+      position: relative;
+      min-height: 400px;
+      margin-bottom: 132px;
+      @media (min-width: 1024px) {
+        margin-bottom: 0;
+        width: 25%;
+        height: calc(100vh - 450px);
       }
     }
   }
