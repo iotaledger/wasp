f3c7d44e5926073e71d26afafcbbc9115a42b516
Ruben Alvarez <ralvarez@boxfish.studio>
2021-09-28 15:56:40 +0200

    Feat: add text box to selector bar label

--- contracts/rust/fairroulette/frontend/src/components/selectors/bar.svelte
+++ contracts/rust/fairroulette/frontend/src/components/selectors/bar.svelte
@@ -3,10 +3,42 @@
 
   export let value: number;
   export let disabled: boolean = false;
+
+  let invalidMessage, textValue;
+
+  $: value, updateLabel();
+
+  function updateLabel() {
+    textValue = value;
+    console.log('Text value: ', textValue);
+  }
+
+  $: textValue, validate();
+
+  $: console.log('value', value);
+
+  function validate(): void {
+    invalidMessage = undefined;
+    console.log('TextValue: ', textValue, 'typeof value', typeof value);
+    if (Number(textValue) < 0 || Number(textValue) > $balance) {
+      invalidMessage = `Value must be less than or equal to ${$balance}.`;
+    } else if (Number(textValue) % 1 !== 0) {
+      invalidMessage = 'Value must be an integer.';
+    } else {
+      console.log('Validado: ', textValue);
+
+      value = textValue;
+    }
+  }
 </script>
 
 <div>
-  <div class="value">{value}{' '}i</div>
+  <div class="value">
+    <input bind:value={textValue} on:keydown={() => validate()} />{' '}i
+  </div>
+  {#if invalidMessage}
+    <div class="invalid-message">{invalidMessage}</div>
+  {/if}
   <div class="bar-selector">
     <input
       bind:value
@@ -26,6 +58,11 @@
     line-height: 150%;
     letter-spacing: 0.5px;
     color: var(--white);
+  }
+  .invalid-message {
+    font-size: 11px;
+    color: tomato;
+    margin-top: 8px;
   }
   .bar-selector {
     position: relative;
@@ -53,18 +90,6 @@
         background: var(--mint-green-light);
         -webkit-appearance: none;
         margin-top: -8px;
-      }
-      &::-moz-range-track {
-        height: 8px;
-        border-radius: 4px;
-        background: var(--gray-7);
-      }
-      &::-moz-range-thumb {
-        border: 0px solid var(--mint-green-light);
-        background: var(--mint-green-light);
-        height: 20px;
-        width: 20px;
-        border-radius: 50px;
       }
     }
   }
