e3b8124816c3b4d32407c8bbf184f6c4121fa7bf
Julius Andrikonis <julius.andrikonis@ext.iota.org>
2021-10-15 16:00:55 +0300

    Documentation

--- packages/util/pipe/interface.go
+++ packages/util/pipe/interface.go
@@ -4,10 +4,34 @@ package pipe
 const minQueueLen = 16
 
 type Hashable interface {
-	// For requirements of this function see https://docs.oracle.com/javase/8/docs/api/java/lang/Object.html#hashCode--
-	// Additional requirement: the returned value must be valid as a map key
+	// GetHash should return a hash code value for the object. This method is supported
+	// for the benefit of hash tables such as those provided by HashSet struct.
+	// The general contract of GetHash is:
+	// 	  * Whenever it is invoked on the same object more than once during an execution
+	// 		of Wasp, the GetHash method must consistently return the same struct,
+	//		provided no information used in Equals comparisons on the object is
+	//		modified. This struct need not remain consistent from one execution
+	//		of Wasp to another.
+	//	  * If two objects are equal according to the Equals method, then calling
+	//		the GetHash method on each of the two objects must produce the same struct.
+	//	  * It is not required that if two objects are unequal according to the
+	//		Equals method, then calling the GetHash method on each of the two objects
+	//		must produce distinct struct. However, the programmer should be aware
+	//		that producing distinct struct for unequal objects may improve the performance
+	//		of hash tables.
+	//	  *	the returned struct must be valid as a map key (e.g. no slices)
 	GetHash() interface{}
-	// For requirements of this function see https://docs.oracle.com/javase/8/docs/api/java/lang/Object.html#equals-java.lang.Object-
+	// Equals should indicate whether some other object is "equal to" this one.
+	// The equals method implements an equivalence relation on non-null objects:
+	//	  * It is reflexive: for any non-null value x, x.Equals(x) should return true.
+	//	  * It is symmetric: for any non-null values x and y, x.Equals(y) should
+	//		return true if and only if y.Equals(x) returns true.
+	//	  * It is transitive: for any non-null values x, y, and z, if x.Equals(y)
+	//		returns true and y.Equals(z) returns true, then x.Equals(z) should return true.
+	//	  * It is consistent: for any non-null values x and y, multiple invocations
+	//		of x.Equals(y) consistently return true or consistently return false,
+	//		provided no information used in equals comparisons on the objects is modified.
+	//    * For any non-null value x, x.Equals(null) should return false.
 	Equals(elem interface{}) bool
 }
 
