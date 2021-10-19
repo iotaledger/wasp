d3b94873a8167c4f93ecf0ecfb16bbfbe6fbe5f8
Begoña Alvarez <balvarez@boxfish.studio>
2021-10-19 15:59:37 +0200

    Add missing cookie disclaimer component

---
+++ contracts/rust/fairroulette/frontend/src/components/cookie_disclaimer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let allowCookies: () => void;
+  export let declineCookies: () => void;
+</script>
+
+<div class="cookie-disclaimer">
+  <button on:click={declineCookies}>Decline cookies</button>
+  <button on:click={allowCookies}>Allow cookies</button>
+</div>
+
+<style lang="scss">
+  .cookie-disclaimer {
+    padding: 20px;
+    background: turquoise;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+  }
+</style>
