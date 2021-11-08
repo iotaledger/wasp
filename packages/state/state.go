c8939f7c332d005b25763e6f96e17dea6d6ff784
Julius Andrikonis <julius.andrikonis@ext.iota.org>
2021-11-08 12:50:31 +0200

    Code style change in ApplyBlock

--- packages/state/state.go
+++ packages/state/state.go
@@ -48,10 +48,12 @@ func newVirtualState(db kvstore.KVStore, chainID *iscp.ChainID) *virtualStateAcc
 
 func newZeroVirtualState(db kvstore.KVStore, chainID *iscp.ChainID) (VirtualStateAccess, Block) {
 	ret := newVirtualState(db, chainID)
-	originBlock, err := ret.applyOriginBlock()
-	if err != nil {
-		panic(err)
-	}
+	originBlock := newOriginBlock()
+
+	// Applying origin block to empty virtual state
+	ret.ApplyStateUpdates(originBlock.(*blockImpl).stateUpdate)
+	ret.appliedBlockHashes = []hashing.HashValue{hashing.HashData(originBlock.EssenceBytes())}
+
 	_, _ = ret.ExtractBlock() // clear the update log
 	return ret, originBlock
 }
@@ -129,24 +131,13 @@ func (vs *virtualStateAccess) PreviousStateHash() hashing.HashValue {
 }
 
 // ApplyBlock applies a block of state updates. Checks consistency of the block and previous state. Updates state hash
+// It is not suitible for applying origin block to empty virtual state. This is done in `newZeroVirtualState`
 func (vs *virtualStateAccess) ApplyBlock(b Block) error {
-	return vs.applyAnyBlock(b, false)
-}
-
-func (vs *virtualStateAccess) applyOriginBlock() (Block, error) {
-	originBlock := newOriginBlock()
-	return originBlock, vs.applyAnyBlock(originBlock, true)
-}
-
-func (vs *virtualStateAccess) applyAnyBlock(b Block, empty bool) error {
-	if empty && b.BlockIndex() != 0 {
-		return xerrors.Errorf("ApplyBlock: b state index #%d can't be applied to the empty state", b.BlockIndex())
-	}
-	if !empty && vs.BlockIndex()+1 != b.BlockIndex() {
+	if vs.BlockIndex()+1 != b.BlockIndex() {
 		return xerrors.Errorf("ApplyBlock: b state index #%d can't be applied to the state with index #%d",
 			b.BlockIndex(), vs.BlockIndex())
 	}
-	if !empty && vs.Timestamp().After(b.Timestamp()) {
+	if vs.Timestamp().After(b.Timestamp()) {
 		return xerrors.New("ApplyBlock: inconsistent timestamps")
 	}
 	vs.ApplyStateUpdates(b.(*blockImpl).stateUpdate)
