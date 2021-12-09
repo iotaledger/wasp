625a77236e7d6c741c823ebf4c46458894f8bc68
Jorge Silva <jorgesilva.me@gmail.com>
2021-12-09 16:50:03 +0000

    draft gas costs

--- packages/iscp/gas/constants.go
+++ packages/iscp/gas/constants.go
@@ -2,7 +2,9 @@ package gas
 
 const (
 	// Gas burned per 1 stored byte
-	PerByte = 1
+	StoreByte = 2
+	LogByte   = 1 // logging (issuing events) can be cheaper than state storage, as it can be pruned without breaking things
+
 	// for the iscp.Sandbox.Utils interface
 	UtilsHashingBlake2b              = 200
 	UtilsHashingSha3                 = 300
@@ -17,4 +19,26 @@ const (
 
 	// fixed gas budged to process NFT in the VM
 	FixedGasBudgedNFT = 100
+
+	// Sandbox calls
+	ReadTheState        = 2 // (should we charge per byte read ?)
+	GetRequestData      = 10
+	GetCallerData       = 10
+	GetIncomingTransfer = 10
+	GetEntropy          = 10
+	GetStateAnchorInfo  = 10
+	LogEvent            = 10 // + byte cost of LogByte * payload size
+	CallContract        = 100
+	SendL1Request       = 1000
+
+	// Constant initial cas cost to call Core Contracts entrypoints
+	CoreRootDeployContract    = 5000
+	CoreRootChangePermissions = 50   // grant/revoke/require permissions
+	CoreAccounts              = 100  // withdrawal/deposit/harvest/sendTo
+	CoreBlobStore             = 5000 // + byte cost of StoreByte * blob size
+	CoreGovernance            = 1000 // all governance operations
+
+	// Tokenization stuff yet to be implemented
+	CreateTokenFoundry = 50000
+	IssueNFT           = 2000
 )
