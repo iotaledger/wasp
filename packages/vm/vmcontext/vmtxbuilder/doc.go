b0a9a4e08b39cc6d685b443e338211114eedb3be
lunfardo314 <evaldas.drasutis@iota.org>
2021-12-09 11:30:36 -0300

    edit vmtxbuilder docs

--- packages/vm/vmcontext/vmtxbuilder/doc.go
+++ packages/vm/vmcontext/vmtxbuilder/doc.go
@@ -1,15 +1,17 @@
-// Package vmtxbuilder implements AnchorTransactionBuilder, an transaction builder used by the VM to construct
+// Package vmtxbuilder implements AnchorTransactionBuilder, a transaction builder used by the VM to construct
 // anchor transaction. The AnchorTransactionBuilder keeps consistent state along operations of adding inputs
 // and adding outputs (`Consume` and `AddOutput`).
 // Total iotas available to on-chain accounts are kept in the anchor output.
 // The builder automatically maintains `internal` outputs which holds on-chain total balances of native tokens: one UTXO
 // for one non-zero balance of iotago.NativeTokenID.
-// Whenever native tokes are moved to/form the chain, those internal UTXO are updated by consuming input/producing output.
+// Whenever native tokens are moved to/form the chain, those internal UTXO are updated by consuming input/producing output.
 // The builder automatically ensures necessary minimal dust deposit on each of internal outputs. For this, builder takes
 // iotas from the total iotas on the chain or puts them back, depending on the needs of internal outputs.
-// When txbuilder is unable to maintain consisten state, it panics. The following panic code are possible:
+// When txbuilder is unable to maintain consistent state, it panics. The following panic code are possible:
 // - ErrInputLimitExceeded when maximum number of inputs in the transaction is exceeded
 // - ErrOutputLimitExceeded when maximum number of outputs in the transaction is exceeded
-// - ErrNotEnoughFundsForInternalDustDeposit when total number of iotas available is not enough for dust deposit
-//   of the new internal output
+// - ErrNotEnoughFundsForInternalDustDeposit when total number of iotas available is not enough for dust deposit of the new internal output
+// - ErrNotEnoughIotaBalance attempt to debit more iotas than possible
+// - ErrNotEnoughNativeAssetBalance attempt to debit more native tokens than possible
+// - ErrOverflow overflow of arithmetics
 package vmtxbuilder
