bc4cd7411e830d5fee483f5920de3de1d288ff5f
shawkyz <abdelrhmanshawky4@gmail.com>
2022-01-16 22:00:25 +0100

    add evt argument to events

--- tools/schema/generator/tsclienttemplates/events.go
+++ tools/schema/generator/tsclienttemplates/events.go
@@ -28,12 +28,12 @@ $#each events eventClass
 `,
 	// *******************************
 	"eventHandlerMember": `
-	$evtName: (Event$EvtName) => void = () => {};
+	$evtName: (evt: Event$EvtName) => void = () => {};
 `,
 	// *******************************
 	"funcSignature": `
 
-	public on$PkgName$EvtName(handler: (Event$EvtName) => void): void {
+	public on$PkgName$EvtName(handler: (evt: Event$EvtName) => void): void {
 		this.$evtName = handler;
 	}
 `,
