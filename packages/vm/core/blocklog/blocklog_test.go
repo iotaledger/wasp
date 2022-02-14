121765b1f72dfbf5263c0b91ab11934524a1635d
Lukas Möller <lukas.moeller@iota.org>
2022-02-14 21:59:38 +0100

    Revert blocklog test

--- packages/vm/core/blocklog/blocklog_test.go
+++ packages/vm/core/blocklog/blocklog_test.go
@@ -8,39 +8,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-/*func TestSimpleErrorSerialization(t *testing.T) {
-	mu := marshalutil.New()
-
-	// Initial error
-	blockError := FailedToLoadError.Create("placeBet", "destroy", "setAdmin")
-
-	// Serialize error to bytes
-	err := blockError.Serialize(mu)
-	require.NoError(t, err)
-
-	// Recreate error from bytes
-	newError, err := errors.ErrorFromBytes(mu, nil)
-	require.NoError(t, err)
-
-	// Validate that properties are the same
-	require.EqualValues(t, blockError.Hash(), newError.Hash())
-	require.EqualValues(t, blockError.params, newError.params)
-	require.EqualValues(t, blockError.messageFormat, newError.messageFormat)
-
-	// Validate that error returns a proper error type
-	require.Error(t, newError)
-
-	t.Log(newError.Error())
-}*/
-
 func TestSerdeRequestReceipt(t *testing.T) {
 	nonce := uint64(time.Now().UnixNano())
 	req := iscp.NewOffLedgerRequest(iscp.RandomChainID(), iscp.Hn("0"), iscp.Hn("0"), nil, nonce)
-	//testError, err := GeneralErrorCollection.Create(1, 1)
 
 	rec := &RequestReceipt{
 		Request: req,
-		//Error:   testError,
 	}
 	forward := rec.Bytes()
 	back, err := RequestReceiptFromBytes(forward)
