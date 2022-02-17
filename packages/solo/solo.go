91cbfa7aa5831c3f244f7975312f4650b118c92a
lunfardo314 <evaldas.drasutis@iota.org>
2022-02-17 12:14:46 +0200

    FIXME comment

--- packages/solo/solo.go
+++ packages/solo/solo.go
@@ -47,15 +47,15 @@ const (
 // Solo is a structure which contains global parameters of the test: one per test instance
 type Solo struct {
 	// instance of the test
-	T           TestContext
-	logger      *logger.Logger
-	dbmanager   *dbmanager.DBManager
-	utxoDB      *utxodb.UtxoDB
-	glbMutex    sync.RWMutex
-	ledgerMutex sync.RWMutex
+	T                            TestContext
+	logger                       *logger.Logger
+	dbmanager                    *dbmanager.DBManager
+	utxoDB                       *utxodb.UtxoDB
+	glbMutex                     sync.RWMutex
+	ledgerMutex                  sync.RWMutex
 	chains                       map[iscp.ChainID]*Chain
-	vmRunner    vm.VMRunner
-	processorConfig  *processors.Config
+	vmRunner                     vm.VMRunner
+	processorConfig              *processors.Config
 	disableAutoAdjustDustDeposit bool
 }
 
@@ -87,9 +87,9 @@ type Chain struct {
 	ValidatorFeeTarget *iscp.AgentID
 
 	// State ia an interface to access virtual state of the chain: a buffered collection of key/value pairs
-	State       state.VirtualStateAccess
+	State state.VirtualStateAccess
 	// GlobalSync represents global atomic flag for the optimistic state reader. In Solo it has no function
-	GlobalSync  coreutil.ChainStateSync
+	GlobalSync coreutil.ChainStateSync
 	// StateReader is the read only access to the state
 	StateReader state.OptimisticStateReader
 	// Log is the named logger of the chain
@@ -111,7 +111,7 @@ type InitOptions struct {
 	Seed                  cryptolib.Seed
 	RentStructure         *iotago.RentStructure
 	Log                   *logger.Logger
-	}
+}
 
 func defaultInitOptions() *InitOptions {
 	return &InitOptions{
@@ -146,12 +146,12 @@ func New(t TestContext, initOptions ...*InitOptions) *Solo {
 
 	utxoDBinitParams := utxodb.DefaultInitParams(opt.Seed[:]).WithRentStructure(opt.RentStructure)
 	ret := &Solo{
-		T:               t,
+		T:                            t,
 		logger:                       opt.Log,
 		dbmanager:                    dbmanager.NewDBManager(opt.Log.Named("db"), true),
 		utxoDB:                       utxodb.New(utxoDBinitParams),
 		chains:                       make(map[iscp.ChainID]*Chain),
-		vmRunner:        runvm.NewVMRunner(),
+		vmRunner:                     runvm.NewVMRunner(),
 		processorConfig:              processors.NewConfig(),
 		disableAutoAdjustDustDeposit: !opt.AutoAdjustDustDeposit,
 	}
@@ -336,7 +336,7 @@ func (env *Solo) requestsByChain(tx *iotago.Transaction) map[iscp.ChainID][]iscp
 	ret, err := iscp.RequestsInTransaction(tx)
 	require.NoError(env.T, err)
 	return ret
-		}
+}
 
 func (env *Solo) AddRequestsToChainMempool(ch *Chain, reqs []iscp.Request) {
 	env.glbMutex.RLock()
@@ -345,7 +345,7 @@ func (env *Solo) AddRequestsToChainMempool(ch *Chain, reqs []iscp.Request) {
 	defer ch.runVMMutex.Unlock()
 
 	ch.mempool.ReceiveRequests(reqs...)
-		}
+}
 
 // AddRequestsToChainMempoolWaitUntilInbufferEmpty adds all the requests to the chain mempool,
 // then waits for the in-buffer to be empty, before resuming VM execution
@@ -409,10 +409,10 @@ func (ch *Chain) collateBatch() []iscp.Request {
 			onLedgerReq := req.AsOnLedger()
 			if !iscp.RequestIsUnlockable(onLedgerReq, ch.ChainID.AsAddress(), now) {
 				continue
-				}
 			}
-			ret = append(ret, req)
 		}
+		ret = append(ret, req)
+	}
 	return ret
 }
 
@@ -457,6 +457,7 @@ func (ch *Chain) BacklogLen() int {
 }
 
 func (env *Solo) UnspentOutputs(addr iotago.Address) (iotago.OutputSet, iotago.OutputIDs) {
+	// FIXME: the order outputIDs does not correspond to the set (a map) of outputs
 	allOuts := env.utxoDB.GetUnspentOutputs(addr)
 	ids := make(iotago.OutputIDs, len(allOuts))
 	i := 0
