0190f8eb26d4bfe26ba65960298285d4601c3a9c
Jorge Silva <jorgesilva.me@gmail.com>
2022-03-18 15:12:49 +0000

    nodeconn plugin

--- plugins/nodeconn/plugin.go
+++ plugins/nodeconn/plugin.go
@@ -2,14 +2,14 @@ package nodeconn
 
 import (
 	"context"
-	"net"
 	"time"
 
 	"github.com/iotaledger/hive.go/daemon"
 	"github.com/iotaledger/hive.go/logger"
 	"github.com/iotaledger/hive.go/node"
+	"github.com/iotaledger/wasp/packages/chain"
+	"github.com/iotaledger/wasp/packages/nodeconn"
 	"github.com/iotaledger/wasp/packages/parameters"
-	"github.com/iotaledger/wasp/packages/txstream"
 	"github.com/iotaledger/wasp/packages/util/ready"
 	"github.com/iotaledger/wasp/plugins/peering"
 )
@@ -22,7 +22,7 @@ const dialTimeout = 1 * time.Second
 var (
 	log *logger.Logger
 
-	nodeConn    *txstream.Client
+	nc          chain.NodeConnection
 	initialized = ready.New("NodeConn")
 )
 
@@ -31,9 +31,9 @@ func Init() *node.Plugin {
 	return node.NewPlugin(PluginName, nil, node.Enabled, configure, run)
 }
 
-func NodeConnection() *txstream.Client {
+func NodeConnection() chain.NodeConnection {
 	initialized.MustWait(5 * time.Second)
-	return nodeConn
+	return nc
 }
 
 func configure(_ *node.Plugin) {
@@ -42,16 +42,13 @@ func configure(_ *node.Plugin) {
 
 func run(_ *node.Plugin) {
 	err := daemon.BackgroundWorker(PluginName, func(ctx context.Context) {
-		addr := parameters.GetString(parameters.NodeAddress)
-		dial := txstream.DialFunc(func() (string, net.Conn, error) {
-			log.Infof("connecting with node at %s", addr)
-			conn, err := net.DialTimeout("tcp", addr, dialTimeout)
-			return addr, conn, err
-		})
+		addr := parameters.GetString(parameters.NodeConnHost)
+		port := parameters.GetInt(parameters.NodeConnPort)
+		networkProvider := peering.DefaultNetworkProvider()
+		nc = nodeconn.New(addr, port, networkProvider, log)
+		defer nc.Close()
 
-		nodeConn = txstream.New(peering.DefaultNetworkProvider().Self().NetID(), log, dial)
 		initialized.SetReady()
-		defer nodeConn.Close()
 
 		<-ctx.Done()
 
