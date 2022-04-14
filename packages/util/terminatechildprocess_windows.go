09613f07cd683d80c28b3f940ccbac483c58b61b
Jorge Silva <jorgesilva.me@gmail.com>
2022-04-14 10:58:07 +0100

    fix windows build

---
+++ packages/util/terminatechildprocess_windows.go
@@ -0,0 +1,10 @@
+//go:build windows
+
+package util
+
+func TerminateCmdWhenTestStops(cmd *exec.cmd) {
+	// do nothing, SysprocAttr is not available on windows
+	// maybe there is a way to archieve a similar result, but as of now
+	// just be aware that child processes might be left hanging if
+	// the test process is forcebly stopped (i.e. it times out)
+}
