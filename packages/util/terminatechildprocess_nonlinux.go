74633d8403bae08d23685108d1d8a37400b00c9b
Jorge Silva <jorgesilva.me@gmail.com>
2022-04-18 10:48:20 +0100

    fix sysproattr build error on mac

--- packages/util/terminatechildprocess_nonlinux.go
+++ packages/util/terminatechildprocess_nonlinux.go
@@ -1,9 +1,9 @@
-//go:build windows
+//go:build !linux
 
 package util
 
 func TerminateCmdWhenTestStops(cmd *exec.cmd) {
-	// do nothing, SysprocAttr is not available on windows
+	// do nothing, SysprocAttr is not available on windows/Mac
 	// maybe there is a way to archieve a similar result, but as of now
 	// just be aware that child processes might be left hanging if
 	// the test process is forcebly stopped (i.e. it times out)
