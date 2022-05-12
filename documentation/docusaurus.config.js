464acb86d679a6cb8681ae53f07a69d361782d51
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-12 14:07:58 +0200

    Change branch to develop

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
         path: path.resolve(__dirname, 'docs'),
         routeBasePath: 'smart-contracts',
         sidebarPath: path.resolve(__dirname, 'sidebars.js'),
-        editUrl: 'https://github.com/iotaledger/wasp/edit/master/documentation',
+        editUrl: 'https://github.com/iotaledger/wasp/edit/develop/documentation',
         remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments')],
       }
     ],
