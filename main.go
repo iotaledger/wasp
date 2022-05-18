478aac9d21cd1d1b015a90fa5ae6d1fc2f959c52
Jorge Silva <jorgesilva.me@gmail.com>
2022-05-18 15:06:22 +0100

    remove legacy cli plugin

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"github.com/iotaledger/hive.go/node"
 	"github.com/iotaledger/wasp/packages/parameters"
-	_ "github.com/iotaledger/wasp/packages/vm/sandbox"
 	"github.com/iotaledger/wasp/plugins/banner"
 	"github.com/iotaledger/wasp/plugins/chains"
-	"github.com/iotaledger/wasp/plugins/cli"
 	"github.com/iotaledger/wasp/plugins/config"
 	"github.com/iotaledger/wasp/plugins/dashboard"
 	"github.com/iotaledger/wasp/plugins/database"
@@ -33,12 +31,11 @@ func main() {
 		config.Init(params),
 		logger.Init(params),
 		gracefulshutdown.Init(),
-		cli.Init(),
+		nodeconn.Init(),
 		database.Init(),
 		registry.Init(),
 		peering.Init(),
 		dkg.Init(),
-		nodeconn.Init(),
 		processors.Init(),
 		wasmtimevm.Init(),
 		wal.Init(),
