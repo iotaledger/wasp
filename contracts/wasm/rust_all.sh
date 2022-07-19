2a3a2b9fa482396b138a87f17c6988055daa0672
Eric Hop <eric@iota.org>
2022-07-19 12:59:37 -0700

    Updated build scripts for gascalibration SCs

--- contracts/wasm/rust_all.sh
+++ contracts/wasm/rust_all.sh
@@ -4,3 +4,10 @@ for dir in ./*; do
     bash rust_build.sh "$dir" $1
   fi
 done
+cd gascalibration
+for dir in ./*; do
+ if [ -d "$dir" ]; then
+    bash rust_build.sh "$dir" $1
+  fi
+done
+cd ..
--- contracts/wasm/ts_all.cmd
+++ contracts/wasm/ts_all.cmd
@@ -1,2 +1,5 @@
 @echo off
 for /d %%f in (*.) do call ts_build.cmd %%f %1
+cd gascalibration
+for /d %%f in (*.) do call ..\ts_build.cmd %%f %1
+cd ..
