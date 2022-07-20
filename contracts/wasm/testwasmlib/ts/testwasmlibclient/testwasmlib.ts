df655769f3b5493b86b9c82ecdeb2e453657c7ee
Yang Hau <yuanyanghau@gmail.com>
2022-07-21 01:42:24 +0700

    chore: Remove schema-generated files and wasm files

--- contracts/wasm/testwasmlib/ts/testwasmlibclient/testwasmlib.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-import * as events from "./events"
-import * as service from "./service"
-
-export function onTestWasmLibTest(event: events.EventTest): void {
-}
