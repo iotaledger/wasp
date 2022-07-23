59731982e2f89a59d2d27d389190f028ec5679a8
Yang Hau <yuanyanghau@gmail.com>
2022-07-23 18:36:56 +0700

    fix: Assign correct bash path

--- contracts/wasm/go_all.sh
+++ contracts/wasm/go_all.sh
@@ -7,7 +7,7 @@ done
 cd gascalibration
 for dir in ./*; do
  if [ -d "$dir" ]; then
-    bash go_build.sh "$dir" $1
+    bash ../go_build.sh "$dir" $1
   fi
 done
 cd ..
--- contracts/wasm/ts_all.sh
+++ contracts/wasm/ts_all.sh
@@ -7,7 +7,7 @@ done
 cd gascalibration
 for dir in ./*; do
  if [ -d "$dir" ]; then
-    bash ts_build.sh "$dir" $1
+    bash ../ts_build.sh "$dir" $1
   fi
 done
 cd ..
