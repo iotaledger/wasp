9d12367ee0ad4d5dbd8116de525ad48ee19c4afa
Eric Hop <eric@iota.org>
2022-07-23 09:42:27 -0700

    fixed cleanup scipts

--- contracts/wasm/cleanup.cmd
+++ contracts/wasm/cleanup.cmd
@@ -7,20 +7,27 @@ del /s results.*
 del /s state.*
 del /s typedefs.*
 del /s types.*
-del /s main.go
 del /s /q *.wasm
 
-rem careful, this could fuck up fairroulette frontend
+rem careful not to delete all, this could fuck up gascalibration
+for /d %%f in (*.) do del %%f\go\main.go
+
+rem careful not to delete all, this could fuck up fairroulette frontend
 for /d %%f in (*.) do del %%f\ts\%%f\index.ts
 for /d %%f in (*.) do del %%f\ts\%%f\tsconfig.json
 
 for /d %%f in (*.) do del /s /q %%f\pkg\*.*
 for /d %%f in (*.) do del /s /q %%f\ts\pkg\*.*
 del /s /q target\*.*
+
 cd gascalibration
+
+for /d %%f in (*.) do del %%f\go\main.go
+
 for /d %%f in (*.) do del %%f\ts\%%f\index.ts
 for /d %%f in (*.) do del %%f\ts\%%f\tsconfig.json
 
 for /d %%f in (*.) do del /s /q %%f\pkg\*.*
 for /d %%f in (*.) do del /s /q %%f\ts\pkg\*.*
 del /s /q target\*.*
+cd ..
