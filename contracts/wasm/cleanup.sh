023210922be44f5514e43f4428152f2a93151097
Eric Hop <eric@iota.org>
2022-07-23 09:42:27 -0700

    fixed cleanup scipts

--- contracts/wasm/cleanup.sh
+++ contracts/wasm/cleanup.sh
@@ -10,26 +10,28 @@ root=$(git rev-parse --show-toplevel)
  rm "$root"/contracts/wasm/*/*/*/state.*
  rm "$root"/contracts/wasm/*/*/*/typedefs.*
  rm "$root"/contracts/wasm/*/*/*/types.*
- rm "$root"/contracts/wasm/*/*/*/main.go
  rm -r "$root"/contracts/wasm/*/*/pkg
  rm -r "$root"/contracts/wasm/target
  rm -r "$root"/contracts/wasm/node_modules
 
 for dir in "$root"/contracts/wasm/*; do
  if [ -d "$dir" ]; then
+    rm "$dir"/go/main.go
     rm "$dir"/ts/"$dir"/index.ts
     rm "$dir"/ts/"$dir"/tsconfig.json
     rm "$dir"/pkg/*.*
     rm "$dir"/ts/pkg/*.*
   fi
+done
 
 cd gascalibration
 for dir in ./*; do
  if [ -d "$dir" ]; then
+   rm "$dir"/go/main.go
     rm "$dir"/ts/"$dir"/index.ts
     rm "$dir"/ts/"$dir"/tsconfig.json
     rm "$dir"/pkg/*.*
     rm "$dir"/ts/pkg/*.*
   fi
 done
-done
+cd ..
