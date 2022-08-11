6c35c35664896fc00d127b7cbd2586a267699706
Jorge Silva <jorgesilva.me@gmail.com>
2022-08-11 11:37:10 +0100

    minor fixes

--- tools/evm-l2-withdraw/src/App.svelte
+++ tools/evm-l2-withdraw/src/App.svelte
@@ -1,87 +1,100 @@
 <script lang="ts">
-import { connected, web3, selectedAccount, chainId, chainData, defaultEvmStores } from 'svelte-web3';
+  import {
+    connected,
+    web3,
+    selectedAccount,
+    chainId,
+    chainData,
+    defaultEvmStores,
+  } from "svelte-web3";
 
-import iscAbiAsText from '../../../packages/vm/core/evm/isccontract/ISC.abi?raw';
+  import iscAbiAsText from "../../../packages/vm/core/evm/iscmagic/ISC.abi?raw";
 
-const iscAbi = JSON.parse(iscAbiAsText);
-const iscContractAddress: string = '0x0000000000000000000000000000000000001074';
+  const iscAbi = JSON.parse(iscAbiAsText);
+  const iscContractAddress: string =
+    "0x0000000000000000000000000000000000001074";
 
-async function connectToWallet() {
-  await defaultEvmStores.setProvider()
-}
+  async function connectToWallet() {
+    await defaultEvmStores.setProvider();
+  }
 
-async function start() {
-  await defaultEvmStores.setProvider();
+  async function start() {
+    await defaultEvmStores.setProvider();
+    if (!defaultEvmStores.$selectedAccount) {
+      console.log("no account selected");
+      return;
+    }
 
-  const chainId = await $web3.eth.getChainId()
-  const contract = new $web3.eth.Contract(iscAbi, iscContractAddress, {
-    from: '0x8B65DD08C7784017fe6B8Af20904e61916506fD4'
-  });
+    const chainId = await $web3.eth.getChainId();
+    const contract = new $web3.eth.Contract(iscAbi, iscContractAddress, {
+      from: defaultEvmStores.$selectedAccount,
+    });
 
-  var amount = $web3.utils.toBN(1074);
+    var amount = $web3.utils.toBN(1074);
 
-  var parameters = [
-    { // Receiver
-        data: new Uint8Array(32) 
-      }, 
-      { // Fungible Tokens
+    var parameters = [
+      {
+        // Receiver
+        data: new Uint8Array(32),
+      },
+      {
+        // Fungible Tokens
         baseTokens: 1074,
-        tokens: []
-      }, 
+        tokens: [],
+      },
       true,
-      { // Metadata
-        targetContract: $web3.utils.hexToNumber('0x3c4b5e02'),
-        entrypoint: $web3.utils.hexToNumber('0x23f4e3a1'), 
+      {
+        // Metadata
+        targetContract: $web3.utils.hexToNumber("0x3c4b5e02"),
+        entrypoint: $web3.utils.hexToNumber("0x23f4e3a1"),
         gasBudget: 50000000,
 
         params: {
           items: [
             {
-              key: "x".charCodeAt(0), 
-              value: "0x" + amount //TODO: Fix
-            }
-          ]
+              key: "x".charCodeAt(0),
+              value: "0x" + amount, //TODO: Fix
+            },
+          ],
         },
 
         allowance: {
-          nfts:[],
+          nfts: [],
           baseTokens: 1074,
-          assets: [
+          tokens: [
             /*{
               ID: [],
               Amount: 1
             }*/
-          ]
-            
-        }
+          ],
+        },
       },
-      { // Options
+      {
+        // Options
         timelock: 0,
         expiration: {
           time: 0,
           returnAddress: {
-            data: []
-          }
-        }
-      } 
-  ];
+            data: [],
+          },
+        },
+      },
+    ];
 
-  console.log(...parameters)
+    console.log(...parameters);
 
-  const result = await contract.methods.send(...parameters).send();
-  console.log(result)
-}
+    const result = await contract.methods.send(...parameters).send();
+    console.log(result);
+  }
 
-
-start();
-
+  start();
 </script>
 
 <main>
   {#if !$connected}
-    <button on:click="{connectToWallet}">Connect to Wallet</button>
+    <button on:click={connectToWallet}>Connect to Wallet</button>
   {:else}
-      Connected to Chain {$chainId}
+    Connected to Chain {$chainId}
   {/if}
 </main>
 
