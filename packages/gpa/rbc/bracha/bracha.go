7542e460da25899fa1b7d943aec54bfc6d641fe4
Karolis Petrauskas <karolis.petrauskas@erisata.lt>
2022-08-25 11:27:44 +0300

    RBC: Use sending function in all the places, check if initial is received from the broadcaster.

--- packages/gpa/rbc/bracha/bracha.go
+++ packages/gpa/rbc/bracha/bracha.go
@@ -3,22 +3,44 @@
 
 // package bracha implements Bracha's Reliable Broadcast, as described in
 //
-// 		Gabriel Bracha. 1987. Asynchronous byzantine agreement protocols. Inf. Comput.
-// 		75, 2 (November 1, 1987), 130–143. DOI:https://doi.org/10.1016/0890-5401(87)90054-X
+//      Gabriel Bracha. 1987. Asynchronous byzantine agreement protocols. Inf. Comput.
+//      75, 2 (November 1, 1987), 130–143. DOI:https://doi.org/10.1016/0890-5401(87)90054-X
+//
+// The following pseudo-code is taken from "FIG. 1. The broadcast primitive" in the above
+// paper. Here the paper assumes $0 ≤ t < n/3$.
+//
+//      Broadcast(v)
+//      step 0. (By process p)
+//          Send (initial, v) to all the processes.
+//      step 1. Wait until the receipt of,
+//              one (initial, v) message
+//              or (n+t)/2 (echo, v) messages
+//              or (t+l) (ready, v) messages
+//              for some v.
+//          Send (echo, v) to all the processes.
+//      step 2. Wait until the receipt of,
+//              (n+t)/2 (echo, v) messages
+//              or (t+1) (ready, v) messages
+//              (including messages received in step 1)
+//              for some v.
+//          Send (ready, v) to all the processes.
+//      step 3. Wait until the receipt of,
+//              2t+1 (ready, v) messages
+//              (including messages received in step 1 and step 2) for some v.
+//          Accept v.
 //
 // Additionally a predicate is added as it was used in
 //
-// 		Sourav Das, Zhuolun Xiang, and Ling Ren. 2021. Asynchronous Data Dissemination
-// 		and its Applications. In Proceedings of the 2021 ACM SIGSAC Conference on Computer
-// 		and Communications Security (CCS '21). Association for Computing Machinery,
-// 		New York, NY, USA, 2705–2721. DOI:https://doi.org/10.1145/3460120.3484808
+//      Sourav Das, Zhuolun Xiang, and Ling Ren. 2021. Asynchronous Data Dissemination
+//      and its Applications. In Proceedings of the 2021 ACM SIGSAC Conference on Computer
+//      and Communications Security (CCS '21). Association for Computing Machinery,
+//      New York, NY, USA, 2705–2721. DOI:https://doi.org/10.1145/3460120.3484808
 //
 // NOTE: Only a dedicated process can broadcast a value. Otherwise it would be a consensus.
 package bracha
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/iotaledger/wasp/packages/gpa"
 	"github.com/iotaledger/wasp/packages/hashing"
@@ -81,20 +103,8 @@ func (r *rbc) Input(input gpa.Input) []gpa.Message {
 	}
 	inputVal := input.([]byte)
 	r.ensureValueStored(inputVal)
-	msgs := r.broadcast(msgBrachaTypeInitial, inputVal)
+	msgs := r.sendToAll(msgBrachaTypeInitial, inputVal)
 	r.initialSent = true
-	return msgs
-}
-
-func (r *rbc) broadcast(t msgBrachaType, v []byte) []gpa.Message {
-	var msgs []gpa.Message
-	for i := range r.peers {
-		msgs = append(msgs, &msgBracha{
-			t: t,
-			r: r.peers[i],
-			v: v,
-		})
-	}
 	return msgs
 }
 
@@ -119,21 +129,19 @@ func (r *rbc) Message(msg gpa.Message) []gpa.Message {
 }
 
 func (r *rbc) handleInitial(msg *msgBracha) []gpa.Message {
+	if msg.s != r.broadcaster {
+		// Initial messages can only be sent by the broadcaster process.
+		// Ignore all the rest.
+		return gpa.NoMessages()
+	}
 	if r.echoSent {
-		return []gpa.Message{}
+		return gpa.NoMessages()
 	}
 	if !r.predicate(msg.v) {
 		r.pendingMsgs = append(r.pendingMsgs, msg)
-		return []gpa.Message{}
+		return gpa.NoMessages()
 	}
-	msgs := []gpa.Message{}
-	for i := range r.peers {
-		msgs = append(msgs, &msgBracha{
-			t: msgBrachaTypeEcho,
-			r: r.peers[i],
-			v: msg.v,
-		})
-	}
+	msgs := r.sendToAll(msgBrachaTypeEcho, msg.v)
 	r.echoSent = true
 	return msgs
 }
@@ -183,29 +191,24 @@ func (r *rbc) handlePredicateUpdate(msg msgPredicateUpdate) []gpa.Message {
 	r.predicate = msg.predicate
 	resendMsgs := r.pendingMsgs
 	r.pendingMsgs = []gpa.Message{}
-	return resendMsgs // The OwnHandler will resend them back.
+	//
+	// Resend the messages again.
+	// The OwnHandler overrides the sender, thus we can't rely on it here.
+	msgs := gpa.NoMessages()
+	for i := range resendMsgs {
+		msgs = append(msgs, r.Message(resendMsgs[i])...)
+	}
+	return msgs
 }
 
 func (r *rbc) maybeSendEchoReady(v []byte) []gpa.Message {
 	msgs := []gpa.Message{}
 	if !r.echoSent {
-		for i := range r.peers {
-			msgs = append(msgs, &msgBracha{
-				t: msgBrachaTypeEcho,
-				r: r.peers[i],
-				v: v,
-			})
-		}
+		msgs = append(msgs, r.sendToAll(msgBrachaTypeEcho, v)...)
 		r.echoSent = true
 	}
 	if !r.readySent {
-		for i := range r.peers {
-			msgs = append(msgs, &msgBracha{
-				t: msgBrachaTypeReady,
-				r: r.peers[i],
-				v: v,
-			})
-		}
+		msgs = append(msgs, r.sendToAll(msgBrachaTypeReady, v)...)
 		r.readySent = true
 	}
 	return msgs
@@ -231,6 +234,18 @@ func (r *rbc) UnmarshalMessage(data []byte) (gpa.Message, error) {
 		return nil, xerrors.Errorf("cannot unmarshal RBC:msgBracha message: %w", err)
 	}
 	return m, nil
+}
+
+func (r *rbc) sendToAll(t msgBrachaType, v []byte) []gpa.Message {
+	msgs := make([]gpa.Message, len(r.peers))
+	for i := range r.peers {
+		msgs[i] = &msgBracha{
+			t: t,
+			r: r.peers[i],
+			v: v,
+		}
+	}
+	return msgs
 }
 
 func (r *rbc) ensureValueStored(val []byte) hashing.HashValue {
