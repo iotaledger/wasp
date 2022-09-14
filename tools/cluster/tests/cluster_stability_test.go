88d25f6081536b9d42135ed7587c551e52d6cbac
Jorge Silva <jorgesilva.me@gmail.com>
2022-09-14 15:40:13 +0100

    remove freeze cluster tests

--- tools/cluster/tests/cluster_stability_test.go
+++ tools/cluster/tests/cluster_stability_test.go
@@ -5,7 +5,6 @@ This test will test the recovery capabilities of the clusterized nodes, where so
 package tests
 
 import (
-	"runtime"
 	"sync"
 	"testing"
 	"time"
@@ -53,7 +52,7 @@ func (e *SabotageEnv) sendRequests(numRequests int, messageDelay time.Duration) 
 	}
 }
 
-func (e *SabotageEnv) setSabotageValidators(breakCount int) {
+func (e *SabotageEnv) setSabotageValidators(breakCount int) { //nolint:unused // false positive
 	clusterSize := len(e.chainEnv.Clu.Config.Wasp)
 
 	from := clusterSize - e.NumValidators
@@ -69,15 +68,7 @@ func (e *SabotageEnv) setSabotageAll(breakCount int) {
 	e.SabotageList = util.MakeRange(from, to)
 }
 
-type SabotageOption int
-
-const (
-	SabotageByKill SabotageOption = iota
-	// Important: Frozen nodes need to get killed/unfrozen manually after the test is done, otherwise they stay alive after the test has finished
-	SabotageByFreeze SabotageOption = iota
-)
-
-func (e *SabotageEnv) sabotageNodes(sabotageOption SabotageOption, startDelay, inBetweenDelay time.Duration) *sync.WaitGroup {
+func (e *SabotageEnv) sabotageNodes(startDelay, inBetweenDelay time.Duration) *sync.WaitGroup {
 	// Give the test time to start
 
 	var wg sync.WaitGroup
@@ -92,13 +83,7 @@ func (e *SabotageEnv) sabotageNodes(sabotageOption SabotageOption, startDelay, i
 		for _, nodeID := range e.SabotageList {
 			e.chainEnv.t.Logf("Breaking node: %v (%s)", nodeID, time.Now())
 
-			var err error
-
-			if sabotageOption == SabotageByKill {
-				err = e.chainEnv.Clu.KillNodeProcess(nodeID)
-			} else if sabotageOption == SabotageByFreeze {
-				err = e.chainEnv.Clu.FreezeNode(nodeID)
-			}
+			err := e.chainEnv.Clu.KillNodeProcess(nodeID)
 
 			require.NoError(e.chainEnv.t, err)
 
@@ -111,16 +96,7 @@ func (e *SabotageEnv) sabotageNodes(sabotageOption SabotageOption, startDelay, i
 	return &wg
 }
 
-func (e *SabotageEnv) unfreezeNodes() {
-	for _, nodeID := range e.SabotageList {
-		e.chainEnv.t.Logf("Unfreezing node %v (%s)", nodeID, time.Now())
-		err := e.chainEnv.Clu.UnfreezeNode(nodeID)
-
-		require.NoError(e.chainEnv.t, err)
-	}
-}
-
-func (e *SabotageEnv) getActiveNodeList() []int {
+func (e *SabotageEnv) getActiveNodeList() []int { //nolint:unused // false positive
 	contains := func(x int) bool {
 		for _, n := range e.SabotageList {
 			if n == x {
@@ -170,7 +146,7 @@ func TestSuccessfulIncCounterIncreaseWithMildInstability(t *testing.T) {
 	env := initializeStabilityTest(t, numValidators, clusterSize)
 	env.setSabotageValidators(numBrokenNodes)
 
-	wg := env.sabotageNodes(SabotageByKill, 4*time.Second, 1*time.Second)
+	wg := env.sabotageNodes(4*time.Second, 1*time.Second)
 	env.sendRequests(numRequests, time.Millisecond*250)
 
 	wg.Wait()
@@ -182,7 +158,7 @@ func runTestFailsIncCounterIncreaseAsQuorumNotMet(t *testing.T, clusterSize, num
 	env := initializeStabilityTest(t, numValidators, clusterSize)
 	env.setSabotageAll(numBrokenNodes)
 
-	wg := env.sabotageNodes(SabotageByKill, 5*time.Second, 500*time.Millisecond)
+	wg := env.sabotageNodes(5*time.Second, 500*time.Millisecond)
 	env.sendRequests(numRequests, time.Millisecond*250)
 
 	wg.Wait()
@@ -214,239 +190,5 @@ func TestFailsIncCounterIncreaseAsQuorumNotMet(t *testing.T) {
 		const numRequests = 35
 
 		runTestFailsIncCounterIncreaseAsQuorumNotMet(t, clusterSize, numValidators, numBrokenNodes, numRequests)
-	})
-}
-
-func testConsenseusReconnectingNodesNoQuorum(t *testing.T, clusterSize, numValidators, numBrokenNodes, numRequestsBeforeFailure, numRequestsAfterFailure int) {
-	env := initializeStabilityTest(t, numValidators, clusterSize)
-	env.setSabotageValidators(numBrokenNodes)
-
-	t.Cleanup(func() {
-		// This hook is just a safety measure to unfreeze all nodes when an error happens. Otherwise they stay in a zombie mode after the tests ended.
-		if env != nil {
-			env.unfreezeNodes()
-		}
-	})
-
-	env.sendRequests(numRequestsBeforeFailure, time.Millisecond*250)
-	waitUntil(t, env.chainEnv.counterEquals(int64(numRequestsBeforeFailure)), env.getActiveNodeList(), 60*time.Second, "incCounter matches expectation")
-
-	wg := env.sabotageNodes(SabotageByFreeze, 5*time.Second, 1*time.Second)
-	env.sendRequests(numRequestsAfterFailure, time.Millisecond*500)
-	wg.Wait()
-
-	t.Log("Waiting for network to settle, no incCounter increases should be logged now")
-	time.Sleep(time.Second * 25)
-	counter := env.chainEnv.getNativeContractCounter(nativeIncCounterSCHname)
-	// quorum is not met, work stops, incCounter should not equal numRequests
-	require.NotEqual(env.chainEnv.t, numRequestsBeforeFailure+numRequestsAfterFailure, int(counter))
-
-	// unfreeze nodes, after bootstrapping it is expected to reach a full quorum leading to an equal incCounter
-	env.unfreezeNodes()
-
-	waitUntil(t, env.chainEnv.counterEquals(int64(numRequestsBeforeFailure+numRequestsAfterFailure)), env.chainEnv.Clu.Config.AllNodes(), 60*time.Second, "incCounter matches expectation")
-	time.Sleep(10 * time.Second) // wait for nodes to be properly shutdown before starting a new test
-}
-
-func testConsenseusReconnectingNodesHighQuorum(t *testing.T, clusterSize, numValidators, numBrokenNodes, numRequestsBeforeFailure, numRequestsAfterFailure int) {
-	// Windows does not support freezing with SIGSTOP, we skip those for now.
-	if runtime.GOOS == OSWindows {
-		t.Skip()
-	}
-
-	env := initializeStabilityTest(t, numValidators, clusterSize)
-	env.setSabotageValidators(numBrokenNodes)
-
-	t.Cleanup(func() {
-		// This hook is just a safety measure to unfreeze all nodes when an error happens. Otherwise they stay in a zombie mode after the tests ended.
-		if env != nil {
-			env.unfreezeNodes()
-		}
-	})
-
-	env.sendRequests(numRequestsBeforeFailure, time.Millisecond*250)
-	waitUntil(t, env.chainEnv.counterEquals(int64(numRequestsBeforeFailure)), env.getActiveNodeList(), 60*time.Second, "incCounter matches expectation")
-
-	wg := env.sabotageNodes(SabotageByFreeze, 5*time.Second, 1*time.Second)
-	env.sendRequests(numRequestsAfterFailure, time.Millisecond*500)
-	wg.Wait()
-
-	waitUntil(t, env.chainEnv.counterEquals(int64(numRequestsBeforeFailure+numRequestsAfterFailure)), env.getActiveNodeList(), 60*time.Second, "incCounter matches expectation")
-	env.unfreezeNodes()
-	time.Sleep(10 * time.Second) // wait for nodes to be properly shutdown before starting a new test
-}
-
-func TestSuccessfulConsenseusWithReconnectingNodes(t *testing.T) {
-	/**
-	* incCounter requests get sent, after reaching a matching counter value, nodes get shut down, new requests get send in parallel.
-	*	If frozen nodes are below the quorum level, the incCounter count should not reach numRequests until unfrozen, otherwise the opposite is expected
-	**/
-
-	if testing.Short() {
-		t.SkipNow()
-	}
-
-	// Windows does not support freezing with SIGSTOP, we skip those for now.
-	if runtime.GOOS == OSWindows {
-		t.Skip()
-	}
-
-	t.Run("cluster=5,numValidators=4,numBrokenNodes=4,req=35,quorum=NO", func(t *testing.T) {
-		const clusterSize = 5
-		const numValidators = 4
-		const numBrokenNodes = 4
-		const numRequestsBeforeFailure = 10
-		const numRequestsAfterFailure = 25
-
-		testConsenseusReconnectingNodesNoQuorum(t, clusterSize, numValidators, numBrokenNodes, numRequestsBeforeFailure, numRequestsAfterFailure)
-	})
-
-	t.Run("cluster=15,numValidators=13,numBrokenNodes=12,req=35,quorum=NO", func(t *testing.T) {
-		testutil.RunHeavy(t)
-		const clusterSize = 15
-		const numValidators = 13
-		const numBrokenNodes = 12
-		const numRequestsBeforeFailure = 10
-		const numRequestsAfterFailure = 25
-
-		testConsenseusReconnectingNodesNoQuorum(t, clusterSize, numValidators, numBrokenNodes, numRequestsBeforeFailure, numRequestsAfterFailure)
-	})
-
-	t.Run("cluster=4,numValidators=3,numBrokenNodes=1,req=35,quorum=YES", func(t *testing.T) {
-		const clusterSize = 4
-		const numValidators = 3
-		const numBrokenNodes = 1
-		const numRequestsBeforeFailure = 10
-		const numRequestsAfterFailure = 25
-
-		testConsenseusReconnectingNodesHighQuorum(t, clusterSize, numValidators, numBrokenNodes, numRequestsBeforeFailure, numRequestsAfterFailure)
-	})
-
-	// TODO this seems to be failing
-	t.Run("cluster=12,numValidators=10,numBrokenNodes=4,req=35,quorum=YES", func(t *testing.T) {
-		testutil.RunHeavy(t)
-		const clusterSize = 12
-		const numValidators = 10
-		const numBrokenNodes = 4
-		const numRequestsBeforeFailure = 10
-		const numRequestsAfterFailure = 25
-
-		testConsenseusReconnectingNodesHighQuorum(t, clusterSize, numValidators, numBrokenNodes, numRequestsBeforeFailure, numRequestsAfterFailure)
-	})
-}
-
-func runTestOneFailingNodeAfterTheOther(t *testing.T, clusterSize, numValidators, numBrokenNodes, numRequestsEachStep int) {
-	quorum := (2*numValidators)/3 + 1
-
-	t.Logf("Quorum: %v", quorum)
-	t.Logf("Maximum allowed broken nodes: %v", numValidators-quorum)
-
-	requestCounter := 0
-	brokenNodes := 0
-
-	env := initializeStabilityTest(t, numValidators, clusterSize)
-	env.setSabotageValidators(numBrokenNodes)
-
-	t.Cleanup(func() {
-		// This hook is just a safety measure to unfreeze all nodes when an error happens. Otherwise they stay in a zombie mode after the tests ended.
-		if env != nil {
-			env.unfreezeNodes()
-		}
-	})
-
-	t.Logf("Nodes to break: %v", env.SabotageList)
-
-	for _, nodeID := range env.SabotageList {
-		requestCounter += numRequestsEachStep
-		go env.sendRequests(numRequestsEachStep, time.Millisecond*1)
-
-		time.Sleep(time.Millisecond * 1000)
-		env.chainEnv.Clu.FreezeNode(nodeID)
-		brokenNodes++
-
-		t.Logf("on broken node: %v(%v), quorum=%v, match=%v", nodeID, brokenNodes, numValidators-quorum, brokenNodes >= numValidators-quorum)
-		if brokenNodes >= numValidators-quorum {
-			// Wait and validate that not all messages have arrived
-			time.Sleep(15 * time.Second)
-
-			counter := env.chainEnv.getNativeContractCounter(nativeIncCounterSCHname)
-			require.NotEqual(env.chainEnv.t, requestCounter, int(counter))
-
-			break
-		} else {
-			t.Log("Waiting for requests to come in")
-			waitUntil(t, env.chainEnv.counterEquals(int64(requestCounter)), env.getActiveNodeList(), 60*time.Second, "incCounter matches expectation")
-			counter := env.chainEnv.getNativeContractCounter(nativeIncCounterSCHname)
-
-			t.Logf("Seems good? %v", counter)
-		}
-	}
-
-	// Either too many nodes are down now and the process has stopped, or quorum is still fine, requiring no further interaction.
-
-	counter := env.chainEnv.getNativeContractCounter(nativeIncCounterSCHname)
-
-	t.Logf("Counter after first iteration: %v", counter)
-
-	if counter == int64(requestCounter) {
-		return
-	}
-
-	t.Logf("Counter does not match requestCounter: %v", requestCounter)
-
-	for _, nodeID := range env.SabotageList {
-		env.chainEnv.Clu.UnfreezeNode(nodeID)
-
-		time.Sleep(time.Second * 15)
-		counter = env.chainEnv.getNativeContractCounter(nativeIncCounterSCHname)
-
-		if counter == int64(requestCounter) {
-			t.Logf("After unfreezing, the counter matches the requestCounter: %v", requestCounter)
-
-			break
-		}
-	}
-
-	require.Equal(t, requestCounter, int(counter))
-	time.Sleep(10 * time.Second) // wait for nodes to be properly shutdown before starting a new test
-}
-
-func TestOneFailingNodeAfterTheOther(t *testing.T) {
-	// In this test one node after the other gets disabled by either killing or freezing.
-	// Until quorum is unreachable, messages should all be flowing normally.
-
-	if testing.Short() {
-		t.SkipNow()
-	}
-
-	// Windows does not support freezing with SIGSTOP, we skip those for now.
-	if runtime.GOOS == OSWindows {
-		t.Skip()
-	}
-	t.Run("cluster=5,numValidators=4,numBrokenNodes=1", func(t *testing.T) {
-		const clusterSize = 5
-		const numValidators = 4
-		const numBrokenNodes = 1
-		const numRequestsEachStep = 10
-
-		runTestOneFailingNodeAfterTheOther(t, clusterSize, numValidators, numBrokenNodes, numRequestsEachStep)
-	})
-
-	t.Run("cluster=9,numValidators=6,numBrokenNodes=1", func(t *testing.T) {
-		const clusterSize = 5
-		const numValidators = 4
-		const numBrokenNodes = 1
-		const numRequestsEachStep = 10
-
-		runTestOneFailingNodeAfterTheOther(t, clusterSize, numValidators, numBrokenNodes, numRequestsEachStep)
-	})
-
-	t.Run("cluster=10,numValidators=9,numBrokenNodes=2", func(t *testing.T) {
-		const clusterSize = 10
-		const numValidators = 9
-		const numBrokenNodes = 2
-		const numRequestsEachStep = 10
-
-		runTestOneFailingNodeAfterTheOther(t, clusterSize, numValidators, numBrokenNodes, numRequestsEachStep)
 	})
 }
