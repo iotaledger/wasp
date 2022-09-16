7b0774f8f537c8e053a85615b86f6cf5f6b1bd5e
Lukas Möller <lukas.moeller@iota.org>
2022-09-16 12:22:20 +0200

    Update waypoint files, update faucet and withdraw tool

--- tools/evm_deposit_frontend/networks.ts
+++ tools/evm_deposit_frontend/networks.ts
@@ -4,11 +4,13 @@ export const networkOptions = [
     text: 'Shimmer Beta Network',
     apiEndpoint: 'https://api.testnet.shimmer.network',
     faucetEndpoint: 'https://faucet.testnet.shimmer.network',
+    chainAddress: 'rms1pzc5j8hq7ahgw6h9mn3jaq9wuv3zp0e2lxydj9a5f6yuvqhsvleyyym9f4z',
   },
   {
     id: 1,
     text: 'Custom settings',
     apiEndpoint: 'http://localhost:14265',
     faucetEndpoint: 'http://localhost:8091',
+    chainAddress: '',
   },
 ];
