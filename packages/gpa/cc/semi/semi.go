ca18e47d5be0542ee8f876e3ce2a8a12f289f415
Karolis Petrauskas <karolis.petrauskas@erisata.lt>
2022-09-28 20:03:36 +0300

    Semi random CC optimization.

--- packages/gpa/cc/semi/semi.go
+++ packages/gpa/cc/semi/semi.go
@@ -4,4 +4,73 @@
 // semi package implements a Common Coin (CC) that produces deterministic
 // values only for some of the rounds. I.e. only every 5th round will give a
 // real non-deterministic CC.
-package semi // TODO: ...
+package semi
+
+import (
+	"fmt"
+
+	"golang.org/x/xerrors"
+
+	"github.com/iotaledger/wasp/packages/gpa"
+)
+
+type ccSemi struct {
+	target gpa.GPA
+	index  int
+	output *bool
+}
+
+var _ gpa.GPA = &ccSemi{}
+
+func New(index int, target gpa.GPA) gpa.GPA {
+	return &ccSemi{index: index, target: target}
+}
+
+func (cc *ccSemi) Input(input gpa.Input) gpa.OutMessages {
+	if input != nil {
+		panic(xerrors.Errorf("input must be nil"))
+	}
+	mod5 := cc.index % 5
+	if mod5 < 2 {
+		coin := true
+		cc.output = &coin
+		return nil
+	}
+	if mod5 < 4 {
+		coin := false
+		cc.output = &coin
+		return nil
+	}
+	return cc.target.Input(input)
+}
+
+func (cc *ccSemi) Message(msg gpa.Message) gpa.OutMessages {
+	if cc.output != nil {
+		return nil
+	}
+	msgs := cc.target.Message(msg)
+	out := cc.target.Output()
+	if out != nil {
+		cc.output = out.(*bool)
+	}
+	return msgs
+}
+
+func (cc *ccSemi) Output() gpa.Output {
+	if cc.output == nil {
+		return nil // Untyped nil.
+	}
+	return cc.output
+}
+
+func (cc *ccSemi) StatusString() string {
+	if cc.output != nil {
+		// Try produce compact output.
+		return fmt.Sprintf("{CC:semi, index=%v, output=%v}", cc.index, *cc.output)
+	}
+	return fmt.Sprintf("{CC:semi, index=%v, output=%v, target=%v}", cc.index, cc.output, cc.target.StatusString())
+}
+
+func (cc *ccSemi) UnmarshalMessage(data []byte) (gpa.Message, error) {
+	return cc.target.UnmarshalMessage(data)
+}
