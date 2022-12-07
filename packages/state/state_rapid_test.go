e184d5935c71af16995d4f1e3f82393525b8517a
Karolis Petrauskas <karolis.petrauskas@erisata.lt>
2022-12-07 14:54:26 +0200

    Debugging the state.

--- packages/state/state_rapid_test.go
+++ packages/state/state_rapid_test.go
@@ -100,122 +100,18 @@ func TestRapid(t *testing.T) {
 	rapid.Check(t, rapid.Run[*stateSM]())
 }
 
-// === RUN   TestRapid
-//
-//	state_rapid_test.go:97: [rapid] failed after 3 tests: (*T).FailNow() called
-//	    To reproduce, specify -run="TestRapid" -rapid.failfile="testdata/rapid/TestRapid/TestRapid-20221207135045-108636.fail" (or -rapid.seed=16363909978550960135)
-//	    Failed test output:
-//	state_rapid_test.go:97: [rapid] draw action: "KVSet"
-//	state_rapid_test.go:32: [rapid] draw key: 0x0
-//	state_rapid_test.go:33: [rapid] draw val: 0x0
-//	state_rapid_test.go:97: [rapid] draw action: "CommitAddEmpty"
-//	state_rapid_test.go:97: [rapid] draw action: "KVSet"
-//	state_rapid_test.go:32: [rapid] draw key: 0x1
-//	state_rapid_test.go:33: [rapid] draw val: 0x0
-//	state_rapid_test.go:97: [rapid] draw action: "CommitAddEmpty"
-//	state_rapid_test.go:79:
-//	            Error Trace:    wasp/packages/state/state_rapid_test.go:79
-//	                            wasp/packages/state/synced_map.go:79
-//	                            wasp/packages/state/mapdb.go:59
-//	                            wasp/packages/state/state_rapid_test.go:76
-//	                            wasp/packages/state/state_rapid_test.go:46
-//	                            wasp/packages/state/statemachine.go:174
-//	                            wasp/packages/state/statemachine.go:149
-//	                            wasp/packages/state/statemachine.go:83
-//	                            wasp/packages/state/engine.go:319
-//	                            wasp/packages/state/engine.go:201
-//	                            wasp/packages/state/engine.go:93
-//	                            wasp/packages/state/state_rapid_test.go:97
-//	            Error:          Should be true
-//	            Test:           TestRapid
-//	            Messages:       Should have key [0]
-//
-// --- FAIL: TestRapid (4.17s)
-// FAIL
-// FAIL    github.com/iotaledger/wasp/packages/state       4.189s
-// FAIL
-func TestRapidReproduced(t *testing.T) { // TODO: Fails to reproduce....
-	cmp := func(model kvstore.KVStore, reader kv.KVStoreReader) {
-		require.NoError(t, model.Iterate(kvstore.EmptyPrefix, func(key, value kvstore.Value) bool {
-			draftHasVal, err := reader.Has(kv.Key(key))
-			require.NoError(t, err)
-			require.True(t, draftHasVal, "Should have key %v", key)
-			draftValue, err := reader.Get(kv.Key(key))
-			require.NoError(t, err)
-			require.Equal(t, value, draftValue, "Values for key %v should be equal", key)
-			return true
-		}))
-		require.NoError(t, reader.Iterate(kv.EmptyPrefix, func(key kv.Key, value kvstore.Value) bool {
-			modelHasVal, err := model.Has([]byte(key))
-			require.NoError(t, err)
-			require.True(t, modelHasVal, "Should have key %v", key)
-			modelValue, err := model.Get([]byte(key))
-			require.NoError(t, err)
-			require.Equal(t, value, modelValue, "Values for key %v should be equal", key)
-			return true
-		}))
-	}
-
-	var err error
-	// XXX: Init
-	store := state.NewStore(mapdb.NewMapDB())
-	draft := store.NewOriginStateDraft()
-	model := mapdb.NewMapDB()
-	cmp(model, draft)
-	// XXX: KVSet 0=>0
-	draft.Set(kv.Key([]byte{0}), []byte{0})
-	model.Set([]byte{0}, []byte{0})
-	cmp(model, draft)
-	// XXX: CommitAddEmpty
-	block := store.Commit(draft)
-	blockState, err := store.StateByTrieRoot(block.TrieRoot())
-	require.NoError(t, err)
-	cmp(model, blockState)
-	draft, err = store.NewEmptyStateDraft(block.L1Commitment())
-	require.NoError(t, err)
-	cmp(model, draft)
-	// XXX: KVSet 1=>0
-	draft.Set(kv.Key([]byte{1}), []byte{0})
-	model.Set([]byte{1}, []byte{0})
-	cmp(model, draft)
-	// XXX: CommitAddEmpty
-	block = store.Commit(draft)
-	blockState, err = store.StateByTrieRoot(block.TrieRoot())
-	require.NoError(t, err)
-	cmp(model, blockState)
-	draft, err = store.NewEmptyStateDraft(block.L1Commitment())
-	require.NoError(t, err)
-	cmp(model, draft)
-	//////
-	require.NoError(t, err)
-	has, err := blockState.Has(kv.Key([]byte{0}))
-	require.NoError(t, err)
-	require.True(t, has)
-	has, err = blockState.Has(kv.Key([]byte{1}))
-	require.NoError(t, err)
-	require.True(t, has)
-}
-
 func TestRapidReproduced2(t *testing.T) {
 	var err error
-	// XXX: Init
 	store := state.NewStore(mapdb.NewMapDB())
 	draft := store.NewOriginStateDraft()
-	model := mapdb.NewMapDB()
-	// XXX: KVSet 0x0=>0
 	draft.Set(kv.Key([]byte{0}), []byte{0})
-	model.Set([]byte{0x0}, []byte{0})
-	// XXX: KVSet 0x1=>0
-	draft.Set(kv.Key([]byte{0}), []byte{0})
-	model.Set([]byte{0x1}, []byte{0})
-	// XXX: KVSet 0x16=>0
-	draft.Set(kv.Key([]byte{0}), []byte{0})
-	model.Set([]byte{0x16}, []byte{0})
-	// XXX: CommitAddEmpty
+	draft.Set(kv.Key([]byte{1}), []byte{0})
+	draft.Set(kv.Key([]byte{0x10}), []byte{0})
+	//
 	block := store.Commit(draft)
 	blockState, err := store.StateByTrieRoot(block.TrieRoot())
 	require.NoError(t, err)
-	//////
+	//
 	require.NoError(t, err)
 	has, err := blockState.Has(kv.Key([]byte{0}))
 	require.NoError(t, err)
@@ -223,7 +119,7 @@ func TestRapidReproduced2(t *testing.T) {
 	has, err = blockState.Has(kv.Key([]byte{1}))
 	require.NoError(t, err)
 	require.True(t, has)
-	has, err = blockState.Has(kv.Key([]byte{16}))
+	has, err = blockState.Has(kv.Key([]byte{0x10}))
 	require.NoError(t, err)
 	require.True(t, has)
 }
