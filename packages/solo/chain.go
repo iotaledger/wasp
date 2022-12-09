a3180a67ff350ca160eeab5f90bf1a34b1a327f8
Lukas Möller <lukas.moeller@iota.org>
2022-12-09 14:54:41 +0100

    Add blocklog view calls

--- packages/solo/chain.go
+++ packages/solo/chain.go
@@ -452,7 +452,7 @@ func (ch *Chain) GetRequestReceiptsForBlock(blockIndex ...uint32) []*blocklog.Re
 	return ret
 }
 
-// GetRequestIDsForBlock returns return the list of requestIDs settled in a particular block
+// GetRequestIDsForBlock returns the list of requestIDs settled in a particular block
 func (ch *Chain) GetRequestIDsForBlock(blockIndex uint32) []isc.RequestID {
 	res, err := ch.CallView(blocklog.Contract.Name, blocklog.ViewGetRequestIDsForBlock.Name,
 		blocklog.ParamBlockIndex, blockIndex)
