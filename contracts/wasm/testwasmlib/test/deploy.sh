a0acb2c224d381aa7139af55d6cf312c2f7b3cf3
Yang Hau <yuanyanghau@gmail.com>
2023-01-09 11:32:13 +0800

    refactor: Add bash deploy script

---
+++ contracts/wasm/testwasmlib/test/deploy.sh
@@ -0,0 +1,12 @@
+wasp-cli init
+wasp-cli request-funds
+wasp-cli chain deploy --committee=0 --quorum=1 --chain=mychain --description="My chain"
+wasp-cli chain deposit base:500000000
+wasp-cli balance
+wasp-cli chain balance
+wasp-cli chain deploy-contract wasmtime testwasmlib "Test WasmLib" ../rs/testwasmlibwasm/pkg/testwasmlibwasm_bg.wasm
+wasp-cli chain post-request -s testwasmlib random
+wasp-cli chain call-view testwasmlib getRandom | wasp-cli decode string random uint64
+wasp-cli chain balance
+wasp-cli chain list-accounts
+wasp-cli check-versions
