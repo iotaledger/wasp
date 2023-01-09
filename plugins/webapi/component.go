f71e9c068290e6f83a4b56ac01baf089f62dc462
Jorge Silva <jorgesilva.me@gmail.com>
2023-01-09 18:10:24 +0000

    add params for read/write timeout

--- plugins/webapi/component.go
+++ plugins/webapi/component.go
@@ -102,8 +102,9 @@ func provide(c *dig.Container) error {
 			nil,
 			ParamsWebAPI.DebugRequestLoggerEnabled,
 		)
-		e.Server.ReadTimeout = 20 * time.Second
-		e.Server.WriteTimeout = 30 * time.Second
+
+		e.Server.ReadTimeout = ParamsWebAPI.ReadTimeout
+		e.Server.WriteTimeout = ParamsWebAPI.WriteTimeout
 
 		e.HidePort = true
 		e.HTTPErrorHandler = httperrors.HTTPErrorHandler
