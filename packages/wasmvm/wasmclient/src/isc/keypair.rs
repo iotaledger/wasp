c9c617580969463d431a0d5750924489d567b9ec
Yang Hau <yuanyanghau@gmail.com>
2023-01-17 15:31:08 +0700

    feat: Impl KeyPair::address

--- packages/wasmvm/wasmclient/src/isc/keypair.rs
+++ packages/wasmvm/wasmclient/src/isc/keypair.rs
@@ -1,7 +1,9 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use crypto::hashes::{blake2b::Blake2b256, Digest};
 use crypto::signatures::ed25519;
+use wasmlib::*;
 
 pub struct KeyPair {
     private_key: ed25519::SecretKey,
@@ -9,6 +11,13 @@ pub struct KeyPair {
 }
 
 impl KeyPair {
+    pub fn address(&self) -> ScAddress {
+        let mut addr: Vec<u8> = Vec::with_capacity(wasmlib::SC_LENGTH_ED25519);
+        addr[0] = wasmlib::SC_ADDRESS_ED25519;
+        let hash = Blake2b256::digest(self.public_key.to_bytes());
+        addr.copy_from_slice(&hash[..]);
+        return wasmlib::address_from_bytes(&addr);
+    }
     pub fn sign(&self, data: &[u8]) -> Vec<u8> {
         return self.private_key.sign(data).to_bytes().to_vec();
     }
