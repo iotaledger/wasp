640553ae6867fb5c9f90f3b64176ec91cb5f56cd
Karolis Petrauskas <karolis.petrauskas@erisata.lt>
2023-02-02 21:21:09 +0200

    Count LI votes with >= instead ==.

--- packages/chain/cmtLog/var_log_index_test.go
+++ packages/chain/cmtLog/var_log_index_test.go
@@ -39,6 +39,45 @@ func TestVarLogIndex(t *testing.T) {
 	require.Equal(t, nextLI, vliLI)
 }
 
+func TestVarLogIndexV2(t *testing.T) {
+	log := testlogger.NewLogger(t)
+	defer log.Sync()
+	n := 4
+	f := 1
+	//
+	ao := randomAliasOutputWithID()
+	nodeIDs := gpa.MakeTestNodeIDs(4)
+	initLI := NilLogIndex().Next()
+	//
+	vli := NewVarLogIndex(nodeIDs, n, f, initLI, log)
+	vliValueLI := func() LogIndex {
+		li, _ := vli.Value()
+		return li
+	}
+	li15 := LogIndex(15)
+	li16 := LogIndex(16)
+	li18 := LogIndex(18)
+	require.Equal(t, initLI, vliValueLI())
+
+	msgWithSender := func(sender gpa.NodeID, li LogIndex) *msgNextLogIndex {
+		msg := newMsgNextLogIndex(nodeIDs[0], li, ao)
+		msg.SetSender(sender)
+		return msg
+	}
+
+	vli.MsgNextLogIndexReceived(msgWithSender(nodeIDs[0], li15))
+	require.Equal(t, initLI, vliValueLI())
+
+	vli.MsgNextLogIndexReceived(msgWithSender(nodeIDs[1], li18))
+	require.Equal(t, initLI, vliValueLI())
+
+	vli.MsgNextLogIndexReceived(msgWithSender(nodeIDs[2], li16))
+	require.Equal(t, li15, vliValueLI())
+
+	vli.MsgNextLogIndexReceived(msgWithSender(nodeIDs[3], li15))
+	require.Equal(t, li15, vliValueLI())
+}
+
 func randomAliasOutputWithID() *isc.AliasOutputWithID {
 	outputID := testiotago.RandOutputID()
 	aliasOutput := &iotago.AliasOutput{}
