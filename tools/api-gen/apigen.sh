e66ce66c26a0cdd78e7ede065bd6db09b3225737
Lukas Möller <lukas.moeller@iota.org>
2023-02-06 23:16:25 +0100

    update client api

--- tools/api-gen/apigen.sh
+++ tools/api-gen/apigen.sh
@@ -3,4 +3,8 @@
 GIT_REF_TAG=$(git describe --tags)
 BUILD_LD_FLAGS="-X=github.com/iotaledger/wasp/core/app.Version=$GIT_REF_TAG"
 
-go run -ldflags="$BUILD_LD_FLAGS" ./main.go "$@"
+# To generate with the current commit version:
+# go run -ldflags="$BUILD_LD_FLAGS" ./main.go "$@"
+
+# During development the version is unset, therefore 0 to not commit a new api client each time.
+go run ./main.go "$@"
