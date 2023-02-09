e9bf24fa01516c5f1b08588d22fb0d63b650b097
Lukas Möller <lukas.moeller@iota.org>
2023-02-09 19:21:33 +0100

    Fix hostname path in client creation

--- clients/apiextensions/client.go
+++ clients/apiextensions/client.go
@@ -2,21 +2,14 @@ package apiextensions
 
 import (
 	"context"
-	"net/url"
 
 	"github.com/iotaledger/wasp/clients/apiclient"
 	"github.com/iotaledger/wasp/packages/kv/dict"
 )
 
 func WaspAPIClientByHostName(hostname string) (*apiclient.APIClient, error) {
-	parsed, err := url.Parse(hostname)
-	if err != nil {
-		return nil, err
-	}
-
 	config := apiclient.NewConfiguration()
-	config.Host = parsed.Host
-	config.Scheme = parsed.Scheme
+	config.Servers[0].URL = hostname
 
 	return apiclient.NewAPIClient(config), nil
 }
