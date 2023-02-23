f600e60b76e130fc009a731acbcc56d5b38fe7c5
Karolis Petrauskas <karolis.petrauskas@ext.iota.org>
2023-02-23 09:12:52 +0200

    Revert "Retry block post if it fails."
    This reverts commit 0a8da126401815a4fd31b122679129002d49b401.

--- packages/l1connection/l1connection.go
+++ packages/l1connection/l1connection.go
@@ -125,29 +125,18 @@ func (c *l1client) postBlock(ctx context.Context, block *iotago.Block) (*iotago.
 			return nil, fmt.Errorf("failed during local PoW: %w", err)
 		}
 	}
-
-	var postedBlock *iotago.Block
-	maxRetries := 10
-	for i := 0; ; i++ {
-		var err error
-		postedBlock, err = c.nodeAPIClient.SubmitBlock(ctx, block, parameters.L1().Protocol)
-		if err == nil {
-			break
-		}
-		if i >= maxRetries {
-			return nil, fmt.Errorf("failed to submit block: %w", err)
-		}
-		c.log.Debugf("Retry %v/%v failed to submit block: %w", i, maxRetries, err)
-		time.Sleep(1 * time.Second)
+	block, err := c.nodeAPIClient.SubmitBlock(ctx, block, parameters.L1().Protocol)
+	if err != nil {
+		return nil, fmt.Errorf("failed to submit block: %w", err)
 	}
 
-	blockID, err := postedBlock.ID()
+	blockID, err := block.ID()
 	if err != nil {
 		return nil, err
 	}
 	c.log.Infof("Posted blockID %v", blockID.ToHex())
 
-	return postedBlock, nil
+	return block, nil
 }
 
 // PostBlock sends a block (including tipselection and local PoW if necessary).
