96ddffbf541dc443c8384b5f8812ccb0b844d668
Jorge Silva <jorgesilva.me@gmail.com>
2023-03-02 16:25:05 +0000

    fix affordable gas calc

--- packages/vm/gas/policy_test.go
+++ packages/vm/gas/policy_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/iotaledger/iota.go/v3/tpkg"
+	"github.com/iotaledger/wasp/packages/util"
 )
 
 func TestFeePolicySerde(t *testing.T) {
@@ -29,4 +30,32 @@ func TestFeePolicySerde(t *testing.T) {
 	require.EqualValues(t, feePolicy.GasFeeTokenID, feePolicyBack.GasFeeTokenID)
 	require.EqualValues(t, feePolicy.ValidatorFeeShare, feePolicyBack.ValidatorFeeShare)
 	require.EqualValues(t, feePolicy.GasPerToken, feePolicyBack.GasPerToken)
+}
+
+func TestFeePolicyAffordableGas(t *testing.T) {
+	feePolicy := DefaultGasFeePolicy()
+	// needs 110 tokens for 1 gas
+	feePolicy.GasPerToken = util.Ratio32{A: 1, B: 110}
+
+	// map of [n tokens] expected gas
+	cases := map[uint64]int{
+		109: 0,
+		200: 1,
+		219: 1,
+		220: 2,
+	}
+	for tokens, expectedGas := range cases {
+		require.EqualValues(t, expectedGas, feePolicy.GasBudgetFromTokens(tokens))
+	}
+
+	// tokens charged for max gas
+	// map of [n tokens] expected tokens charged
+	cases2 := map[uint64]uint64{
+		109: 0,
+		110: 110,
+		111: 110,
+	}
+	for tokens, expected := range cases2 {
+		require.EqualValues(t, expected, feePolicy.FeeFromGas(feePolicy.GasBudgetFromTokens(tokens)))
+	}
 }
