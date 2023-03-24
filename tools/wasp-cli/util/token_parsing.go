4092e612e91e1473f81fe30239ff3875e69f3c80
Begoña Alvarez <balvarez@boxfish.studio>
2023-03-24 16:33:42 +0100

    revert: cli & isc changes

--- tools/wasp-cli/util/token_parsing.go
+++ tools/wasp-cli/util/token_parsing.go
@@ -19,52 +19,18 @@ func TokenIDFromString(s string) []byte {
 	return ret
 }
 
-func ParseNFTIDs(args []string) iotago.NFTIDs {
-	tokens := iotago.NFTIDs{}
-
-	for _, tr := range args {
-		nftId, err := iotago.ParseNFTAddressFromHexString(tr)
-
-		if err != nil {
-			continue
-		}
-
-		tokens = append(tokens, nftId.NFTID())
-	}
-
-	return tokens
-}
-
-func ParseAssetArgs(args []string) *isc.Assets {
+func ParseFungibleTokens(args []string) *isc.Assets {
 	tokens := isc.NewEmptyAssets()
 	for _, tr := range args {
 		parts := strings.Split(tr, ":")
 		if len(parts) != 2 {
-			log.Fatal("assets syntax: <token-id>:<amount>, [<token-id>:amount ...], " +
-				"<nft>:nftID, [<nft>:nftID ...] -- Example: base:100, " +
-				"nft:0xb7d0b62cafbae6c2c60215acce712b4d714c44fca211f65b9f6e51818cb59dcc")
+			log.Fatal("fungible tokens syntax: <token-id>:<amount>, <token-id:amount>... -- Example: base:100")
 		}
-
-		leftSide := strings.ToLower(parts[0])
-
-		// First check if the supplied argument is of id "nft"
-		if leftSide == "nft" {
-			rightSide := strings.ToLower(parts[1])
-			nft, err := iotago.ParseNFTAddressFromHexString(strings.TrimSpace(rightSide))
-
-			log.Check(err)
-			tokens.AddNFTs(nft.NFTID())
-			continue
-		}
-
-		// Otherwise handle native tokens
-
 		// In the past we would indicate base tokens as 'IOTA:nnn'
 		// Now we can simply use ':nnn', but let's keep it
 		// backward compatible for now and allow both
 		tokenIDBytes := isc.BaseTokenID
-
-		if leftSide != BaseTokenStr {
+		if strings.ToLower(parts[0]) != BaseTokenStr {
 			tokenIDBytes = TokenIDFromString(strings.TrimSpace(parts[0]))
 		}
 
