da510e1b83fca1c885e360760e84e59c4e0574b7
Julius Andrikonis <julius.andrikonis@ext.iota.org>
2023-03-27 10:24:15 +0300

    Use shrinking map instead of regular one

--- packages/chain/statemanager/smGPA/block_fetchers.go
+++ packages/chain/statemanager/smGPA/block_fetchers.go
@@ -1,39 +1,42 @@
 package smGPA
 
 import (
+	"github.com/iotaledger/hive.go/ds/shrinkingmap"
+	"github.com/iotaledger/wasp/packages/chain/statemanager/smGPA/smGPAUtils"
 	"github.com/iotaledger/wasp/packages/state"
 )
 
 type blockFetchersImpl struct {
-	fetchers map[state.BlockHash]blockFetcher
+	fetchers *shrinkingmap.ShrinkingMap[smGPAUtils.BlockKey, blockFetcher]
 }
 
 var _ blockFetchers = &blockFetchersImpl{}
 
 func newBlockFetchers() blockFetchers {
-	return &blockFetchersImpl{fetchers: make(map[state.BlockHash]blockFetcher)}
+	return &blockFetchersImpl{fetchers: shrinkingmap.New[smGPAUtils.BlockKey, blockFetcher]()}
 }
 
 func (bfiT *blockFetchersImpl) getSize() int {
-	return len(bfiT.fetchers)
+	return bfiT.fetchers.Size()
 }
 
 func (bfiT *blockFetchersImpl) addFetcher(fetcher blockFetcher) {
-	bfiT.fetchers[fetcher.getCommitment().BlockHash()] = fetcher
+	bfiT.fetchers.Set(smGPAUtils.NewBlockKey(fetcher.getCommitment()), fetcher)
 }
 
 func (bfiT *blockFetchersImpl) takeFetcher(commitment *state.L1Commitment) blockFetcher {
-	fetcher, ok := bfiT.fetchers[commitment.BlockHash()]
-	if !ok {
+	blockKey := smGPAUtils.NewBlockKey(commitment)
+	fetcher, exists := bfiT.fetchers.Get(blockKey)
+	if !exists {
 		return nil
 	}
-	delete(bfiT.fetchers, commitment.BlockHash())
+	bfiT.fetchers.Delete(blockKey)
 	return fetcher
 }
 
 func (bfiT *blockFetchersImpl) addCallback(commitment *state.L1Commitment, callback blockRequestCallback) bool {
-	fetcher, ok := bfiT.fetchers[commitment.BlockHash()]
-	if !ok {
+	fetcher, exists := bfiT.fetchers.Get(smGPAUtils.NewBlockKey(commitment))
+	if !exists {
 		return false
 	}
 	fetcher.addCallback(callback)
@@ -41,8 +44,8 @@ func (bfiT *blockFetchersImpl) addCallback(commitment *state.L1Commitment, callb
 }
 
 func (bfiT *blockFetchersImpl) addRelatedFetcher(commitment *state.L1Commitment, relatedFetcher blockFetcher) bool {
-	fetcher, ok := bfiT.fetchers[commitment.BlockHash()]
-	if !ok {
+	fetcher, exists := bfiT.fetchers.Get(smGPAUtils.NewBlockKey(commitment))
+	if !exists {
 		return false
 	}
 	fetcher.addRelatedFetcher(relatedFetcher)
@@ -52,15 +55,17 @@ func (bfiT *blockFetchersImpl) addRelatedFetcher(commitment *state.L1Commitment,
 func (bfiT *blockFetchersImpl) getCommitments() []*state.L1Commitment {
 	result := make([]*state.L1Commitment, bfiT.getSize())
 	i := 0
-	for _, fetcher := range bfiT.fetchers {
+	bfiT.fetchers.ForEach(func(_ smGPAUtils.BlockKey, fetcher blockFetcher) bool {
 		result[i] = fetcher.getCommitment()
 		i++
-	}
+		return true
+	})
 	return result
 }
 
 func (bfiT *blockFetchersImpl) cleanCallbacks() {
-	for _, fetcher := range bfiT.fetchers {
+	bfiT.fetchers.ForEach(func(_ smGPAUtils.BlockKey, fetcher blockFetcher) bool {
 		fetcher.cleanCallbacks()
-	}
+		return true
+	})
 }
