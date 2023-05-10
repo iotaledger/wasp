81e196a11a28012f31b580b0e1d13fad7d1e2801
Karolis Petrauskas <karolis.petrauskas@ext.iota.org>
2023-05-10 10:56:37 +0300

    Clean the input channel when discarding a pipe.

--- packages/util/pipe/pipe.go
+++ packages/util/pipe/pipe.go
@@ -108,6 +108,10 @@ func (ch *InfinitePipe[E]) infiniteBuffer() {
 		case ch.length <- ch.buffer.Length():
 		case <-ch.discardCh:
 			// Close the pipe without waiting for the values to be consumed.
+			for range ch.input { //nolint:revive
+				// Just clear the channel, to avoid blocking the senders.
+				// The channel itself should be closed already.
+			}
 			close(ch.output)
 			close(ch.length)
 			return
