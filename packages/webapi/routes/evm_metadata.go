9ff39aa62018c053bdc4847c410709c3c6dc17d2
Lukas Möller <lukas.moeller@iota.org>
2023-05-16 23:07:12 +0200

    Add metadata cli

---
+++ packages/webapi/routes/evm_metadata.go
@@ -0,0 +1,7 @@
+package routes
+
+const EVMJsonRPCPathSuffix = "/evm"
+const EVMJsonRPCRoute = "chains/:chainID" + EVMJsonRPCPathSuffix
+
+const EVMJsonWebSocketPathSuffix = "/evm/ws"
+const EVMJsonWebSocketRoute = "chains/:chainID" + EVMJsonWebSocketPathSuffix
