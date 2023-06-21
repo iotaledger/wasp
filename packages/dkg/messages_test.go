ec5fed00f5e8d8b8d709aead4cd1d728775484e1
Eric Hop <eric@iota.org>
2023-06-20 17:42:12 -0700

    Add missing error checks

--- packages/dkg/messages_test.go
+++ packages/dkg/messages_test.go
@@ -14,11 +14,17 @@ import (
 func TestInitiatorMsgSerialization(t *testing.T) {
 	hash1 := hashing.PseudoRandomHash(nil).Bytes()
 	pubKey1, err := cryptolib.PublicKeyFromBytes(hash1)
+	require.NoError(t, err)
 	hash2 := hashing.PseudoRandomHash(nil).Bytes()
 	pubKey2, err := cryptolib.PublicKeyFromBytes(hash2)
+	require.NoError(t, err)
 	hash3 := hashing.PseudoRandomHash(nil).Bytes()
 	pubKey3, err := cryptolib.PublicKeyFromBytes(hash3)
 	require.NoError(t, err)
+
+	// set up a random initiatorInitMsg
+	// make sure to fill in all the fields that get serialized
+	// first test with an empty peerPubs array
 	msg := &initiatorInitMsg{
 		step:         69,
 		dkgRef:       "some text",
@@ -30,9 +36,11 @@ func TestInitiatorMsgSerialization(t *testing.T) {
 	}
 	rwutil.SerializationTest(t, msg, new(initiatorInitMsg))
 
+	//  test with a 1-item peerPubs array
 	msg.peerPubs = []*cryptolib.PublicKey{pubKey2}
 	rwutil.SerializationTest(t, msg, new(initiatorInitMsg))
 
+	//  test with a 3-item peerPubs array
 	msg.peerPubs = []*cryptolib.PublicKey{pubKey3, pubKey2, pubKey1}
 	rwutil.SerializationTest(t, msg, new(initiatorInitMsg))
 }
