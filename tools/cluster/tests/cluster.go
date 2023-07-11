806684535580afe5780929b3991b39e7cb39cb49
Yang Hau <yuanyanghau@gmail.com>
2023-07-11 17:44:49 +0530

    docs: Add README for cluster tests

--- tools/cluster/tests/cluster.go
+++ tools/cluster/tests/cluster.go
@@ -20,7 +20,7 @@ type waspClusterOpts struct {
 }
 
 // by default, when running the cluster tests we will automatically setup a private tangle,
-// however its possible to run the tests on any compatible network, by providing the L1 node configuration:
+// however it's possible to run the tests on any compatible network, by providing the L1 node configuration.
 // example:
 // go test -timeout 30m github.com/iotaledger/wasp/tools/cluster/tests -layer1-api="http://1.1.1.123:3000" -layer1-faucet="http://1.1.1.123:5000"
 var l1 = l1starter.New(flag.CommandLine, flag.CommandLine)
