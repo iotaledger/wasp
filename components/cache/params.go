11713adb8319ffd4073ee40e876110583d0d212d
shufps <shufps80@gmail.com>
2023-07-20 21:18:23 +0200

    fixed comments

--- components/cache/params.go
+++ components/cache/params.go
@@ -8,13 +8,13 @@ import (
 
 // ParametersDatabase contains the definition of the parameters used by the ParametersDatabase.
 type ParametersCache struct {
-	// Engine defines the used database engine (rocksdb/mapdb).
+	// CacheSize defines the maximum cache size
 	CacheSize string `default:"512MiB" usage:"cache size"`
 
-	// Engine defines the used database engine (rocksdb/mapdb).
+	// CacheStatsInterval is the interval for the statistics ticker
 	CacheStatsInterval time.Duration `default:"30s" usage:"interval for printing cache statistics"`
 
-	// DebugSkipHealthCheck defines whether to ignore the check for corrupted databases.
+	// CacheEnable enabled the cache
 	CacheEnabled bool `default:"true" usage:"enables or disables caching of states"`
 }
 
