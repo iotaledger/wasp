7c7aa27103fe09f5f6a8403933da96716ee88c69
cwarnerdev <138500512+cwarnerdev@users.noreply.github.com>
2023-08-14 04:56:36 -1000

    use hashing instead of LCG

--- tools/evm/iscutils/prng.sol
+++ tools/evm/iscutils/prng.sol
@@ -6,25 +6,19 @@ pragma solidity >=0.8.5;
 /// @notice This library is used to generate pseudorandom numbers
 /// @dev Not recommended for generating cryptographic secure randomness
 library PRNG {
-    /// @notice Parameters of the PRNG (specifically, a Linear Congruential Generator)
-    /// @dev These are constants required for generating the random number sequence
-    uint256 constant LCG_MULTIPLIER = 1103515245; // Multiplier in LCG
-    uint256 constant LCG_INCREMENT = 12345; // Increment in LCG
-    uint256 constant LCG_MODULUS = 2**31; // Modulus in LCG
-
     /// @dev Represents the state of the PRNG
     struct PRNGState {
-        uint256 state;
+        bytes32 state;
     }
 
     /// @notice Generate a new pseudorandom number
-    /// @dev Uses the formula of Linear Congruential Generator (LCG): new_state = (LCG_MULTIPLIER*state + LCG_INCREMENT) mod LCG_MODULUS
+    /// @dev Takes the current state, hashes it and returns the new state.
     /// @param self The PRNGState struct to use and alter the state
     /// @return The generated pseudorandom number
-    function generateRandomNumber(PRNGState storage self) public returns (uint256) {
-        require(self.state != 0, "Generator has not been initialized, state is zero.");
-        self.state = (LCG_MULTIPLIER * self.state + LCG_INCREMENT) % LCG_MODULUS;
-        return self.state;
+    function generateRandomNumber(PRNGState storage self) internal returns (uint256) {
+        require(self.state != bytes32(0), "state must be seeded first");
+        self.state = keccak256(abi.encodePacked(self.state));
+        return uint256(self.state);
     }
 
     /// @notice Seed the PRNG
@@ -32,8 +26,7 @@ library PRNG {
     /// @param self The PRNGState struct to update the state
     /// @param entropy The seed value (entropy)
     function seed(PRNGState storage self, bytes32 entropy) internal {
-        require(entropy != bytes32(0), "Entered entropy should not be zero");
-        self.state = uint256(entropy);
+        require(entropy != bytes32(0), "seed must not be zero");
+        self.state = entropy;
     }
 }
-
