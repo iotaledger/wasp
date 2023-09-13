d78ec27b8c78a720a9f0ca7db84d60f457ea40d4
Jorge Silva <jorgesilva.me@gmail.com>
2023-09-13 15:58:04 +0100

    refactor: add accounts.AgentIDFromKey

--- packages/vm/core/accounts/stateaccess.go
+++ packages/vm/core/accounts/stateaccess.go
@@ -4,8 +4,11 @@
 package accounts
 
 import (
+	"github.com/ethereum/go-ethereum/common"
+
 	"github.com/iotaledger/wasp/packages/isc"
 	"github.com/iotaledger/wasp/packages/kv"
+	"github.com/iotaledger/wasp/packages/kv/codec"
 	"github.com/iotaledger/wasp/packages/kv/subrealm"
 )
 
@@ -24,4 +27,25 @@ func (sa *StateAccess) Nonce(agentID isc.AgentID, chainID isc.ChainID) uint64 {
 
 func (sa *StateAccess) AccountExists(agentID isc.AgentID, chainID isc.ChainID) bool {
 	return accountExists(sa.state, agentID, chainID)
+}
+
+func AgentIDFromKey(key kv.Key, chainID isc.ChainID) (isc.AgentID, error) {
+	if len(key) < isc.ChainIDLength {
+		// short form saved (withoutChainID)
+		switch len(key) {
+		case 4:
+			hn, err := isc.HnameFromBytes([]byte(key))
+			if err != nil {
+				return nil, err
+			}
+			return isc.NewContractAgentID(chainID, hn), nil
+		case common.AddressLength:
+			var ethAddr common.Address
+			copy(ethAddr[:], []byte(key))
+			return isc.NewEthereumAddressAgentID(chainID, ethAddr), nil
+		default:
+			panic("bad key length")
+		}
+	}
+	return codec.DecodeAgentID([]byte(key))
 }
