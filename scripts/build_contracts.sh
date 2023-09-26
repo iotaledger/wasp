f8f7d86c5c7093fec28c237487ab47302ce4c19d
T1 <126980610+tadash10@users.noreply.github.com>
2023-09-26 22:07:24 +0000

    Update build_contracts.sh
    Here's a summary of the improvements made:
    
        Added set -e to make the script exit on any error.
        Simplified how the current and parent directories are determined.
        Added informative comments to describe each step of the script.
        Included error-checking after each significant operation to ensure the script doesn't proceed if something fails.
        Added a completion message at the end of the script for better feedback.

--- scripts/build_contracts.sh
+++ scripts/build_contracts.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
-CURRENT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-PARENT_DIR="$( builtin cd ${CURRENT_DIR}/.. >/dev/null 2>&1 ; pwd -P )"
-cd ${PARENT_DIR}
+set -e  # Exit on error
 
+# Determine the current script directory
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Determine the parent directory
+PARENT_DIR="$( dirname "$CURRENT_DIR" )"
+
+# Move to the parent directory
+cd "$PARENT_DIR"
+
+# Install Go packages in tools/schema
+echo "Installing Go packages in tools/schema..."
 cd tools/schema
 go install
-cd ${PARENT_DIR}
 
+# Move back to the parent directory
+cd "$PARENT_DIR"
+
+# Run 'make install'
+echo "Running 'make install'..."
 make install
+
+# Move to contracts/wasm/scripts
+echo "Moving to contracts/wasm/scripts..."
 cd contracts/wasm/scripts
+
+# Run cleanup.sh
+echo "Running cleanup.sh..."
 ./cleanup.sh
+
+# Run all_build.sh
+echo "Running all_build.sh..."
 ./all_build.sh
+
+# Run update_hardcoded.sh
+echo "Running update_hardcoded.sh..."
 ./update_hardcoded.sh
 
-cd ${CURRENT_DIR}
+# Move back to the original script directory
+cd "$CURRENT_DIR"
+
+echo "Script completed successfully."
