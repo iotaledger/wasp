7bd6cd419121edb1cdea2e91820e6b7dc61313db
Yang Hau <yuanyanghau@gmail.com>
2023-10-21 16:53:22 +0300

    fix: Fix wasmclient

--- packages/wasmvm/wasmclient/src/isc/keypair.rs
+++ packages/wasmvm/wasmclient/src/isc/keypair.rs
@@ -19,7 +19,7 @@ impl KeyPair {
         if seed_bytes.len() != 0 {
             seed.copy_from_slice(seed_bytes);
         }
-        let key = ed25519::SecretKey::from_bytes(seed);
+        let key = ed25519::SecretKey::from_bytes(&seed);
         let pub_key = key.public_key();
         return KeyPair {
             private_key: key,
@@ -42,7 +42,8 @@ impl KeyPair {
     pub fn verify(&self, data: &[u8], sig: &[u8]) -> bool {
         let mut sig_data = [0; 64];
         sig_data.copy_from_slice(sig);
-        self.public_key.verify(&Signature::from_bytes(sig_data), data)
+        self.public_key
+            .verify(&Signature::from_bytes(sig_data), data)
     }
 
     pub fn sub_seed(seed: &[u8], n: u64) -> Vec<u8> {
@@ -63,7 +64,7 @@ impl KeyPair {
 impl Clone for KeyPair {
     fn clone(&self) -> Self {
         return KeyPair {
-            private_key: ed25519::SecretKey::from_bytes(self.private_key.to_bytes()),
+            private_key: ed25519::SecretKey::from_bytes(&self.private_key.to_bytes()),
             public_key: self.public_key.clone(),
         };
     }
@@ -94,8 +95,14 @@ mod tests {
         println!("Publ2: {}", bytes_to_string(&pair2.public_key.to_bytes()));
         println!("Priv1: {}", bytes_to_string(&pair1.private_key.to_bytes()));
         println!("Priv2: {}", bytes_to_string(&pair2.private_key.to_bytes()));
-        assert_eq!(bytes_to_string(&pair1.public_key.to_bytes()), bytes_to_string(&pair2.public_key.to_bytes()));
-        assert_eq!(bytes_to_string(&pair2.private_key.to_bytes()), bytes_to_string(&pair2.private_key.to_bytes()));
+        assert_eq!(
+            bytes_to_string(&pair1.public_key.to_bytes()),
+            bytes_to_string(&pair2.public_key.to_bytes())
+        );
+        assert_eq!(
+            bytes_to_string(&pair2.private_key.to_bytes()),
+            bytes_to_string(&pair2.private_key.to_bytes())
+        );
     }
 
     #[test]
@@ -104,8 +111,14 @@ mod tests {
         let pair = KeyPair::new(&my_seed);
         println!("Publ: {}", bytes_to_string(&pair.public_key.to_bytes()));
         println!("Priv: {}", bytes_to_string(&pair.private_key.to_bytes()));
-        assert_eq!(bytes_to_string(&pair.public_key.to_bytes()), "0x30adc0bd555d56ed51895528e47dcb403e36e0026fe49b6ae59e9adcea5f9a87");
-        assert_eq!(bytes_to_string(&pair.private_key.to_bytes()), "0xa580555e5b84a4b72bbca829b4085a4725941f3b3702525f36862762d76c21f3");
+        assert_eq!(
+            bytes_to_string(&pair.public_key.to_bytes()),
+            "0x30adc0bd555d56ed51895528e47dcb403e36e0026fe49b6ae59e9adcea5f9a87"
+        );
+        assert_eq!(
+            bytes_to_string(&pair.private_key.to_bytes()),
+            "0xa580555e5b84a4b72bbca829b4085a4725941f3b3702525f36862762d76c21f3"
+        );
     }
 
     #[test]
@@ -114,8 +127,14 @@ mod tests {
         let pair = KeyPair::from_sub_seed(&my_seed, 0);
         println!("Publ: {}", bytes_to_string(&pair.public_key.to_bytes()));
         println!("Priv: {}", bytes_to_string(&pair.private_key.to_bytes()));
-        assert_eq!(bytes_to_string(&pair.public_key.to_bytes()), "0x40a757d26f6ef94dccee5b4f947faa78532286fe18117f2150a80acf2a95a8e2");
-        assert_eq!(bytes_to_string(&pair.private_key.to_bytes()), "0x24642f47bd363fbd4e05f13ed6c60b04c8a4cf1d295f76fc16917532bc4cd0af");
+        assert_eq!(
+            bytes_to_string(&pair.public_key.to_bytes()),
+            "0x40a757d26f6ef94dccee5b4f947faa78532286fe18117f2150a80acf2a95a8e2"
+        );
+        assert_eq!(
+            bytes_to_string(&pair.private_key.to_bytes()),
+            "0x24642f47bd363fbd4e05f13ed6c60b04c8a4cf1d295f76fc16917532bc4cd0af"
+        );
     }
 
     #[test]
@@ -124,8 +143,14 @@ mod tests {
         let pair = KeyPair::from_sub_seed(&my_seed, 1);
         println!("Publ: {}", bytes_to_string(&pair.public_key.to_bytes()));
         println!("Priv: {}", bytes_to_string(&pair.private_key.to_bytes()));
-        assert_eq!(bytes_to_string(&pair.public_key.to_bytes()), "0x120d2b26fc1b1d53bb916b8a277bcc2efa09e92c95be1a8fd5c6b3adbc795679");
-        assert_eq!(bytes_to_string(&pair.private_key.to_bytes()), "0xb83d28550d9ee5651796eeb36027e737f0d79495b56d3d8931c716f2141017c8");
+        assert_eq!(
+            bytes_to_string(&pair.public_key.to_bytes()),
+            "0x120d2b26fc1b1d53bb916b8a277bcc2efa09e92c95be1a8fd5c6b3adbc795679"
+        );
+        assert_eq!(
+            bytes_to_string(&pair.private_key.to_bytes()),
+            "0xb83d28550d9ee5651796eeb36027e737f0d79495b56d3d8931c716f2141017c8"
+        );
     }
 
     #[test]
@@ -144,7 +169,10 @@ mod tests {
         let my_seed = bytes_from_string(&MYSEED);
         let sub_seed = KeyPair::sub_seed(&my_seed, 0);
         println!("Seed: {}", bytes_to_string(&sub_seed));
-        assert_eq!(bytes_to_string(&sub_seed), "0x24642f47bd363fbd4e05f13ed6c60b04c8a4cf1d295f76fc16917532bc4cd0af");
+        assert_eq!(
+            bytes_to_string(&sub_seed),
+            "0x24642f47bd363fbd4e05f13ed6c60b04c8a4cf1d295f76fc16917532bc4cd0af"
+        );
     }
 
     #[test]
@@ -152,6 +180,9 @@ mod tests {
         let my_seed = bytes_from_string(&MYSEED);
         let sub_seed = KeyPair::sub_seed(&my_seed, 1);
         println!("Seed: {}", bytes_to_string(&sub_seed));
-        assert_eq!(bytes_to_string(&sub_seed), "0xb83d28550d9ee5651796eeb36027e737f0d79495b56d3d8931c716f2141017c8");
+        assert_eq!(
+            bytes_to_string(&sub_seed),
+            "0xb83d28550d9ee5651796eeb36027e737f0d79495b56d3d8931c716f2141017c8"
+        );
     }
 }
