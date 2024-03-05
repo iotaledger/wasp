556b7607203f0f0612c816c5a9c8c4d0f956eea1
Dr-Electron <dr-electr0n@protonmail.com>
2024-03-05 16:20:45 +0100

    Add solidity docs tool

---
+++ tools/evm/docs-generator/hardhat.config.iscmagic.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import 'solidity-docgen';
+import path from 'path';
+
+const rootPath = '../../../packages/vm/core/evm/';
+const cachePath = path.relative(path.resolve(rootPath), path.resolve('./cache'));
+const artifactsPath = path.relative(path.resolve(rootPath), path.resolve('./artifacts'));
+const outputDirPath = path.relative(path.resolve(rootPath), path.resolve('./docs/iscmagic'));
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  paths: {
+    sources: 'iscmagic',
+    root: rootPath,
+    cache: cachePath,
+    artifacts: artifactsPath,
+  },
+  docgen: { 
+    outputDir: outputDirPath,
+    pages: 'files',
+  },
+};
+
+export default config;
