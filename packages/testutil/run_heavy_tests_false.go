3aac8b217e1df27705c509e28c88c080ed69c2b9
Jorge Silva <jorgesilva.me@gmail.com>
2024-04-23 12:17:42 +0100

    lint fix

--- packages/testutil/run_heavy_tests_false.go
+++ packages/testutil/run_heavy_tests_false.go
@@ -5,6 +5,7 @@ package testutil
 
 import "testing"
 
+//nolint:gocritic // its not a test function, but gets called by other test functions
 func RunHeavy(t *testing.T) {
 	t.Logf("skipping heavy test %s", t.Name())
 	t.SkipNow()
