a90263847fb597ee0a7d3065a299be7f144862df
Lukas Möller <lukas.moeller@iota.org>
2024-06-03 18:01:51 +0200

    Update types and tests

--- packages/isc/sui/isc.go
+++ packages/isc/sui/isc.go
@@ -9,6 +9,8 @@ import (
 /*
   Structs are currently set up with the following assumptions:
   * Option<T> types are nullable types => (*T)
+    => Option<T> may require the `bcs:"optional"` tag.
+
   * "ID" and "UID" are for now both typed as ObjectID, the actual typing maybe needs to be reconsidered. On our end it maybe not make a difference.
   * Type "Bag" is not available as a proper type in Sui-Go. It needs to be considered if we will need this, as
 	=> Bag is a heterogeneous map, so it can hold key-value pairs of arbitrary types (map[any]any)
@@ -16,57 +18,67 @@ import (
 */
 
 // Related to: https://github.com/iotaledger/kinesis/tree/isc-models/dapps/isc/sources
-
+// Might change completely: https://github.com/iotaledger/iota/pull/370#discussion_r1617682560
 type Allowance struct {
-	CoinAmounts []uint64             `json:"coin_amounts"`
-	CoinTypes   []string             `json:"coin_types"`
-	NFTs        []sui_types.ObjectID `json:"nfts"`
+	CoinAmounts []uint64
+	CoinTypes   []string
+	NFTs        []sui_types.ObjectID
 }
 
-type Assets struct {
-	ID    sui_types.ObjectID `json:"id"`
-	Coins Bag                `json:"coins"`
-	NFTs  []NFT              `json:"nfts"`
+type Referent[T any] struct {
+	ID    sui_types.ObjectID
+	Value *T `bcs:"optional"`
+}
+
+type AssetBag struct {
+	ID   sui_types.ObjectID
+	Size uint64
 }
 
 type Anchor struct {
-	ID sui_types.ObjectID `json:"id"`
+	ID        sui_types.ObjectID
+	Assets    AssetBag
+	StateRoot []uint8
+}
+
+type Receipt struct {
+	RequestID sui_types.ObjectID
 }
 
 type RequestData struct {
-	ID        sui_types.ObjectID `json:"id"`
-	Contract  isc.Hname          `json:"contract"`
-	Function  isc.Hname          `json:"function"`
-	Args      [][]uint8          `json:"args"`
-	Allowance *Allowance         `json:"allowance"`
+	Contract isc.Hname
+	Function isc.Hname
+	Args     [][]uint8
 }
 
 type Request struct {
-	ID   sui_types.ObjectID `json:"id"`
-	Data RequestData        `json:"data"`
+	ID        sui_types.ObjectID
+	Sender    sui_types.SuiAddress
+	AssetsBag Referent[AssetBag] // Need to decide if we want to use this Referent wrapper as well. Could probably be of *AssetBag with `bcs:"optional`
+	Data      *RequestData       `bcs:"optional"`
 }
 
 // Related to: https://github.com/iotaledger/kinesis/blob/isc-models/crates/sui-framework/packages/stardust/sources/nft/irc27.move
 type IRC27MetaData struct {
-	Version           string                              `json:"version"`
-	MediaType         string                              `json:"media_type"`
-	URI               string                              `json:"uri"` // Actually of Type "Url" in SUI -> Create proper type?
-	Name              string                              `json:"name"`
-	CollectionName    *string                             `json:"collection_name"`
-	Royalties         Table[sui_types.SuiAddress, uint32] `json:"royalties"`
-	IssuerName        *string                             `json:"issuer_name"`
-	Description       *string                             `json:"description"`
-	Attributes        []string                            `json:"attributes"` // This is actually of Type VecSet which guarantees no duplicates. Not sure if we want to create a separate type for it. But we need to filter it to ensure no duplicates eventually.
-	NonStandardFields Table[string, string]               `json:"non_standard_fields"`
+	Version           string
+	MediaType         string
+	URI               string // Actually of type "Url" in SUI -> Create proper type?
+	Name              string
+	CollectionName    *string `bcs:"optional"`
+	Royalties         Table[sui_types.SuiAddress, uint32]
+	IssuerName        *string  `bcs:"optional"`
+	Description       *string  `bcs:"optional"`
+	Attributes        []string // This is actually of Type VecSet which guarantees no duplicates. Not sure if we want to create a separate type for it. But we need to filter it to ensure no duplicates eventually.
+	NonStandardFields Table[string, string]
 }
 
 // Related to: https://github.com/iotaledger/kinesis/blob/isc-models/crates/sui-framework/packages/stardust/sources/nft/nft.move
 
 type NFT struct {
-	ID                sui_types.ObjectID    `json:"id"`
-	LegacySender      *sui_types.SuiAddress `json:"legacy_sender"`
-	Metadata          *[]uint8              `json:"metadata"`
-	Tag               *[]uint8              `json:"tag"`
-	ImmutableIssuer   *sui_types.SuiAddress `json:"immutable_issuer"`
-	ImmutableMetadata IRC27MetaData         `json:"immutable_metadata"`
+	ID                sui_types.ObjectID
+	LegacySender      *sui_types.SuiAddress `bcs:"optional"`
+	Metadata          *[]uint8              `bcs:"optional"`
+	Tag               *[]uint8              `bcs:"optional"`
+	ImmutableIssuer   *sui_types.SuiAddress `bcs:"optional"`
+	ImmutableMetadata IRC27MetaData
 }
--- packages/isc/sui/sui_test.go
+++ packages/isc/sui/sui_test.go
@@ -1,10 +1,14 @@
 package sui
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"testing"
+	"time"
 
+	"github.com/fardream/go-bcs/bcs"
 	"github.com/stretchr/testify/require"
 
 	"github.com/iotaledger/wasp/sui-go/isc"
@@ -12,14 +16,25 @@ import (
 	"github.com/iotaledger/wasp/sui-go/sui"
 	"github.com/iotaledger/wasp/sui-go/sui/conn"
 	"github.com/iotaledger/wasp/sui-go/sui_signer"
+	"github.com/iotaledger/wasp/sui-go/sui_types"
 	"github.com/iotaledger/wasp/sui-go/utils"
 )
 
-func setupAndDeploy(t *testing.T) {
+type testSetup struct {
+	suiClient *sui.ImplSuiAPI
+	iscClient *isc.Client
+	signer    *sui_signer.Signer
+	packageID sui_types.PackageID
+	chain     *models.SuiTransactionBlockResponse
+}
+
+func setupAndDeploy(t *testing.T) testSetup {
 	suiClient, signer := sui.NewTestSuiClientWithSignerAndFund(conn.LocalnetEndpointUrl, sui_signer.TEST_MNEMONIC)
 	client := isc.NewIscClient(suiClient)
 
-	modules, err := utils.MoveBuild(utils.GetGitRoot() + "/../kinesis/dapps/isc/")
+	printCoinsForAddress(t, suiClient, *signer.Address)
+
+	modules, err := utils.MoveBuild(utils.GetGitRoot() + "/sui-go/isc/contracts/isc/sources")
 	require.NoError(t, err)
 
 	fmt.Printf("%s", signer.Address.String())
@@ -42,7 +57,12 @@ func setupAndDeploy(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, txnResponse.Effects.Data.IsSuccess())
 
-	packageID := txnResponse.GetPublishedPackageID()
+	t.Log("Before StartNewChain")
+	printCoinsForAddress(t, suiClient, *signer.Address)
+
+	packageID, err := txnResponse.GetPublishedPackageID()
+	require.NoError(t, err)
+
 	t.Log("packageID: ", packageID)
 
 	startNewChainRes, err := client.StartNewChain(
@@ -58,8 +78,97 @@ func setupAndDeploy(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, startNewChainRes.Effects.Data.IsSuccess())
 	t.Logf("StartNewChain response: %#v\n", startNewChainRes)
+
+	printCoinsForAddress(t, suiClient, *signer.Address)
+
+	return testSetup{
+		suiClient: suiClient,
+		signer:    signer,
+		chain:     startNewChainRes,
+		iscClient: client,
+		packageID: *packageID,
+	}
+}
+
+func jsonPrettyPrint(in string) string {
+	var out bytes.Buffer
+	err := json.Indent(&out, []byte(in), "", "\t")
+	if err != nil {
+		return in
+	}
+	return out.String()
+}
+
+func printCoinsForAddress(t *testing.T, suiClient *sui.ImplSuiAPI, address sui_types.SuiAddress) {
+	coins, err := suiClient.GetSuiCoinsOwnedByAddress(context.Background(), &address)
+	require.NoError(t, err)
+
+	t.Logf("Coins for address: %v", address.String())
+	for _, v := range coins {
+		t.Logf("COIN -> %v: %v (%v)", v.CoinObjectID, v.Balance, v.CoinType)
+	}
+}
+
+func printGasCoinsForAddress(t *testing.T, suiClient *sui.ImplSuiAPI, address sui_types.SuiAddress) {
+	coins, err := suiClient.GetCoinObjectForGasFee(context.Background(), &address, 10000, sui.DefaultGasBudget)
+	require.NoError(t, err)
+
+	t.Logf("Gas for address: %v", address.String())
+	for _, v := range coins {
+		t.Logf("GAS -> %v: %v sui", v.CoinObjectID, v.Balance)
+	}
+}
+
+func GetAnchor(t *testing.T, setup testSetup) Anchor {
+	anchor, err := setup.suiClient.GetObject(context.Background(), &setup.chain.ObjectChanges[1].Data.Created.ObjectID, &models.SuiObjectDataOptions{
+		ShowType:                true,
+		ShowContent:             true,
+		ShowBcs:                 true,
+		ShowOwner:               true,
+		ShowPreviousTransaction: true,
+		ShowStorageRebate:       true,
+		ShowDisplay:             true,
+	})
+	require.NoError(t, err)
+
+	decodedAnchor := Anchor{}
+	_, err = bcs.Unmarshal(anchor.Data.Bcs.Data.MoveObject.BcsBytes.Data(), &decodedAnchor)
+	require.NoError(t, err)
+
+	return decodedAnchor
 }
 
 func TestMinimalClient(t *testing.T) {
-	setupAndDeploy(t)
+	setup := setupAndDeploy(t)
+
+	suiUserClient, userSigner := sui.NewTestSuiClientWithSignerAndFund(conn.LocalnetEndpointUrl, sui_signer.TEST_CLIENT_MNEMONIC)
+	iscUserClient := isc.NewIscClient(suiUserClient)
+
+	printCoinsForAddress(t, setup.suiClient, *setup.signer.Address)
+	printCoinsForAddress(t, suiUserClient, *userSigner.Address)
+
+	printGasCoinsForAddress(t, setup.suiClient, *setup.signer.Address)
+	printGasCoinsForAddress(t, suiUserClient, *userSigner.Address)
+
+	anchor := GetAnchor(t, setup)
+	t.Log(anchor)
+
+	coins, err := setup.suiClient.GetSuiCoinsOwnedByAddress(context.Background(), userSigner.Address)
+	require.NoError(t, err)
+
+	_, err = iscUserClient.SendCoin(context.Background(), userSigner, &setup.packageID, &anchor.ID, coins[0].CoinType, coins[0].CoinObjectID, coins[1].CoinObjectID, sui.DefaultGasBudget, &models.SuiTransactionBlockResponseOptions{})
+	require.NoError(t, err)
+
+	contractCoins, err := setup.suiClient.GetSuiCoinsOwnedByAddress(context.Background(), setup.signer.Address)
+	require.NoError(t, err)
+
+	time.Sleep(5 * time.Second)
+
+	_, err = setup.iscClient.ReceiveCoin(context.Background(), setup.signer, &setup.packageID, &anchor.ID, coins[0].CoinType, coins[0].CoinObjectID, contractCoins[0].CoinObjectID, sui.DefaultGasBudget, &models.SuiTransactionBlockResponseOptions{})
+	require.NoError(t, err)
+
+	printCoinsForAddress(t, setup.suiClient, *setup.signer.Address)
+
+	anchor = GetAnchor(t, setup)
+	t.Log(anchor)
 }
