011886bdad2f104031f372e3871771e034c5b3e3
Julius Andrikonis <julius@erisata.lt>
2024-06-10 15:06:22 +0300

    SUI address compatability test

--- packages/cryptolib/address_test.go
+++ packages/cryptolib/address_test.go
@@ -2,11 +2,37 @@ package cryptolib
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"testing"
 
 	iotago "github.com/iotaledger/iota.go/v3"
 	"github.com/stretchr/testify/require"
 )
+
+// $ sui keytool list
+// ╭────────────────────────────────────────────────────────────────────────────────────────────╮
+// │ ╭─────────────────┬──────────────────────────────────────────────────────────────────────╮ │
+// │ │ alias           │  kind-chrysoberyl                                                    │ │
+// │ │ suiAddress      │  0x1769589102f3749e02a24a1fcffea70e3fca86bf507cdb9d578a76a9c09e929a  │ │	<-- targetAddress
+// │ │ publicBase64Key │  ALZfx6OH0xhJi8cXxoyyGhEi5A67byKdcHcaxdfskguH                        │ │	<-- publicKeyBase64
+// │ │ keyScheme       │  ed25519                                                             │ │
+// │ │ flag            │  0                                                                   │ │
+// │ │ peerId          │  b65fc7a387d318498bc717c68cb21a1122e40ebb6f229d70771ac5d7ec920b87    │ │
+// │ ╰─────────────────┴──────────────────────────────────────────────────────────────────────╯ │
+// ╰────────────────────────────────────────────────────────────────────────────────────────────╯
+func TestSuiPublicKeyToAddress(t *testing.T) {
+	testSuiPublicKeyToAddress(t, "ALZfx6OH0xhJi8cXxoyyGhEi5A67byKdcHcaxdfskguH", "0x1769589102f3749e02a24a1fcffea70e3fca86bf507cdb9d578a76a9c09e929a")
+	testSuiPublicKeyToAddress(t, "AOOU7NBSgm8KzQyh1wksLYu/b+IHG9u4U93QndtS7qIc", "0xcce6099545a2de67a326ad619e953f97e05cb4b6baaf2dcc74752265396922ef")
+}
+
+func testSuiPublicKeyToAddress(t *testing.T, publicKeyBase64, targetAddress string) {
+	publicKeyWithType, err := base64.StdEncoding.DecodeString(publicKeyBase64)
+	require.NoError(t, err)
+	require.Equal(t, byte(0), publicKeyWithType[0])
+	publicKey, err := PublicKeyFromBytes(publicKeyWithType[1:])
+	require.NoError(t, err)
+	require.Equal(t, targetAddress, EncodeHex(publicKey.AsAddress().Bytes()))
+}
 
 func TestAddressSerialization(t *testing.T) {
 	data := make([]byte, AddressSize)
