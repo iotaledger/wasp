5211d6fb2bfa423f64b9d5f9a4ecdb772158eb2b
Diego Essaya <dessaya@gmail.com>
2024-06-28 15:19:46 -0300

    implement client.RequestFunds using sui package

--- clients/iscmove/client.go
+++ clients/iscmove/client.go
@@ -1,21 +1,18 @@
 package iscmove
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/fardream/go-bcs/bcs"
 
 	"github.com/iotaledger/wasp/packages/cryptolib"
 	"github.com/iotaledger/wasp/sui-go/models"
 	"github.com/iotaledger/wasp/sui-go/sui"
+	"github.com/iotaledger/wasp/sui-go/sui/conn"
 	"github.com/iotaledger/wasp/sui-go/sui_types"
 )
 
@@ -43,41 +40,20 @@ func NewClient(config Config) *Client {
 }
 
 func (c *Client) RequestFunds(ctx context.Context, address *cryptolib.Address) error {
-	paramJSON := fmt.Sprintf(`{"FixedAmountRequest":{"recipient":"%v"}}`, address)
-	request, err := http.NewRequestWithContext(ctx, http.MethodPost, c.config.FaucetURL, bytes.NewBuffer([]byte(paramJSON)))
-	if err != nil {
-		return err
+	var faucetURL string = c.config.FaucetURL
+	if faucetURL == "" {
+		switch c.config.APIURL {
+		case conn.TestnetEndpointUrl:
+			faucetURL = conn.TestnetFaucetUrl
+		case conn.DevnetEndpointUrl:
+			faucetURL = conn.DevnetFaucetUrl
+		case conn.LocalnetEndpointUrl:
+			faucetURL = conn.LocalnetFaucetUrl
+		default:
+			panic("unspecified FaucetURL")
+		}
 	}
-	request.Header.Set("Content-Type", "application/json")
-	client := http.Client{}
-	res, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusCreated && res.StatusCode != http.StatusAccepted {
-		return fmt.Errorf("post %v response code: %v", c.config.FaucetURL, res.Status)
-	}
-	defer res.Body.Close()
-
-	resByte, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-
-	//
-	var response struct {
-		Task  string `json:"task,omitempty"`
-		Error string `json:"error,omitempty"`
-	}
-	err = json.Unmarshal(resByte, &response)
-	if err != nil {
-		return err
-	}
-	if strings.TrimSpace(response.Error) != "" {
-		return errors.New(response.Error)
-	}
-
-	return nil
+	return sui.RequestFundFromFaucet(address.AsSuiAddress(), faucetURL)
 }
 
 func (c *Client) Health(ctx context.Context) error {
