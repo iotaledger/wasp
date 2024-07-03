9370eee77a5a453db5f4256860d55e6020d3607e
Gino <ginowine1@gmail.com>
2024-07-03 14:01:33 +0100

    refactor(wiki): add docstrings to ISCTypes and ISCUtil interface functions

--- packages/vm/core/evm/iscmagic/ISCUtil.sol
+++ packages/vm/core/evm/iscmagic/ISCUtil.sol
@@ -10,10 +10,19 @@ import "./ISCTypes.sol";
  * @dev Functions of the ISC Magic Contract not directly related to the ISC sandbox
  */
 interface ISCUtil {
-    // Get an ISC contract's hname given its instance name
+    /**
+     * @notice Get an ISC contract's hname given its instance name
+     * @dev Converts a string instance name to its corresponding ISC hname.
+     * @param s The instance name of the ISC contract.
+     * @return The ISCHname corresponding to the given instance name.
+     */
     function hn(string memory s) external pure returns (ISCHname);
 
-    // Print something to the console (will only work when debugging contracts with Solo)
+    /**
+     * @notice Print something to the console (will only work when debugging contracts with Solo)
+     * @dev Prints the given string to the console for debugging purposes.
+     * @param s The string to print to the console.
+     */
     function print(string memory s) external pure;
 }
 
