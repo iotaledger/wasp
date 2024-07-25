203bc509d3375370e111553186187c62adfbc539
Nikita Nikolashyn <mykyta.nikolashyn@ideasoft.io>
2024-07-25 16:17:16 +0300

    chore: Added test which was rolled back

--- packages/isc/chainid_test.go
+++ packages/isc/chainid_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/iotaledger/wasp/packages/util/rwutil"
+	"github.com/stretchr/testify/require"
 )
 
 func TestChainIDSerialization(t *testing.T) {
@@ -11,4 +12,11 @@ func TestChainIDSerialization(t *testing.T) {
 	rwutil.ReadWriteTest(t, &chainID, new(ChainID))
 	rwutil.BytesTest(t, chainID, ChainIDFromBytes)
 	rwutil.StringTest(t, chainID, ChainIDFromString)
+}
+
+func TestIncorrectPrefix(t *testing.T) {
+	chainID := "rms1prxunz807j39nmhzy3gre4hwdlzvdjyrkfn59d27x6xh426y8ajt205mh9g"
+	_, err := ChainIDFromString(chainID)
+
+	require.ErrorContains(t, err, "invalid network prefix: rms")
 }
