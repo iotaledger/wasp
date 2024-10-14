3d505b1e1f4821e234a30bcd9275561138095b60
Lukas Möller <lukas.moeller@iota.org>
2024-10-14 14:00:13 +0200

    Update default field

--- components/nodeconn/params.go
+++ components/nodeconn/params.go
@@ -5,16 +5,13 @@ import (
 )
 
 type ParametersNodeCon struct {
-	WebsocketURL          string `default:"localhost:9029" usage:"the WS address to which to connect to"`
+	WebsocketURL          string `default:"wss://api.iota-rebased-alphanet.iota.cafe/websocket" usage:"the WS address to which to connect to"`
 	PackageID             string `default:"" usage:"the identifier of the isc move package"`
 	MaxConnectionAttempts uint   `default:"30" usage:"the amount of times the connection to INX will be attempted before it fails (1 attempt per second)"`
 	TargetNetworkName     string `default:"" usage:"the network name on which the node should operate on (optional)"`
 }
 
-var ParamsWS = &ParametersNodeCon{
-	WebsocketURL: "wss://api.iota-rebased-alphanet.iota.cafe/websocket",
-	PackageID:    "",
-}
+var ParamsWS = &ParametersNodeCon{}
 
 var params = &app.ComponentParams{
 	Params: map[string]any{
