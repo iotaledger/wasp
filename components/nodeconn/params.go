f2995cbd6bb3478e227be7c0ffc65d29e3225b46
Lukas Möller <lukas.moeller@iota.org>
2024-10-14 13:15:58 +0200

    Add nodeconn params

--- components/nodeconn/params.go
+++ components/nodeconn/params.go
@@ -2,14 +2,23 @@ package nodeconn
 
 import (
 	"github.com/iotaledger/hive.go/app"
-	"github.com/iotaledger/inx-app/core/inx"
 )
 
-var ParamsINX = &inx.ParametersINX{}
+type ParametersNodeCon struct {
+	WebsocketURL          string `default:"localhost:9029" usage:"the WS address to which to connect to"`
+	PackageID             string `default:"" usage:"the identifier of the isc move package"`
+	MaxConnectionAttempts uint   `default:"30" usage:"the amount of times the connection to INX will be attempted before it fails (1 attempt per second)"`
+	TargetNetworkName     string `default:"" usage:"the network name on which the node should operate on (optional)"`
+}
+
+var ParamsWS = &ParametersNodeCon{
+	WebsocketURL: "wss://api.iota-rebased-alphanet.iota.cafe/websocket",
+	PackageID:    "",
+}
 
 var params = &app.ComponentParams{
 	Params: map[string]any{
-		"inx": ParamsINX,
+		"ws": ParamsWS,
 	},
 	Masked: nil,
 }
