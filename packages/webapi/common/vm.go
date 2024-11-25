270967d0441b09dfc21a1b7e43f14ce890380a08
Nikita Nikolashyn <mykyta.nikolashyn@ideasoft.io>
2024-11-25 12:05:09 +0200

    fix: Fixes to webapi

--- packages/webapi/common/vm.go
+++ packages/webapi/common/vm.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	chainpkg "github.com/iotaledger/wasp/packages/chain"
+	"github.com/iotaledger/wasp/packages/chainutil"
 	"github.com/iotaledger/wasp/packages/cryptolib"
 	"github.com/iotaledger/wasp/packages/isc"
 	"github.com/iotaledger/wasp/packages/state"
@@ -14,66 +15,77 @@ import (
 )
 
 func ParseReceipt(chain chainpkg.Chain, receipt *blocklog.RequestReceipt) (*isc.Receipt, error) {
-	panic("refactor me: ParseReceipt")
-	/*resolvedReceiptErr, err := chainutil.ResolveError(chain, receipt.Error)
+	state, err := chain.Store().StateByIndex(receipt.BlockIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	resolvedReceiptErr, err := chainutil.ResolveError(state, receipt.Error)
 	if err != nil {
 		return nil, err
 	}
 
 	iscReceipt := receipt.ToISCReceipt(resolvedReceiptErr)
 
-	return iscReceipt, nil*/
+	return iscReceipt, nil
 }
 
 func CallView(ch chainpkg.Chain, msg isc.Message, blockIndexOrHash string) (isc.CallArguments, error) {
-	panic("refactor me: CallView")
+	anchor, err := ch.LatestAnchor(chainpkg.ActiveOrCommittedState)
+	if err != nil {
+		return nil, fmt.Errorf("error getting latest anchor: %w", err)
+	}
 
 	var chainState state.State
-	_ = chainState
-	var err error
+
 	switch {
 	case blockIndexOrHash == "":
 		chainState, err = ch.LatestState(chainpkg.ActiveOrCommittedState)
 		if err != nil {
 			return nil, fmt.Errorf("error getting latest chain state: %w", err)
 		}
+
 	case strings.HasPrefix(blockIndexOrHash, "0x"):
 		hashBytes, err := cryptolib.DecodeHex(blockIndexOrHash)
 		if err != nil {
 			return nil, fmt.Errorf("invalid block hash: %v", blockIndexOrHash)
 		}
+
 		trieRoot, err := trie.HashFromBytes(hashBytes)
 		if err != nil {
 			return nil, fmt.Errorf("invalid block hash: %v", blockIndexOrHash)
 		}
+
 		chainState, err = ch.Store().StateByTrieRoot(trieRoot)
 		if err != nil {
 			return nil, fmt.Errorf("error getting block by trie root: %w", err)
 		}
+
 	default:
 		blockIndex, err := strconv.ParseUint(blockIndexOrHash, 10, 32)
 		if err != nil {
 			return nil, fmt.Errorf("invalid block number: %v", blockIndexOrHash)
 		}
+
 		chainState, err = ch.Store().StateByIndex(uint32(blockIndex))
 		if err != nil {
 			return nil, fmt.Errorf("error getting block by index: %w", err)
 		}
 	}
 
-	return nil, nil
-	//return chainutil.CallView(chainState, ch, msg)
+	return chainutil.CallView(anchor, chainState, ch.Processors(), ch.Log(), msg)
 }
 
 func EstimateGas(ch chainpkg.Chain, req isc.Request) (*isc.Receipt, error) {
-	panic("refactor me: EstimateGas")
+	anchor, err := ch.LatestAnchor(chainpkg.ActiveOrCommittedState)
+	if err != nil {
+		return nil, fmt.Errorf("error getting latest anchor: %w", err)
+	}
 
-	/*
-			rec, err := chainutil.SimulateRequest(ch, req, true)
-		if err != nil {
-			return nil, err
-		}
-		parsedRec, err := ParseReceipt(ch, rec)
-		return parsedRec, err
-	*/
+	rec, err := chainutil.SimulateRequest(anchor, ch.Store(), ch.Processors(), ch.Log(), req, true)
+	if err != nil {
+		return nil, err
+	}
+	parsedRec, err := ParseReceipt(ch, rec)
+	return parsedRec, err
 }
