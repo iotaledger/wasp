748f39de01aea53b9ad251c6ad9db09d16cd9464
Yang Hau <yuanyanghau@gmail.com>
2024-11-28 20:09:34 +0100

    fix state_test

--- packages/state/state_test.go
+++ packages/state/state_test.go
@@ -6,7 +6,6 @@ package state_test
 import (
 	"bytes"
 	"fmt"
-	"github.com/iotaledger/wasp/clients/iota-go/iotago"
 	"math/rand"
 	"strings"
 	"testing"
@@ -17,9 +16,11 @@ import (
 
 	"github.com/iotaledger/hive.go/kvstore"
 	"github.com/iotaledger/hive.go/kvstore/mapdb"
+	"github.com/iotaledger/wasp/clients/iota-go/iotago"
 	"github.com/iotaledger/wasp/packages/coin"
 	"github.com/iotaledger/wasp/packages/isc"
 	"github.com/iotaledger/wasp/packages/isc/coreutil"
+	"github.com/iotaledger/wasp/packages/isc/isctest"
 	"github.com/iotaledger/wasp/packages/kv"
 	"github.com/iotaledger/wasp/packages/origin"
 	"github.com/iotaledger/wasp/packages/state"
@@ -112,11 +113,11 @@ func (m mustChainStore) checkTrie(trieRoot trie.Hash) {
 	})
 }
 
-var baseTokenCoinInfo = &isc.IotaCoinInfo{CoinType: coin.BaseTokenType}
+var initArgs = origin.DefaultInitParams(isctest.NewRandomAgentID()).Encode()
 
 func initializedStore(db kvstore.KVStore) state.Store {
 	st := state.NewStoreWithUniqueWriteMutex(db)
-	origin.InitChain(0, st, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo)
+	origin.InitChain(0, st, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo)
 	return st
 }
 
@@ -149,14 +150,14 @@ func TestOriginBlockDeterminism(t *testing.T) {
 		db := mapdb.NewMapDB()
 		st := state.NewStoreWithUniqueWriteMutex(db)
 		require.True(t, st.IsEmpty())
-		blockA, _ := origin.InitChain(0, st, nil, iotago.ObjectID{}, deposit, baseTokenCoinInfo)
-		blockB, _ := origin.InitChain(0, st, nil, iotago.ObjectID{}, deposit, baseTokenCoinInfo)
+		blockA, _ := origin.InitChain(0, st, initArgs, iotago.ObjectID{}, deposit, isc.BaseTokenCoinInfo)
+		blockB, _ := origin.InitChain(0, st, initArgs, iotago.ObjectID{}, deposit, isc.BaseTokenCoinInfo)
 		require.False(t, st.IsEmpty())
 		require.Equal(t, blockA.L1Commitment(), blockB.L1Commitment())
 		db2 := mapdb.NewMapDB()
 		st2 := state.NewStoreWithUniqueWriteMutex(db2)
 		require.True(t, st2.IsEmpty())
-		blockC, _ := origin.InitChain(0, st2, nil, iotago.ObjectID{}, deposit, baseTokenCoinInfo)
+		blockC, _ := origin.InitChain(0, st2, initArgs, iotago.ObjectID{}, deposit, isc.BaseTokenCoinInfo)
 		require.False(t, st2.IsEmpty())
 		require.Equal(t, blockA.L1Commitment(), blockC.L1Commitment())
 	})
@@ -261,7 +262,7 @@ func TestEqualStates(t *testing.T) {
 	db1 := mapdb.NewMapDB()
 	cs1 := mustChainStore{initializedStore(db1)}
 	time1 := time.Now()
-	draft1 := cs1.NewStateDraft(time1, origin.L1Commitment(0, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo))
+	draft1 := cs1.NewStateDraft(time1, origin.L1Commitment(0, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo))
 	draft1.Set("a", []byte("variable a"))
 	draft1.Set("b", []byte("variable b"))
 	block1 := cs1.Commit(draft1)
@@ -279,7 +280,7 @@ func TestEqualStates(t *testing.T) {
 
 	db2 := mapdb.NewMapDB()
 	cs2 := mustChainStore{initializedStore(db2)}
-	draft1 = cs2.NewStateDraft(time1, origin.L1Commitment(0, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo))
+	draft1 = cs2.NewStateDraft(time1, origin.L1Commitment(0, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo))
 	draft1.Set("b", []byte("variable b"))
 	draft1.Set("a", []byte("variable a"))
 	block1 = cs2.Commit(draft1)
@@ -332,14 +333,14 @@ func TestDiffStatesValues(t *testing.T) {
 	db1 := mapdb.NewMapDB()
 	cs1 := mustChainStore{initializedStore(db1)}
 	time1 := time.Now()
-	draft1 := cs1.NewStateDraft(time1, origin.L1Commitment(0, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo))
+	draft1 := cs1.NewStateDraft(time1, origin.L1Commitment(0, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo))
 	draft1.Set("a", []byte("variable a"))
 	block1 := cs1.Commit(draft1)
 	state1 := cs1.StateByTrieRoot(block1.TrieRoot())
 
 	db2 := mapdb.NewMapDB()
 	cs2 := mustChainStore{initializedStore(db2)}
-	draft1 = cs2.NewStateDraft(time1, origin.L1Commitment(0, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo))
+	draft1 = cs2.NewStateDraft(time1, origin.L1Commitment(0, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo))
 	draft1.Set("a", []byte("other value of a"))
 	block1 = cs2.Commit(draft1)
 	state2 := cs2.StateByTrieRoot(block1.TrieRoot())
@@ -352,7 +353,7 @@ func TestDiffStatesBlockIndex(t *testing.T) {
 	db1 := mapdb.NewMapDB()
 	cs1 := mustChainStore{initializedStore(db1)}
 	time1 := time.Now()
-	draft1 := cs1.NewStateDraft(time1, origin.L1Commitment(0, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo))
+	draft1 := cs1.NewStateDraft(time1, origin.L1Commitment(0, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo))
 	draft1.Set("a", []byte("variable a"))
 	block1 := cs1.Commit(draft1)
 	time2 := time.Now()
@@ -363,7 +364,7 @@ func TestDiffStatesBlockIndex(t *testing.T) {
 
 	db2 := mapdb.NewMapDB()
 	cs2 := mustChainStore{initializedStore(db2)}
-	draft1 = cs2.NewStateDraft(time1, origin.L1Commitment(0, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo))
+	draft1 = cs2.NewStateDraft(time1, origin.L1Commitment(0, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo))
 	draft1.Set("a", []byte("variable a"))
 	draft1.Set("b", []byte("variable b"))
 	block1 = cs2.Commit(draft1)
@@ -378,14 +379,14 @@ func TestDiffStatesBlockIndex(t *testing.T) {
 func TestDiffStatesTimestamp(t *testing.T) {
 	db1 := mapdb.NewMapDB()
 	cs1 := mustChainStore{initializedStore(db1)}
-	draft1 := cs1.NewStateDraft(time.Now(), origin.L1Commitment(0, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo))
+	draft1 := cs1.NewStateDraft(time.Now(), origin.L1Commitment(0, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo))
 	draft1.Set("a", []byte("variable a"))
 	block1 := cs1.Commit(draft1)
 	state1 := cs1.StateByTrieRoot(block1.TrieRoot())
 
 	db2 := mapdb.NewMapDB()
 	cs2 := mustChainStore{initializedStore(db2)}
-	draft1 = cs2.NewStateDraft(time.Now(), origin.L1Commitment(0, nil, iotago.ObjectID{}, 0, baseTokenCoinInfo))
+	draft1 = cs2.NewStateDraft(time.Now(), origin.L1Commitment(0, initArgs, iotago.ObjectID{}, 0, isc.BaseTokenCoinInfo))
 	draft1.Set("a", []byte("variable a"))
 	block1 = cs2.Commit(draft1)
 	state2 := cs2.StateByTrieRoot(block1.TrieRoot())
