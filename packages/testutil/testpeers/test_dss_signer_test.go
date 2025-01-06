cf31cac6660bb4002836d1a27bbdb3cbd85923c4
Karolis Petrauskas <karolis.petrauskas@erisata.lt>
2025-01-06 15:26:55 +0200

    Test DSS signer implemented.
    Signed-off-by: Karolis Petrauskas <karolis.petrauskas@erisata.lt>

---
+++ packages/testutil/testpeers/test_dss_signer_test.go
@@ -0,0 +1,32 @@
+package testpeers_test
+
+import (
+	"testing"
+
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/require"
+
+	"github.com/iotaledger/wasp/packages/gpa"
+	"github.com/iotaledger/wasp/packages/testutil/testlogger"
+	"github.com/iotaledger/wasp/packages/testutil/testpeers"
+)
+
+func TestDSSSigner(t *testing.T) {
+	//
+	// Infrastructure.
+	log := testlogger.NewLogger(t)
+	defer log.Sync()
+	n := 4
+	f := 1
+	_, peerIdentities := testpeers.SetupKeys(uint16(n))
+	nodeIDs := gpa.MakeTestNodeIDs(n)
+	addr, dkRegs := testpeers.SetupDkgTrivial(t, n, f, peerIdentities, nil)
+	//
+	// Create the signer.
+	signer := testpeers.NewTestDSSSigner(addr, dkRegs, nodeIDs, peerIdentities, log)
+	//
+	// Use it.
+	msg := []byte{1, 2, 3}
+	signature := lo.Must(signer.Sign(msg))
+	require.True(t, signature.Validate(msg))
+}
