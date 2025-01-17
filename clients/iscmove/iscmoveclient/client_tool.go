b9ee4b28853868e637a33c8190981b7fb3b01deb
Yang Hau <yuanyanghau@gmail.com>
2025-01-17 14:06:23 +0100

    test: Fix TestCounter

---
+++ clients/iscmove/iscmoveclient/client_tool.go
@@ -0,0 +1,29 @@
+package iscmoveclient
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/iotaledger/wasp/clients/iota-go/iotaclient"
+	"github.com/iotaledger/wasp/clients/iota-go/iotago"
+	"github.com/iotaledger/wasp/clients/iota-go/iotajsonrpc"
+)
+
+func (c *Client) GetCoin(
+	ctx context.Context,
+	coinID *iotago.ObjectID,
+) (*MoveCoin, error) {
+	getCoinRes, err := c.GetObject(ctx, iotaclient.GetObjectRequest{
+		ObjectID: coinID,
+		Options:  &iotajsonrpc.IotaObjectDataOptions{ShowBcs: true},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to call GetObject: %w", err)
+	}
+	var moveCoin MoveCoin
+	err = iotaclient.UnmarshalBCS(getCoinRes.Data.Bcs.Data.MoveObject.BcsBytes, &moveCoin)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarhal MoveCoin: %w", err)
+	}
+	return &moveCoin, nil
+}
