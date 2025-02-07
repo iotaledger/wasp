844b0b0ffdee7581fadefc230ddb310e934cfb88
Lukas Möller <lukas.moeller@iota.org>
2025-02-07 19:03:34 +0100

    Update disrec test

--- tools/wasp-cli/disrec/disrec_test.go
+++ tools/wasp-cli/disrec/disrec_test.go
@@ -37,45 +37,27 @@ func TestCreateTX(t *testing.T) {
 	// It really is the Committee Address (Not the Anchor Object ID aka ChainID as this transaction will be signed by the committee)
 	// This specific address is the product of the committee keys in `test_committee_keys`.
 	var CommitteeAddress = lo.Must(cryptolib.AddressFromHex("0x4c7fb31a460907210c3b7cbaa50cf9faa23f60cbfbe5f26efd27809265458894"))
-	var GasCoinAddress = iotago.MustAddressFromHex("0x2da2d776a0fa86c76a7a1f01d13209abc36930c9357e648e79c0c3c261951c76")
 
 	client := cliclients.L1Client()
 	kp := cryptolib.NewKeyPair()
+	wallet := providers.NewUnsafeInMemoryTestingSeed(kp, 0)
 	require.NoError(t, client.RequestFunds(context.Background(), *kp.Address()))
 	packageID := lo.Must(client.DeployISCContracts(context.Background(), cryptolib.SignerToIotaSigner(kp)))
 
 	ptb := iotago.NewProgrammableTransactionBuilder()
 	ptb = iscmoveclient.PTBAssetsBagNewAndTransfer(ptb, packageID, CommitteeAddress)
 
-	gasCoin, err := client.GetObject(context.Background(), iotaclient.GetObjectRequest{
-		ObjectID: GasCoinAddress,
-	})
+	t.Log("Creating new coin and transfer it to the Committee address")
+	newGasCoinAddress := lo.Must(chain.CreateAndSendGasCoin(context.Background(), client, wallet, CommitteeAddress.AsIotaAddress()))
 
-	if err != nil || gasCoin.Error != nil || gasCoin.Data == nil {
-		t.Log("The gas coin could not be found. Creating a new one.")
+	gasCoin := lo.Must(client.GetObject(context.Background(), iotaclient.GetObjectRequest{
+		ObjectID: &newGasCoinAddress,
+	})).Data.Ref()
 
-		wallet := providers.NewUnsafeInMemoryTestingSeed(kp, 0)
+	t.Logf("Gas coin ref: %v\n", gasCoin)
 
-		t.Log("Creating new coin and transfer it to the Committee address")
-		newGasCoinAddress := lo.Must(chain.CreateAndSendGasCoin(context.Background(), client, wallet, CommitteeAddress.AsIotaAddress()))
-
-		gasCoin = lo.Must(client.GetObject(context.Background(), iotaclient.GetObjectRequest{
-			ObjectID: &newGasCoinAddress,
-		}))
-	}
-
-	gasCoinRef := gasCoin.Data.Ref()
-
-	t.Logf("Gas coin ref: %v\n", gasCoinRef)
-
-	tx := iotago.NewProgrammable(
-		CommitteeAddress.AsIotaAddress(),
-		ptb.Finish(),
-		[]*iotago.ObjectRef{&gasCoinRef},
-		9999999,
-		1000,
-	)
-
+	tx := iotago.NewProgrammable(CommitteeAddress.AsIotaAddress(), ptb.Finish(), []*iotago.ObjectRef{&gasCoin}, 9999999, 1000)
 	txnBytes := lo.Must(bcs.Marshal(&tx))
+
 	t.Logf("Test Transaction hex:\n%s\n", hexutil.Encode(txnBytes))
 }
