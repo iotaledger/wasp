afe75dcd2fcf32f3c4966d17112507a210ec559a
Mykyta Nikolashyn <mykyta.nikolashyn@ideasoft.io>
2025-03-28 13:27:42 +0200

    Fixed validation of blocklog for case of more then 10000 blocks

--- tools/stardust-migration/validation/contract_blocklog.go
+++ tools/stardust-migration/validation/contract_blocklog.go
@@ -8,11 +8,15 @@ import (
 	old_isc "github.com/nnikolash/wasp-types-exported/packages/isc"
 	old_kv "github.com/nnikolash/wasp-types-exported/packages/kv"
 	old_blocklog "github.com/nnikolash/wasp-types-exported/packages/vm/core/blocklog"
+	"github.com/samber/lo"
 
 	"github.com/iotaledger/wasp/packages/isc"
 	"github.com/iotaledger/wasp/packages/kv"
 	"github.com/iotaledger/wasp/packages/vm/core/blocklog"
+	"github.com/iotaledger/wasp/tools/stardust-migration/utils/cli"
 )
+
+const blockRetentionPeriod = 10000
 
 func OldBlocklogContractContentToStr(contractState old_kv.KVStoreReader, chainID old_isc.ChainID, firstIndex, lastIndex uint32) string {
 	return OldReceiptsContentToStr(contractState, firstIndex, lastIndex)
@@ -20,8 +24,10 @@ func OldBlocklogContractContentToStr(contractState old_kv.KVStoreReader, chainID
 
 func OldReceiptsContentToStr(contractState old_kv.KVStoreReader, firstIndex, lastIndex uint32) string {
 	var requestStr strings.Builder
+	var firstAvailableBlockIndex = getFirstAvailableBlockIndex(firstIndex, lastIndex)
+	cli.Logf("Retrieving old receipts content: %v-%v => %v-%v", firstIndex, lastIndex, firstAvailableBlockIndex, lastIndex)
 
-	for blockIndex := max(firstIndex, 1); blockIndex < lastIndex; blockIndex++ {
+	for blockIndex := firstAvailableBlockIndex; blockIndex < lastIndex; blockIndex++ {
 		_, requests, err := old_blocklog.GetRequestsInBlock(contractState, blockIndex)
 		if err != nil {
 			panic(err)
@@ -34,6 +40,13 @@ func OldReceiptsContentToStr(contractState old_kv.KVStoreReader, firstIndex, las
 		}
 	}
 
+	if firstAvailableBlockIndex > 0 {
+		firstUnavailableBlockIndex := firstAvailableBlockIndex - 1
+		_, _, err := old_blocklog.GetRequestsInBlock(contractState, firstUnavailableBlockIndex)
+		requestStr.WriteString(fmt.Sprintf("Last pruned block (%v): %v\n",
+			firstUnavailableBlockIndex, lo.Ternary(err == nil, "AVAILABLE", "unavailable")))
+	}
+
 	return requestStr.String()
 }
 
@@ -43,8 +56,10 @@ func NewBlocklogContractContentToStr(contractState kv.KVStoreReader, chainID isc
 
 func NewReceiptsContentToStr(contractState kv.KVStoreReader, firstIndex, lastIndex uint32) string {
 	var requestStr strings.Builder
+	var firstAvailableBlockIndex = getFirstAvailableBlockIndex(firstIndex, lastIndex)
+	cli.Logf("Retrieving new receipts content: %v-%v => %v-%v", firstIndex, lastIndex, firstAvailableBlockIndex, lastIndex)
 
-	for blockIndex := max(firstIndex, 1); blockIndex < lastIndex; blockIndex++ {
+	for blockIndex := firstAvailableBlockIndex; blockIndex < lastIndex; blockIndex++ {
 		_, requests, err := blocklog.NewStateReader(contractState).GetRequestsInBlock(blockIndex)
 		if err != nil {
 			panic(err)
@@ -56,5 +71,20 @@ func NewReceiptsContentToStr(contractState kv.KVStoreReader, firstIndex, lastInd
 		}
 	}
 
+	if firstAvailableBlockIndex > 0 {
+		firstUnavailableBlockIndex := firstAvailableBlockIndex - 1
+		_, _, err := blocklog.NewStateReader(contractState).GetRequestsInBlock(firstUnavailableBlockIndex)
+		requestStr.WriteString(fmt.Sprintf("Last pruned block (%v): %v\n",
+			firstUnavailableBlockIndex, lo.Ternary(err == nil, "AVAILABLE", "unavailable")))
+
+	}
+
 	return requestStr.String()
+}
+
+func getFirstAvailableBlockIndex(firstIndexHint uint32, lastIndex uint32) uint32 {
+	if lastIndex < blockRetentionPeriod {
+		return 0
+	}
+	return max(lastIndex-blockRetentionPeriod+1, firstIndexHint)
 }
