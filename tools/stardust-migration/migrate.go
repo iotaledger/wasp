cc4b6ba682846f439866c32146e066fd872f904e
Nikita Nikolashin <mykyta.nikolashyn@ideasoft.io>
2025-04-01 05:42:57 -0700

    Inverted refCount cache option, because it uses entire RAM (and more), left it just as reference

--- tools/stardust-migration/migrate.go
+++ tools/stardust-migration/migrate.go
@@ -63,10 +63,10 @@ const (
 )
 
 type migrationOptions struct {
-	UseDummyPrepConfig   bool
-	DisableRefCountCache bool
-	ContinueMigration    bool
-	DryRun               bool
+	UseDummyPrepConfig  bool
+	EnableRefCountCache bool
+	ContinueMigration   bool
+	DryRun              bool
 }
 
 func initMigration(srcChainDBDir, destChainDBDir string, o *migrationOptions) (
@@ -126,7 +126,7 @@ func initMigration(srcChainDBDir, destChainDBDir string, o *migrationOptions) (
 	if o.DryRun {
 		destStore = indexedstore.New(state.NewStoreWithUniqueWriteMutex(mapdb.NewMapDB()))
 		close = func() {}
-	} else if o.DisableRefCountCache {
+	} else if !o.EnableRefCountCache {
 		destKVS := db.Create(destChainDBDir)
 		destStore = indexedstore.New(state.NewStoreWithUniqueWriteMutex(destKVS))
 		close = func() { destKVS.Close() }
@@ -259,8 +259,8 @@ func migrateSingleState(c *cmd.Context) error {
 	dryRun := c.Bool("dry-run")
 
 	srcStore, destStore, oldChainID, prepConfig, stateMetadata, flush := initMigration(srcChainDBDir, destChainDBDir, &migrationOptions{
-		DryRun:               dryRun,
-		DisableRefCountCache: true,
+		DryRun:              dryRun,
+		EnableRefCountCache: true,
 	})
 	defer flush()
 
@@ -330,7 +330,7 @@ func migrateAllStates(c *cmd.Context) error {
 	skipLoad := c.Bool("skip-load")
 	continueMigration := c.Bool("continue")
 	disableStateCache := c.Bool("no-state-cache")
-	disableRefcountCache := c.Bool("no-refcount-cache")
+	enableRefcountCache := c.Bool("refcount-cache")
 	useDummyPrepConfig := c.Bool("dummy-prep-config")
 	dryRun := c.Bool("dry-run")
 	printBlockIdx := c.Bool("print-block-idx")
@@ -350,10 +350,10 @@ func migrateAllStates(c *cmd.Context) error {
 	}
 
 	srcStore, destStore, oldChainID, prepareConfig, stateMetadata, flush := initMigration(srcChainDBDir, destChainDBDir, &migrationOptions{
-		UseDummyPrepConfig:   useDummyPrepConfig,
-		ContinueMigration:    continueMigration,
-		DryRun:               dryRun,
-		DisableRefCountCache: disableRefcountCache,
+		UseDummyPrepConfig:  useDummyPrepConfig,
+		ContinueMigration:   continueMigration,
+		DryRun:              dryRun,
+		EnableRefCountCache: enableRefcountCache,
 	})
 	defer flush()
 
