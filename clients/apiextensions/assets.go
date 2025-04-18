ef6b960ee4ae4a4f9196279a122fe32ed6204bbb
Yang Hau <yuanyanghau@gmail.com>
2025-04-18 13:36:51 +0200

    wip

--- clients/apiextensions/assets.go
+++ clients/apiextensions/assets.go
@@ -1,43 +1,21 @@
 package apiextensions
 
 import (
-	"strconv"
-
 	"github.com/iotaledger/wasp/clients/apiclient"
 	"github.com/iotaledger/wasp/packages/coin"
 	"github.com/iotaledger/wasp/packages/isc"
 )
 
-// TODO: Handle Coins (other than base tokens) and Objects
+// TODO: Handle Objects
 func AssetsFromAPIResponse(assetsResponse *apiclient.AssetsResponse) (*isc.Assets, error) {
-	baseTokens, err := strconv.ParseUint(assetsResponse.BaseTokens, 10, 64)
-	if err != nil {
-		return nil, err
+	assets := isc.NewAssets(0)
+	for _, resCoin := range assetsResponse.Coins {
+		bal, err := coin.ValueFromString(resCoin.Balance)
+		if err != nil {
+			return nil, err
+		}
+		assets.AddCoin(coin.MustTypeFromString(resCoin.CoinType), bal)
 	}
 
-	assets := isc.NewAssets(coin.Value(baseTokens))
-	/*
-		for _, nativeToken := range assetsResponse.NativeTokens {
-			nativeTokenIDHex, err2 := cryptolib.DecodeHex(nativeToken.Id)
-			if err2 != nil {
-				return nil, err2
-			}
-
-			nativeTokenID, err2 := isc.NativeTokenIDFromBytes(nativeTokenIDHex)
-			if err2 != nil {
-				return nil, err2
-			}
-
-			amount, err2 := iotago.DecodeUint256(nativeToken.Amount)
-			if err2 != nil {
-				return nil, err2
-			}
-
-			assets.NativeTokens = append(assets.NativeTokens, &iotago.NativeToken{
-				ID:     nativeTokenID,
-				Amount: amount,
-			})
-		}*/
-
-	return assets, err
+	return assets, nil
 }
