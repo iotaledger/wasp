5c79a8f9e6f6946474ad347d21efe1bea0fddd64
Yang Hau <yuanyanghau@gmail.com>
2025-09-15 21:28:55 +0800

    fix: Improve wasp-cli error handling

--- tools/wasp-cli/chain/deploy.go
+++ tools/wasp-cli/chain/deploy.go
@@ -139,8 +139,9 @@ func initializeDeploymentWithGasCoin(ctx context.Context, signer wallets.Wallet,
 
 	client := cliclients.WaspClientWithVersionCheck(ctx, node)
 	_, header, err := client.ChainsAPI.GetChainInfo(ctx).Execute()
-	defer header.Body.Close()
-
+	if header != nil && header.Body != nil {
+		defer header.Body.Close()
+	}
 	// We expect a 404 if no chain has been deployed yet. In any other case, show the error.
 	if err != nil && !strings.Contains(err.Error(), strconv.Itoa(http.StatusNotFound)) {
 		return nil, fmt.Errorf("failed to get current chain info: %w", err)
