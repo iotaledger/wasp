1fcaba2c04a41f7017a139527239b423a8fa5c94
Chris McCandless <chriss.mccandless73@gmail.com>
2025-10-21 13:47:37 +0300

    Fixed lint

--- packages/chain/chainmanager/var_access_nodes_state.go
+++ packages/chain/chainmanager/var_access_nodes_state.go
@@ -30,8 +30,8 @@ func (vas *VarAccessNodeState) Tip() *isc.StateAnchor {
 	return vas.tipAnchor
 }
 
-// TODO: Probably this function can be removed at all. This left from the pipelining. //nolint:staticcheck
 func (vas *VarAccessNodeState) BlockProduced(tx *iotasigner.SignedTransaction) (*isc.StateAnchor, bool, *state.L1Commitment) {
+	// TODO: Probably this function can be removed at all. This left from the pipelining.
 	vas.log.LogDebugf("BlockProduced: tx=%v", tx)
 	return vas.tipAnchor, false, nil
 }
