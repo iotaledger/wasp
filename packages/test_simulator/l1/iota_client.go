38dc432763f21e0fc1ec25a20ddd5ed40fa73e49
Yang Hau <yuanyanghau@gmail.com>
2026-02-19 13:53:38 +0100

    refactor: Remove unused graphql methods

--- packages/test_simulator/l1/iota_client.go
+++ packages/test_simulator/l1/iota_client.go
@@ -564,43 +564,20 @@ func (c *FakeIotaClient) GetReferenceGasPrice(_ context.Context) (*iotagraphql.B
 func (c *FakeIotaClient) GetLatestIotaSystemState(_ context.Context) (*iotagraphql.GetLatestIotaSystemStateResponse, error) {
 	now := time.Now()
 	epochStart := now.Add(-1 * time.Hour)
-	epochEnd := now.Add(23 * time.Hour)
 
 	return &graphqltypes.GetLatestIotaSystemStateResponse{
 		Epoch: graphqltypes.GetLatestIotaSystemStateEpoch{
-			EpochId:            0,
-			StartTimestamp:     epochStart,
-			EndTimestamp:       epochEnd,
-			ReferenceGasPrice:  *graphqltypes.NewBigInt(defaultGasPrice),
-			IotaTotalSupply:    *graphqltypes.NewBigInt(10_000_000_000_000_000_000), // 10B IOTA
-			SystemStateVersion: 1,
+			EpochId:           0,
+			StartTimestamp:    epochStart,
+			ReferenceGasPrice: *graphqltypes.NewBigInt(defaultGasPrice),
+			IotaTotalSupply:   *graphqltypes.NewBigInt(10_000_000_000_000_000_000), // 10B IOTA
 			ProtocolConfigs: graphqltypes.GetLatestIotaSystemStateEpochProtocolConfigs{
 				ProtocolVersion: 1,
 			},
-			SafeMode: graphqltypes.GetLatestIotaSystemStateEpochSafeMode{
-				GasSummary: graphqltypes.GetLatestIotaSystemStateEpochSafeModeGasSummaryGasCostSummary{
-					ComputationCost:         *graphqltypes.NewBigInt(0),
-					NonRefundableStorageFee: *graphqltypes.NewBigInt(0),
-					StorageCost:             *graphqltypes.NewBigInt(0),
-					StorageRebate:           *graphqltypes.NewBigInt(0),
-				},
-			},
-			StorageFund: graphqltypes.GetLatestIotaSystemStateEpochStorageFund{
-				NonRefundableBalance:      *graphqltypes.NewBigInt(0),
-				TotalObjectStorageRebates: *graphqltypes.NewBigInt(0),
-			},
 			SystemParameters: graphqltypes.GetLatestIotaSystemStateEpochSystemParameters{
-				MinValidatorCount:              4,
-				MaxValidatorCount:              150,
-				MinValidatorJoiningStake:       *graphqltypes.NewBigInt(30_000_000_000_000),
-				DurationMs:                     *graphqltypes.NewBigInt(86_400_000),
-				ValidatorLowStakeThreshold:     *graphqltypes.NewBigInt(20_000_000_000_000),
-				ValidatorLowStakeGracePeriod:   *graphqltypes.NewBigInt(7),
-				ValidatorVeryLowStakeThreshold: *graphqltypes.NewBigInt(15_000_000_000_000),
+				DurationMs: *graphqltypes.NewBigInt(86_400_000),
 			},
-			ValidatorSet: graphqltypes.GetLatestIotaSystemStateEpochValidatorSet{
-				TotalStake: *graphqltypes.NewBigInt(1_000_000_000_000_000),
-			},
+			ValidatorSet: graphqltypes.GetLatestIotaSystemStateEpochValidatorSet{},
 		},
 	}, nil
 }
